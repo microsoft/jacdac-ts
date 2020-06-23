bfc7f9df09e4763dd083e52baf5f28ed6c16c718
peli <pelikhan@users.noreply.github.com>
2020-06-22 23:27:11 -0700

    patch: more sanity

--- rollup.config.ts
+++ rollup.config.ts
@@ -24,14 +24,16 @@ export default {
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript({
+      useTsconfigDeclarationDir: true,
+      tsconfig: "tsconfig.dist.json"
+    }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
-
     // Resolve source maps to the original source
     sourceMaps()
   ]
