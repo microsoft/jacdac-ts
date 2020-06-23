ee53a891a8120f6d25d2f84ef885aa78d224c22e
peli <pelikhan@users.noreply.github.com>
2020-06-23 09:25:59 -0700

    enable github pages

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'jacdac'
+const libraryName = 'jacdac-ts'
 
 export default {
   input: `src/${libraryName}.ts`,
