c76c4dfbb20a4365a31ba3184e9ddf04a29d86b7
peli <pelikhan@users.noreply.github.com>
2020-06-22 17:11:51 -0700

    more docs

--- test/jacdac-ts.test.ts
+++ test/jacdac-ts.test.ts
@@ -1,14 +1,5 @@
-import DummyClass from "../src/jacdac"
 
-/**
- * Dummy test
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
-
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+describe("dummy test", () => {
+  it("tests are running", () => {
   })
 })
-*/
