de5cb657ca0f0325e408c2395d485ef2123382be
peli <pelikhan@users.noreply.github.com>
2020-06-23 06:56:16 -0700

    attempt at publishing to npm

--- dist/types/nodewebusb.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="w3c-web-usb" />
-/**
- * Node.JS webusb bindings. Make sure to add "webusb" to your package.json config.
- * @param options
- */
-export declare function requestDevice(options: USBDeviceRequestOptions): any;
