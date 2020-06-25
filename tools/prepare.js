424249f5d66b55dc86f3de905899467f47507d32
peli <pelikhan@users.noreply.github.com>
2020-06-25 07:38:23 -0700

    patch: fix load path

--- tools/prepare.js
+++ tools/prepare.js
@@ -1,18 +1,24 @@
 const replace = require('replace-in-file');
-const pkg = require('./package.json')
+const pkg = require('../package.json')
 const version = pkg.version;
-console.log(`patching docs to ${version}`)
 
-const options = {
-  files: './docs/**/*.html',
-  from: "/dist/jacdac.umd.js",
-  to: `https://cdn.jsdelivr.net/npm/jacdac-ts@${version}`
-};
 
-try {
+async function patchDocs() {
+  console.log(`patching docs to ${version}`)
+
+  const options = {
+    files: './docs/**/*.html',
+    from: "/dist/jacdac.umd.js",
+    to: `https://cdn.jsdelivr.net/npm/jacdac-ts@${version}`
+  };
+
+  try {
     const results = await replace(options)
     console.log('Replacement results:', results);
   }
   catch (error) {
     console.error('Error occurred:', error);
   }
+}
+
+patchDocs();
