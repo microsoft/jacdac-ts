214cf3c27254a0e585469c29924c550c550b0929
peli <pelikhan@users.noreply.github.com>
2020-06-25 13:58:44 -0700

    patch: manual copy to github pages

---
+++ docs/src/gatsby-theme-material-ui-top-layout/components/top-layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+//import { Provider } from "react-redux";
+import ThemeTopLayout from "gatsby-theme-material-ui-top-layout/src/components/top-layout";
+
+//import createStore from "../../state/createStore";
+
+export default function TopLayout({ children, theme }) {
+//  const store = createStore();
+
+  return (
+ //   <Provider store={store}>
+      <ThemeTopLayout theme={theme}>{children}</ThemeTopLayout>
+   // </Provider>
+  );
+}
