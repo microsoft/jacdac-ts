8b6527978083b10a8f9faf4c44eba5a7e3e0458e
peli <pelikhan@users.noreply.github.com>
2020-06-26 10:49:04 -0700

    lazy allocate query

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,26 +1,32 @@
-import { graphql, buildSchema, parse, validate, DocumentNode, ExecutionResult } from "graphql"
+import { graphql, buildSchema, parse, validate, DocumentNode, ExecutionResult, printSchema, GraphQLSchema } from "graphql"
 import { Bus } from "./bus";
 
-// keep in sync with schema.graphql
-const schema = buildSchema(`
-type Bus {
-    connected: Boolean!
-    connecting: Boolean!
-    devices: [Device!]!
-    device(deviceId: String): Device
-}
-type Device {
-    deviceId: ID
-    shortId: String!
-    name: String!
-    services: [Int!]!
-}
-schema {
-  query: Bus
-}
-`);
+
+let schema: GraphQLSchema = undefined;
 
 export function createGraphQLQuery(bus: Bus): (query: string | Query) => Promise<ExecutionResult> {
+    // lazy allocated schema
+    if (!schema) {
+        // keep in sync with schema.graphql
+        schema = buildSchema(`
+        type Bus {
+            connected: Boolean!
+            connecting: Boolean!
+            devices: [Device!]!
+            device(deviceId: String): Device
+        }
+        type Device {
+            deviceId: ID
+            shortId: String!
+            name: String!
+            services: [Int!]!
+        }
+        schema {
+          query: Bus
+        }
+        `);
+    }
+
     const root = {
         connected: () => bus.connected,
         connecting: () => bus.connecting,
