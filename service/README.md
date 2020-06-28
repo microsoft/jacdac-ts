a52d72d9cd8fce86ee8d6551b45a810026d0189a
peli <pelikhan@users.noreply.github.com>
2020-06-28 08:45:39 -0700

    adding readme

---
+++ service/README.md
@@ -0,0 +1,15 @@
+# JACDAC GraphiQL service
+
+Mounts a GraphiGL ide on top of the local JACDAC graphql instance.
+
+## Setup
+
+```
+npm install
+```
+
+## Build
+
+```
+npm run watch
+```
--- service/server.ts
+++ service/server.ts
@@ -4,24 +4,28 @@ import requestDevice from "../src/nodeusb"
 import { createUSBBus } from "../src/usb"
 import { getSchema, rootValue } from "../src/graphql"
 
-const app = express();
-console.log(`creating jacdac bus`)
-const bus = createUSBBus({ requestDevice })
+async function start() {
+    const app = express();
+    console.log(`creating jacdac bus`)
+    const bus = createUSBBus({ requestDevice })
 
-console.log(`connecting bus`)
-bus.connectAsync()
+    console.log(`connecting bus`)
+    await bus.connectAsync()
 
-console.log(`setting up express`)
-app.use(
-    '/graphql',
-    graphqlHTTP({
-        schema: getSchema(),
-        rootValue: rootValue(bus),
-        graphiql: true,
-        pretty: true
-    }),
-);
+    console.log(`setting up express`)
+    app.use(
+        '/graphql',
+        graphqlHTTP({
+            schema: getSchema(),
+            rootValue: rootValue(bus).query,
+            graphiql: true,
+            pretty: true
+        }),
+    );
 
-console.log(`starting server at http://localhost:4000/graphql`)
-app.listen(4000);
-console.log(`closing`)
+    console.log(`starting server at http://localhost:4000/graphql`)
+    app.listen(4000);
+
+}
+
+start()
