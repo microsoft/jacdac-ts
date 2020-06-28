0d5adc68cabd47f8db2bc6cc085caaa32321d294
peli <pelikhan@users.noreply.github.com>
2020-06-28 08:47:13 -0700

    renamed file

--- service/server.ts
+++ service/server.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import graphqlHTTP from "express-graphql"
-import requestDevice from "../src/nodeusb"
+import requestDevice from "../src/nodewebusb"
 import { createUSBBus } from "../src/usb"
 import { getSchema, rootValue } from "../src/graphql"
 
