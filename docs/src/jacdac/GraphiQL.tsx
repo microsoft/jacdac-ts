6fd30ecffcf77921d5b7134568ae997bc79e3a92
peli <pelikhan@users.noreply.github.com>
2020-06-29 00:37:09 -0700

    adding example

--- docs/src/jacdac/GraphiQL.tsx
+++ docs/src/jacdac/GraphiQL.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useContext } from "react";
 import JacdacContext from "./Context"
 import { queryAsync, getSchema } from "../../../src/graphql"
@@ -26,28 +26,31 @@ export function useFetcher() {
     return fetcher;
 }
 
-const defaultQuery = `
-{
+const defaultQuery = `{
+    connected
     devices {
-        id
-        connected
-        services {
-            id
-            name
-        }
+        deviceId
     }
 }
 `
 
 const JacDaciQL = (props: { query?: string }) => {
     const fetcher = useFetcher();
+    const [q, setq] = useState(props.query)
     console.log(props)
     return <JacdacContext.Consumer>
         {({ connected, connecting, connectAsync, disconnectAsync }) => (
             <div style={{ width: "100%", height: "18rem" }}>
-                <GraphiQL fetcher={fetcher} query={props.query} defaultQuery={defaultQuery} defaultVariableEditorOpen={false}>
+                <GraphiQL
+                    fetcher={fetcher}
+                    query={q}
+                    defaultQuery={defaultQuery}
+                    defaultVariableEditorOpen={false}
+                    headerEditorEnabled={false}
+                    shouldPersistHeaders={false}>
                     <GraphiQL.Toolbar>
                         <GraphiQL.ToolbarButton label={connected ? "Disconnect" : connecting ? "..." : "Connect"} title="Connect or disconnect to JACDAC bus" onClick={connected ? disconnectAsync : connectAsync} />
+                        <GraphiQL.ToolbarButton label={"Reset"} title="Reset example" onClick={() => setq(props.query || defaultQuery)} />
                     </GraphiQL.Toolbar>
                     <GraphiQL.Logo>JacDacQL</GraphiQL.Logo>
                     <GraphiQL.Footer></GraphiQL.Footer>
