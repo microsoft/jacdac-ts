35a9af7f5361a32609f876d827d47f176afa5889
peli <pelikhan@users.noreply.github.com>
2020-06-29 18:21:58 -0700

    updated path

--- docs/src/components/layout.tsx
+++ docs/src/components/layout.tsx
@@ -5,10 +5,10 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React, { useState } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import JacdacProvider from "../jacdac/Provider"
+import JacdacProvider from "../../../src/react/Provider"
 
 import Header from "./header"
 // tslint:disable-next-line: no-import-side-effect
