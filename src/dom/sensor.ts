d704e08cba333ea9ab246e62e83a76a61f23a234
peli <pelikhan@users.noreply.github.com>
2020-07-01 09:15:13 -0700

    removing graphql subscriptions

--- src/dom/sensor.ts
+++ src/dom/sensor.ts
@@ -1,6 +1,7 @@
 import { Service } from "./service";
-import { REG_IS_STREAMING, CMD_CALIBRATE, REG_LOW_THRESHOLD } from "./constants";
+import { REG_IS_STREAMING, CMD_CALIBRATE, REG_LOW_THRESHOLD, REPORT_RECEIVE } from "./constants";
 import { bufferOfInt } from "./struct";
+import { DebouncedPoll, debouncedPollAsync } from "./utils";
 
 export function setStreamingAsync(service: Service, on: boolean) {
     const register = service.registerAt(REG_IS_STREAMING);
@@ -14,4 +15,26 @@ export function calibrateAsync(service: Service) {
 export function setThresholdAsync(service: Service, low: boolean, value: number) {
     const register = service.registerAt(low ? REG_LOW_THRESHOLD : REG_LOW_THRESHOLD)
     return register.sendSetAsync(bufferOfInt(value))
+}
+
+/**
+ * Starts a debounced task to check that a device is streaming. Return unsubscribe function.
+ * @param service 
+ */
+export function ensureStreamingSubscription(service: Service): () => void {
+    const register = service.registerAt(REG_IS_STREAMING)
+    const startStreaming = async () => {
+        console.log("start streaming")
+        await setStreamingAsync(this.register.service, true)
+    }
+    const ensureStreaming = debouncedPollAsync(startStreaming, 1000, 2000);
+    const observable = register.observe(REPORT_RECEIVE);
+    return observable.subscribe({
+        next: this.ensureStreaming.execute(),
+        error: e => ensureStreaming.stop(),
+        complete: () => {
+            ensureStreaming.stop()
+            setStreamingAsync(register.service, false)
+        }
+    }).unsubscribe
 }
--- src/graphql/graphql.ts
+++ src/graphql/graphql.ts
@@ -1,16 +1,7 @@
 import { graphql, buildSchema, parse, ExecutionResult, GraphQLSchema, subscribe as graphQLSubscribe, validate } from "graphql"
 import { Bus } from "../dom/bus";
-// tslint:disable-next-line: no-submodule-imports
-import { withFilter } from "graphql-subscriptions/dist/with-filter";
-import { Device } from "../dom/device";
-import { DEVICE_CONNECT, DEVICE_ANNOUNCE, DEVICE_DISCONNECT, REPORT_UPDATE, REPORT_RECEIVE } from "../dom/constants";
-import { serviceClass } from "../dom/pretty"
-import { EventEmitterPubSub, StreamingRegisterPubSub } from "./pubsub";
-import { Register } from "../dom/register";
-
 
 let _schema: GraphQLSchema = undefined;
-
 export function getSchema() {
     // lazy allocated schema
     if (!_schema) {
@@ -91,13 +82,8 @@ type Query {
     """
     device(deviceId: String): Device
 }
-type Subscription {
-    deviceChanged(deviceId: ID = "", serviceClass: Int = -1, serviceName: String = ""): Device!
-    reportReceived(deviceId: ID!, serviceNumber: Int = -1, serviceName: String = "", serviceClass: Int! = -1, address: Int!, updatesOnly: Boolean = false): Register!
-}
 schema {
     query: Query
-    subscription: Subscription
 }`);
     }
     return _schema
@@ -121,102 +107,11 @@ export function queryAsync(bus: Bus, source: string, options?: QueryOptions): Pr
     );
 }
 
-export function wrapIterator<T, U>(asyncIterator: () => AsyncIterator<T>, wrap: (T) => U): () => AsyncIterator<U> {
-    return () => {
-        const iterator = asyncIterator();
-        return ({
-            next() {
-                return iterator.next().then(({ value, done }) => {
-                    return { value: wrap(value), done };
-                });
-            },
-            return() {
-                return Promise.resolve({ value: undefined, done: true });
-            },
-            throw(error) {
-                return Promise.reject(error);
-            },
-            [Symbol.asyncIterator]() {
-                return this;
-            }
-        } as any)
-    }
-}
-
-export function rootValue(bus: Bus) {
-    return {
-        query: bus,
-        subscription: new Subscription(bus)
-    }
-}
-
-class Subscription {
-    constructor(public bus: Bus) {
-
-    }
-    deviceChanged(options?: { deviceId?: string, serviceClass?: number, serviceName?: string }) {
-        if (options?.serviceName && options?.serviceClass > -1)
-            throw Error("serviceClass and serviceName cannot be used together")
-        const deviceId = options?.deviceId;
-        let sc = serviceClass(options?.serviceName);
-        if (sc === undefined) sc = options?.serviceClass;
-        if (sc === undefined) sc = -1;
-
-        const pubSub = new EventEmitterPubSub(this.bus)
-        let subscribe = () => {
-            return pubSub.asyncIterator<Device>([
-                DEVICE_CONNECT,
-                DEVICE_ANNOUNCE,
-                DEVICE_DISCONNECT]);
-        }
-        if (deviceId || sc > -1)
-            subscribe = withFilter(subscribe, (payload) => {
-                return (!deviceId || payload?.deviceId == deviceId)
-                    && (sc < 0 || payload?.hasService(sc));
-            });
-
-        const wrapped = wrapIterator<Device, { deviceChanged: Device }>(subscribe, deviceChanged => { return { deviceChanged } });
-        return toAsyncIterable(wrapped);
-    }
-    reportReceived(options: { deviceId: string, serviceName?: string, serviceClass?: number, serviceNumber?: number, address: number, updatesOnly?: boolean }) {
-        const device = this.bus.device(options.deviceId);
-        if (!device)
-            throw new Error("device not found");
-        const service = device.services(options)[0];
-        if (!service)
-            throw new Error("service not found")
-        const register = service.register(options)
-        if (!register)
-            throw new Error("register not found")
-
-        const pubSub = new StreamingRegisterPubSub(register)
-        let subscribe = () => {
-            const events = [options.updatesOnly ? REPORT_UPDATE : REPORT_RECEIVE]
-            return pubSub.asyncIterator<Register>(events);
-        }
-        const wrapped = wrapIterator<Register, { reportReceived: Register }>(subscribe, reportReceived => { return { reportReceived } });
-        return toAsyncIterable(wrapped);
-    }
-}
-
-export async function subscribeAsync(bus: Bus, query: string) {
-    const subscription = await graphQLSubscribe({
-        schema: getSchema(),
-        document: parse(query),
-        rootValue: new Subscription(bus)
-    });
-    return subscription;
-}
-
 export class Query {
     constructor(public readonly source: string) { }
 
     queryAsync(bus: Bus) {
         return queryAsync(bus, this.source);
-    }
-
-    subscribeAsync(bus: Bus) {
-        return subscribeAsync(bus, this.source);
     }
 }
 
@@ -239,8 +134,4 @@ export function jdql(strings): Query {
         query = jdql[source] = new Query(source)
     }
     return query;
-}
-
-export function toAsyncIterable<T>(iterator: () => AsyncIterator<T>) {
-    return { [Symbol.asyncIterator]: iterator }
 }
