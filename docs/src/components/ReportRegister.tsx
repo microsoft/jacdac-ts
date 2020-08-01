881ca76becc8251e2fbdf6a2d11c8b25797f718a
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-07-31 23:27:30 -0700

    Report react (#28)
    * update rendering of data
    
    * more rendering
    
    * Merge remote-tracking branch 'origin/master' into reportreact
    
    * fixed rendering
    
    Co-authored-by: Peli de Halleux <peli@Pelis-MacBook-Pro.local>

--- docs/src/components/ReportRegister.tsx
+++ docs/src/components/ReportRegister.tsx
@@ -1,24 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { JDRegister } from "../../../src/dom/register";
-import { Slider, Typography } from "@material-ui/core";
+import { Slider, Typography, Switch } from "@material-ui/core";
 import { REPORT_UPDATE } from "../../../src/dom/constants";
+import { DecodedMember } from "../../../src/dom/pretty";
+
+function MemberInput(props: { member: DecodedMember, labelledby: string }) {
+    const { member, labelledby } = props;
+    const { info } = member;
+    console.log(member)
+    if (info.type == "bool")
+        return <Switch checked={member.value} />
+    else if (member.numValue !== undefined && info.storage > 0) {
+        return <Slider value={member.numValue}
+            aria-labelledby={labelledby}
+            min={0} max={1 << (8 * member.size)}
+        />
+    }
+
+    return <Typography variant="h4">{member.humanValue}</Typography>
+}
+
+function Decoded(props: { member: DecodedMember }) {
+    const { member } = props;
+    const { info } = member;
+    return <div>
+        {info.name !== "_" && <Typography id="slider" gutterBottom>
+            {info.name}
+        </Typography>}
+        <MemberInput member={member} labelledby={"slider"} />
+    </div>
+}
 
 export default function ReportRegister(props: { register: JDRegister }) {
     const { register } = props;
-    const spec = register.specification;
-    const [value, setValue] = useState(register.intValue || 0)
+    const [decoded, setDecoded] = useState(register.decode())
 
     useEffect(() => register.subscribe(REPORT_UPDATE, () => {
-        const newValue = register.intValue;
-        setValue(newValue)
+        setDecoded(register.decode())
     }))
 
-    // todo: use decoded data
-    const max = 1 << 16
     return <div>
-        <Typography id="slider" gutterBottom>
-            {spec.name}
-        </Typography>
-        <Slider disabled value={value} aria-labelledby="slider" min={0} max={max} />
+        {!!decoded && <Typography gutterBottom>
+            {decoded.info.name}
+        </Typography>}
+        {decoded && decoded.decoded.map(member =>
+            <Decoded member={member} />)}
     </div>
 }
