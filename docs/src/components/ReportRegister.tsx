373aaa156101a5e3f5191c5f93a4bcd42b0b2326
Peli de Halleux <peli@Pelis-MacBook-Pro.local>
2020-07-31 22:14:15 -0700

    more rendering

--- docs/src/components/ReportRegister.tsx
+++ docs/src/components/ReportRegister.tsx
@@ -2,6 +2,27 @@ import React, { useState, useEffect } from "react";
 import { JDRegister } from "../../../src/dom/register";
 import { Slider, Typography, Switch } from "@material-ui/core";
 import { REPORT_UPDATE } from "../../../src/dom/constants";
+import { DecodedMember } from "../../../src/dom/pretty";
+
+function MemberInput(props: { member: DecodedMember, labelledby: string }) {
+    const { member, labelledby } = props;
+    if (member.info.type == "bool")
+        return <Switch value={member.value} />
+    else if (member.scaledValue !== undefined)
+        return <Slider disabled value={member.scaledValue} aria-labelledby={labelledby} min={0} max={1} />
+    else
+        return <span>{member.humanValue}</span>
+}
+
+function Decoded(props: { member: DecodedMember }) {
+    const { member } = props;
+    return <div>
+        <Typography id="slider" gutterBottom>
+            {member.description}
+        </Typography>
+        <MemberInput member={member} labelledby={"slider"} />
+    </div>
+}
 
 export default function ReportRegister(props: { register: JDRegister }) {
     const { register } = props;
@@ -13,16 +34,7 @@ export default function ReportRegister(props: { register: JDRegister }) {
     }))
 
     return <div>
-        <Typography id="slider" gutterBottom>
-            {spec.name}
-        </Typography>
-        {decoded && decoded.decoded.map(member => {
-            if (member.info.type == "bool")
-                return <Switch value={member.value} />
-            else if (member.scaledValue !== undefined)
-                return <Slider disabled value={member.scaledValue} aria-labelledby="slider" min={0} max={1} />
-            else
-               return member.humanValue
-        })}
+        {decoded && decoded.decoded.map(member =>
+            <Decoded member={member} />)}
     </div>
 }
