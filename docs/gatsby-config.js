2e61949552390fb32edd2713f85841f78eec1217
peli <pelikhan@users.noreply.github.com>
2020-08-14 03:13:36 -0700

    adding robots.txt

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -91,6 +91,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    "gatsby-plugin-robots-txt",
     "gatsby-plugin-meta-redirect",
   ],
 }
