9014d958cd199b3e7b73b56fa18c1d3226fbe8bb
peli <pelikhan@users.noreply.github.com>
2020-08-15 05:47:42 -0700

    handle empty chart

--- docs/src/components/Trend.tsx
+++ docs/src/components/Trend.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { DataSet } from "./DataSet";
+import { DataSet, Example } from "./DataSet";
 import { unique } from "../../../src/dom/utils";
 import { Paper, makeStyles, createStyles } from "@material-ui/core";
 import clsx from 'clsx';
@@ -23,30 +23,24 @@ export interface TrendProps {
     gradient?: boolean;
 }
 
-function UnitTrend(props: {
+function UnitTrendChart(props: {
+    data: Example[],
+    useGradient: boolean,
     unit: string,
     horizon?: number,
-    width?: number,
-    height?: number,
+    vpw: number,
+    vph: number,
 } & TrendProps) {
-    const { dataSet, unit, horizon, width, height, dot, gradient } = props;
-    const { rows } = dataSet;
-    const classes = useStyles()
+    const { dataSet, useGradient, data, unit, horizon, vpw, vph, dot, gradient } = props;
+
     const shape = unit == "frac" ? "step" : "line"
     const symmetric = unit == "g" ? true : false
 
-    if (rows.length < 2)
-        return <></>
-
-    const vpw = width || 80;
-    const vph = height || 15;
     const indexes = dataSet.units
         .map((u, index) => (u || "frac") === unit ? index : undefined)
         .filter(index => index !== undefined)
     const headers = indexes.map(i => dataSet.headers[i])
     const colors = indexes.map(i => dataSet.colors[i])
-    const data = rows.slice(-horizon)
-    const useGradient = gradient || data.length < rows.length
     const times = data.map(ex => ex.timestamp)
     const maxt = Math.max.apply(null, times);
     const mint = Math.min.apply(null, times);
@@ -87,42 +81,61 @@ function UnitTrend(props: {
     const lastRow = data[data.length - 1]
 
     return (
+        <React.Fragment>
+            {useGradient && <defs>
+                <linearGradient key={`gradaxis`} id={`gradientaxis`} x1="0%" y1="0%" x2="100%" y2="0%">
+                    <stop offset="0%" stopOpacity="0" stopColor={axisColor} />
+                    <stop offset="5%" stopOpacity="0" stopColor={axisColor} />
+                    <stop offset="40%" stopOpacity="1" stopColor={axisColor} />
+                    <stop offset="100%" stopOpacity="1" stopColor={axisColor} />
+                </linearGradient>
+                {indexes.map((index, i) =>
+                    <linearGradient key={`grad${i}`} id={`gradient${index}`} x1="0%" y1="0%" x2="100%" y2="0%">
+                        <stop offset="0%" stopOpacity="0" stopColor={colors[i]} />
+                        <stop offset="5%" stopOpacity="0" stopColor={colors[i]} />
+                        <stop offset="40%" stopOpacity="1" stopColor={colors[i]} />
+                        <stop offset="100%" stopOpacity="1" stopColor={colors[i]} />
+                    </linearGradient>)}
+            </defs>}
+            <g transform={`translate(${toffset}, ${vph - margin})`}>
+                {opposite && <line x1={x(mint)} x2={x(maxt)} y1={y(0)} y2={y(0)} strokeWidth={axisWidth} stroke={useGradient ? `url(#gradientaxis)` : axisColor} />}
+                {indexes.map((index, i) => {
+                    const color = colors[i]
+                    const path = shape == "step"
+                        ? data.map((row, ri) => ri == 0 ? `M ${x(row.timestamp)} ${y(row.data[index])}` : `H ${x(row.timestamp)} V ${y(row.data[index])}`).join(' ')
+                        : data.map((row, ri) => `${ri == 0 ? `M` : `L`} ${x(row.timestamp)} ${y(row.data[index])}`).join(' ');
+                    const header = headers[i]
+                    return <g key={`line${index}`}>
+                        <path d={path} fill="none" stroke={useGradient ? `url(#gradient${index})` : color} strokeWidth={strokeWidth} strokeLinejoin="round" />
+                        {dot && <circle cx={x(lastRow.timestamp)} cy={y(lastRow.data[index])} r={pointRadius} fill={color} />}
+                    </g>
+                })}
+            </g>
+        </React.Fragment>
+    )
+}
+
+function UnitTrend(props: {
+    unit: string,
+    horizon?: number,
+    width?: number,
+    height?: number,
+} & TrendProps) {
+    const { dataSet, horizon, width, height, dot, gradient } = props;
+    const { rows } = dataSet;
+    const classes = useStyles()
+    const vpw = width || 80;
+    const vph = height || 15;
+    const data = rows.slice(-horizon)
+    const useGradient = gradient || data.length < rows.length
+    return (
         <Paper className={classes.graph} square>
             <svg viewBox={`0 0 ${vpw} ${vph}`}>
-                {useGradient && <defs>
-                    <linearGradient key={`gradaxis`} id={`gradientaxis`} x1="0%" y1="0%" x2="100%" y2="0%">
-                            <stop offset="0%" stopOpacity="0" stopColor={axisColor} />
-                            <stop offset="5%" stopOpacity="0" stopColor={axisColor} />
-                            <stop offset="40%" stopOpacity="1" stopColor={axisColor} />
-                            <stop offset="100%" stopOpacity="1" stopColor={axisColor} />
-                        </linearGradient>
-                    {indexes.map((index, i) =>
-                        <linearGradient key={`grad${i}`} id={`gradient${index}`} x1="0%" y1="0%" x2="100%" y2="0%">
-                            <stop offset="0%" stopOpacity="0" stopColor={colors[i]} />
-                            <stop offset="5%" stopOpacity="0" stopColor={colors[i]} />
-                            <stop offset="40%" stopOpacity="1" stopColor={colors[i]} />
-                            <stop offset="100%" stopOpacity="1" stopColor={colors[i]} />
-                        </linearGradient>)}
-                </defs>}
-                <g transform={`translate(${toffset}, ${vph - margin})`}>
-                    {opposite && <line x1={x(mint)} x2={x(maxt)} y1={y(0)} y2={y(0)} strokeWidth={axisWidth} stroke={useGradient ? `url(#gradientaxis)` : axisColor} />}
-                    {indexes.map((index, i) => {
-                        const color = colors[i]
-                        const path = shape == "step"
-                            ? data.map((row, ri) => ri == 0 ? `M ${x(row.timestamp)} ${y(row.data[index])}` : `H ${x(row.timestamp)} V ${y(row.data[index])}`).join(' ')
-                            : data.map((row, ri) => `${ri == 0 ? `M` : `L`} ${x(row.timestamp)} ${y(row.data[index])}`).join(' ');
-                        const header = headers[i]
-                        return <g key={`line${index}`}>
-                            <path d={path} fill="none" stroke={useGradient ? `url(#gradient${index})` : color} strokeWidth={strokeWidth} strokeLinejoin="round" />
-                            {dot && <circle cx={x(lastRow.timestamp)} cy={y(lastRow.data[index])} r={pointRadius} fill={color} />}
-                        </g>
-                    })}
-                </g>
+                {data.length > 1 && <UnitTrendChart data={data} useGradient={useGradient} vpw={vpw} vph={vph} {...props} />}
             </svg>
         </Paper>
     )
 }
-
 
 export default function Trend(props: {
     horizon?: number,
@@ -131,11 +144,7 @@ export default function Trend(props: {
     mini?: boolean
 } & TrendProps) {
     const { dataSet, mini } = props;
-    const { rows } = dataSet;
     const classes = useStyles()
-
-    if (!rows.length)
-        return <></>
 
     const units = unique(dataSet.units.map(unit => unit || "frac"))
     return <div className={clsx(classes.root, mini && classes.mini)}>
