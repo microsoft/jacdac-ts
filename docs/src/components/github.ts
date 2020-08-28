6ebb1199141334936bf76e15bffc7ecf04d3bf9f
peli <pelikhan@users.noreply.github.com>
2020-08-27 23:23:24 -0700

    started on github repo list

---
+++ docs/src/components/github.ts
@@ -0,0 +1,35 @@
+const ROOT = "https://api.github.com/"
+
+interface GithubRelease {
+    url: string,
+    html_url: string,
+    tag_name: string,
+    name: string,
+    body: string,
+    assets: {
+        url: string,
+        browser_download_url: string,
+        name: string
+    }[]
+}
+
+export async function fetchLatestRelease(slug: string) {
+    try {
+        const uri = `${ROOT}/repos/${slug}/releases/latest`;
+        const resp = await fetch(uri)
+        console.log(resp)
+        switch (resp.status) {
+            case 200:
+            case 204:
+                const release: GithubRelease = await resp.json()
+                return release;
+            case 403:
+                // throttled
+                break;
+        }
+        throw new Error(`unknown status code ${resp.status}`)
+    } catch (e) {
+        console.log(e)
+        return undefined;
+    }
+}
