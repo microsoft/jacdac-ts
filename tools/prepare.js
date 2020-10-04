fe6c9b457981a1db588f9c0da66a73cb72878767
Peli de Halleux <peli@WIN-L9NN0K8DNUE.redmond.corp.microsoft.com>
2020-10-04 22:20:10 +0200

    fix path in prepaprer

--- tools/prepare.js
+++ tools/prepare.js
@@ -14,7 +14,7 @@ async function patchDocs() {
   for (const suffix of ["", "-graphql", "-react"]) {
     const fn = `jacdac${suffix}.umd.js`
     await patch({
-      files: './docs/static/tools/*.html',
+      files: './docs/static/tools/js/*.html',
       from: `/dist/${fn}`,
       to: `https://cdn.jsdelivr.net/npm/jacdac-ts@${version}/dist/${fn}`
     })
