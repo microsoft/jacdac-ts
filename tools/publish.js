6888fa06f9239944e2faabf76e84db09ee0b743b
Peli de Halleux <peli@WIN-L9NN0K8DNUE.redmond.corp.microsoft.com>
2020-10-04 23:51:00 +0200

    path: more logging

--- tools/publish.js
+++ tools/publish.js
@@ -4,10 +4,15 @@ const ghpages = require("gh-pages")
  * This configuration will avoid logging the GH_TOKEN if there is an error.
  */
 assert(!!process.env.GITHUB_TOKEN, "missing env GITHUB_TOKEN")
-if (!!process.env.GITHUB_TOKEN) {
-    ghpages.publish('docs/public', {
-        repo: 'https://' + process.env.GITHUB_TOKEN + '@github.com/microsoft/jacdac-ts.git',
-        silent: true,
+console.log(`uploading to gh-pages`)
+ghpages.publish('docs/public', {
+    repo: 'https://' + process.env.GITHUB_TOKEN + '@github.com/microsoft/jacdac-ts.git',
+    silent: true,
 
-    });
-}
+}, (err) => {
+    console.log(`upload done`)
+    if (err) {
+        console.error(err)
+        throw err;
+    }
+});
