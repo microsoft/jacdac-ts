122952f5bc6f992ca76d332b9d19f3140bd5b8db
Peli de Halleux <peli@WIN-L9NN0K8DNUE.redmond.corp.microsoft.com>
2020-10-07 06:25:48 +0200

    closeable alerts

--- docs/src/components/WebUSBAlert.tsx
+++ docs/src/components/WebUSBAlert.tsx
@@ -1,6 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 import { isWebUSBSupported } from "../../../src/dom/usb"
-import { NoSsr } from '@material-ui/core';
+import { Collapse, NoSsr } from '@material-ui/core';
 // tslint:disable-next-line: no-submodule-imports
 import Alert from "./Alert";
 import { Link } from "gatsby-theme-material-ui";
@@ -8,7 +8,7 @@ import { Link } from "gatsby-theme-material-ui";
 function NoSsrAlert() {
     const supported = isWebUSBSupported()
     return <>
-        {!supported && <Alert severity="info">Use a browser that supports <Link to="https://caniuse.com/#feat=webusb">WebUSB</Link> to connect to JACDAC devices.</Alert>}
+        {!supported && <Alert closeable={true} severity="info">Use a browser that supports <Link to="https://caniuse.com/#feat=webusb">WebUSB</Link> to connect to JACDAC devices.</Alert>}
     </>
 }
 
