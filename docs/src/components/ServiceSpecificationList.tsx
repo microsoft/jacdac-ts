ffed1481b5302ac9084253c22a629c5b0450d536
peli <pelikhan@users.noreply.github.com>
2020-10-09 06:17:31 -0700

    more cleanup

--- docs/src/components/ServiceSpecificationList.tsx
+++ docs/src/components/ServiceSpecificationList.tsx
@@ -2,14 +2,14 @@ import { Grid } from "@material-ui/core";
 import React from "react";
 import { serviceSpecifications } from "../../../src/dom/spec";
 import ServiceSpecificationCard from "./ServiceSpecificationCard"
+import useGridBreakpoints from "./useGridBreakpoints";
 
 export default function ServiceSpecificationList(props: {}) {
-
+    const gridBreakpoints = useGridBreakpoints();
     const specs = serviceSpecifications();
     return <Grid container spacing={2}>
-        {specs.map(node => <Grid xs={6} sm={4} lg={3} item key={node.id}>
+        {specs.map(node => <Grid {...gridBreakpoints} item key={node.classIdentifier}>
             <ServiceSpecificationCard serviceClass={node.classIdentifier} />
         </Grid>)}
     </Grid>
-
 }
