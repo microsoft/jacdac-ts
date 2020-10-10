0cccf862b087deb8e82532dee6e57de0f9ff16be
Peli de Halleux <peli@DESKTOP-PMVKPCB.corp.microsoft.com>
2020-10-10 17:15:07 +0200

    css fixes for print

--- docs/src/components/PrintButton.tsx
+++ docs/src/components/PrintButton.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 // tslint:disable-next-line: match-default-export-name no-submodule-imports
 import PrintIcon from '@material-ui/icons/Print';
 import { IconButton, IconButtonProps, NoSsr, Tooltip } from "@material-ui/core";
+import DarkModeContext from "./DarkModeContext";
+import { useSnackbar } from "notistack";
+import AppContext from "./AppContext";
+import { delay } from "../../../src/dom/utils";
 
 export default function PrintButton(props: { component?: string } & IconButtonProps) {
     const { disabled, ...others } = props;
-    const onPrint = () => {
-        try {
-            window.print();
-        } catch (e) {
+    const { setError } = useContext(AppContext);
 
+    const onPrint = () => {
+        console.log(`printing...`)
+        try {
+            if (typeof window !== "undefined")
+                window.print();
+        } catch (e) {
+            console.log('print failed')
+            setError(e)
         }
     }
     return <NoSsr>
