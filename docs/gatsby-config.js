f1f9e2e5f18f586d026791b043243b990360f29f
Peter Lisy <pelisy@microsoft.com>
2020-10-11 19:32:43 +0000

    Enable running from a codespace.

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -74,6 +74,7 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        crossOrigin: `use-credentials`,
       },
     },
     {
