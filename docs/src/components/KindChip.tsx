735abf8d293cfdbe4b4eb5782a0fcc3accf01dff
Peli de Halleux <peli@MININT-V3FV23H.fareast.corp.microsoft.com>
2020-10-19 22:52:26 +0200

    auto-filter of kindchip

--- docs/src/components/KindChip.tsx
+++ docs/src/components/KindChip.tsx
@@ -1,9 +1,22 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Chip } from "@material-ui/core";
 import KindIcon, { kindName } from "./KindIcon";
+import PacketsContext from "./PacketsContext"
+import AppContext, { DrawerType } from "./AppContext"
 
 export default function KindChip(props: { kind: string, className?: string }) {
     const { kind, className } = props;
+    const { filter, setFilter } = useContext(PacketsContext);
+    const { setDrawerType }  = useContext(AppContext)
     const icon = KindIcon({ kind })
-    return <Chip className={className} size="small" label={kindName(kind)} icon={icon} />
+    const chipFilter = `kind:${kind}`
+    const filtered = filter && filter.indexOf(chipFilter) > 1
+    const handleClick = () => {
+        if (filtered)
+            setFilter(filter?.replace(chipFilter, ''))
+        else
+            setFilter(filter + ' ' + chipFilter)
+        setDrawerType(DrawerType.Packets)
+    }
+    return <Chip onClick={handleClick} className={className} size="small" label={kindName(kind)} icon={icon} />
 }
