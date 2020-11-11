287a53067ba7ad98b3978e1c78c1e8491ab44823
Peli de Halleux <peli@Pelis-MacBook-Pro.local>
2020-11-11 22:48:02 +0100

    restore download page

--- docs/src/components/github.ts
+++ docs/src/components/github.ts
@@ -42,6 +42,27 @@ export interface GitHubApiOptions {
     ignoreThrottled?: boolean;
 }
 
+export async function fetchLatestRelease(slug: string, options?: GitHubApiOptions): Promise<GithubRelease> {
+    const uri = `${ROOT}repos/${normalizeSlug(slug)}/releases/latest`;
+    const resp = await fetch(uri)
+    //    console.log(resp)
+    switch (resp.status) {
+        case 200:
+        case 204:
+            const release: GithubRelease = await resp.json()
+            return release;
+        case 404:
+            // unknow repo or no access
+            return undefined;
+        case 403:
+            // throttled
+            if (options?.ignoreThrottled)
+                return undefined;
+            throw new Error("Too many calls to GitHub, try again later");
+    }
+    throw new Error(`unknown status code ${resp.status}`)
+}
+
 export async function fetchReleaseBinary(slug: string, tag: string): Promise<Blob> {
     const downloadUrl = `https://raw.githubusercontent.com/${normalizeSlug(slug)}/${tag}/dist/firmware.uf2`
     const req = await fetch(downloadUrl, { headers: { "Accept": "application/octet-stream" } })
