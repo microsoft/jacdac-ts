020573e8ca45b57fb87873add8484f02fe115c10
peli <pelikhan@users.noreply.github.com>
2020-11-13 01:30:05 -0800

    better node.js support

--- src/node/nodewebusb.ts
+++ src/node/nodewebusb.ts
@@ -3,6 +3,7 @@ import { HF2_DEVICE_MAJOR } from "../jdom/hf2";
 const USB = require('webusb').USB;
 
 const devicesFound = async devices => {
+    console.log(devices)
     for (const device of devices) {
         if (device.deviceVersionMajor == HF2_DEVICE_MAJOR) {
             for (const iface of device.configuration.interfaces) {
@@ -19,13 +20,12 @@ const devicesFound = async devices => {
 
 const usb = new USB({ devicesFound })
 
-/**
- * Node.JS webusb bindings. Make sure to add "webusb" to your package.json config.
- * @param options 
- */
 export async function requestDevice(options: USBDeviceRequestOptions): Promise<USBDevice> {
+    console.log(`requesting device...`)
     const device = await usb.requestDevice(options);
     return device;
 }
-
-export default requestDevice;
+export async function getDevices(): Promise<USBDevice[]> {
+    const devices = await usb.getDevices();
+    return devices;
+}
