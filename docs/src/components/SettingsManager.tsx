1f0a8b86480176a0fe34ebfb3e4a4950d66b4885
peli <pelikhan@users.noreply.github.com>
2020-12-01 09:28:08 -0800

    refactor settings manager

--- docs/src/components/SettingsManager.tsx
+++ docs/src/components/SettingsManager.tsx
@@ -6,19 +6,17 @@ import useChange from "../jacdac/useChange"
 import { BusState } from "../../../src/jdom/bus"
 import ConnectAlert from "./ConnectAlert"
 import Alert from "./Alert"
-import useGridBreakpoints from "./useGridBreakpoints"
 
 export default function SettingsManager() {
     const { bus, connectionState } = useContext<JDContextProps>(JACDACContext)
 
     const services = useChange(bus, b => b.services({ serviceClass: SRV_SETTINGS_STORAGE }));
-    const gridBreakpoints = useGridBreakpoints();
 
     return <>
         {<ConnectAlert serviceClass={SRV_SETTINGS_STORAGE} />}
         {!services.length && connectionState == BusState.Connected && <Alert severity="info">We could not find any device with the settings storage service on the bus!</Alert>}
         <Grid container spacing={2}>
-            {services.map(service => <Grid key={service.id} item {...gridBreakpoints}>
+            {services.map(service => <Grid key={service.id} item xs={12}>
             </Grid>)}
         </Grid>
     </>
