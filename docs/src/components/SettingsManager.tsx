4ac411e6551254a88a393f9f9c962ad69c4de3a7
peli <pelikhan@users.noreply.github.com>
2020-12-01 08:34:03 -0800

    more boiler place

--- docs/src/components/SettingsManager.tsx
+++ docs/src/components/SettingsManager.tsx
@@ -1,6 +1,25 @@
+import React, { useContext } from "react"
+import { Grid } from "@material-ui/core"
+import JACDACContext, { JDContextProps } from "../../../src/react/Context"
+import { SRV_SETTINGS_STORAGE } from "../../../src/jdom/constants"
+import useChange from "../jacdac/useChange"
+import { BusState } from "../../../src/jdom/bus"
+import ConnectAlert from "./ConnectAlert"
+import Alert from "./Alert"
+import useGridBreakpoints from "./useGridBreakpoints"
 
+export default function SettingsManager() {
+    const { bus, connectionState } = useContext<JDContextProps>(JACDACContext)
 
-export default function SettingsManager(props: {}) {
+    const services = useChange(bus, b => b.services({ serviceClass: SRV_SETTINGS_STORAGE }));
+    const gridBreakpoints = useGridBreakpoints();
+
     return <>
-        </>
+        {<ConnectAlert serviceClass={SRV_SETTINGS_STORAGE} />}
+        {!services.length && connectionState == BusState.Connected && <Alert severity="info">We could not find any device with the settings storage service on the bus!</Alert>}
+        <Grid container spacing={2}>
+            {services.map(service => <Grid key={service.id} item {...gridBreakpoints}>
+            </Grid>)}
+        </Grid>
+    </>
 }
