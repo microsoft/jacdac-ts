d9202ce8e11085282dc2c499377382f8af541580
Peli de Halleux <peli@Pelis-MacBook-Pro.local>
2020-12-03 22:49:33 +0100

    adding little test suite

--- docs/src/components/ProtocolTest.tsx
+++ docs/src/components/ProtocolTest.tsx
@@ -1,17 +1,7 @@
-import { Card, CardActions, CardContent, CardHeader, Grid, TextField, Typography } from "@material-ui/core";
-import React, { ChangeEvent, useContext, useMemo, useState } from "react";
-import { clone, SMap, uniqueName } from "../../../src/jdom/utils";
-import useLocalStorage from "./useLocalStorage";
+import { Card, CardActions, CardContent, CardHeader, Grid } from "@material-ui/core";
+import React, { useContext } from "react";
+import { cryptoRandomUint32, toHex } from "../../../src/jdom/utils";
 // tslint:disable-next-line: no-submodule-imports match-default-export-name
-import DeleteIcon from '@material-ui/icons/Delete';
-import { serviceSpecificationFromClassIdentifier } from "../../../src/jdom/spec";
-import AddServiceIconButton from "./AddServiceIconButton";
-import ServiceSpecificationSelect from "./ServiceSpecificationSelect"
-import { DTDL_CONTEXT, escapeName, serviceSpecificationToComponent } from "../../../src/azure-iot/dtdl"
-import IconButtonWithTooltip from "./IconButtonWithTooltip";
-import Snippet from "./Snippet";
-import PaperBox from "./PaperBox";
-import Alert from "./Alert"
 import JACDACContext, { JDContextProps } from "../../../src/react/Context";
 import { SRV_PROTOCOL_TEST } from "../../../src/jdom/constants";
 import useChange from "../jacdac/useChange"
@@ -20,15 +10,93 @@ import { JDRegister } from "../../../src/jdom/register";
 import CmdButton from "./CmdButton"
 import RegisterInput from "./RegisterInput";
 import ConnectAlert from "./ConnectAlert";
+import { JDField } from "../../../src/jdom/field";
+import { jdpack } from "../../../src/jdom/pack";
+
+
+function pick(...values: number[]) {
+    return values.find(x => x !== undefined);
+}
+
+function randomRange(min: number, max: number) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomFieldPayload(field: JDField) {
+    const { specification } = field;
+    let r: any = undefined;
+    if (specification.isSimpleType) {
+        switch (specification.type) {
+            case "i8":
+            case "i16":
+            case "i32":
+            case "u8":
+            case "u16":
+            case "u32": {
+                const unsigned = specification.type[0] === "u";
+                const n = parseInt(specification.type.slice(1));
+                const min = pick(specification.typicalMin, specification.absoluteMin, unsigned ? 0 : -((1 << (n - 1)) - 1));
+                const max = pick(specification.typicalMax, specification.absoluteMax, unsigned ? (1 << n) - 1 : (1 << (n - 1)) - 1);
+                r = randomRange(min, max);
+                break;
+            }
+            case "bytes": {
+                // maxBytes?
+                const a = cryptoRandomUint32(randomRange(1, 3));
+                r = new Uint8Array(a.buffer);
+                break;
+            }
+            case "string":
+            case "string0": {
+                const ch_a = "a".charCodeAt(0);
+                const ch_z = "z".charCodeAt(0)
+                const n = randomRange(4, 10);
+                let s = ""
+                for (let i = 0; i < n; ++i) {
+                    s += String.fromCharCode(randomRange(ch_a, ch_z));
+                }
+                r = s;
+                break;
+            }
+        }
+    } else {
+        console.log('complex type not supported')
+    }
+
+    return r;
+}
+
+function randomPayload(fields: JDField[]) {
+    return fields.map(randomFieldPayload);
+}
 
 function RegisterProtocolTest(props: { rw: JDRegister, ro: JDRegister }) {
     const { rw, ro } = props;
-    const name = rw.specification.name.replace(/^rw_/,"")
-
+    const { specification, fields } = rw;
+    const name = specification.name.replace(/^rw_/, "")
+    
     const handleClick = async () => {
-        // generate random data
-        // set register
-    };
+        const payload = randomPayload(fields);
+        if (!payload) throw "data layout not supported"
+        if (!specification.packFormat) throw "format unknown"
+
+        const data = jdpack(specification.packFormat, payload);
+        const xdata = toHex(data);
+        console.log({ payload, data: xdata })
+        // send over packet
+        await rw.sendSetAsync(data, true);
+        // read packet
+        await rw.sendGetAsync();
+        // check read
+        const rwData = toHex(rw.data)
+        if (rwData !== xdata)
+            throw `expected rw ${xdata}, got ${rwData}`
+        // check ro
+        await ro.sendGetAsync();
+        const roData = toHex(rw.data)
+        if (roData !== xdata)
+            throw `expected ro ${xdata}, got ${roData}`
+    }
 
     return <Card>
         <CardHeader title={name} />
