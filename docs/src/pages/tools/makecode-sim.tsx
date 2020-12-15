d962bc13e54f8ab2bad3678f295ce66025ca8d67
pelikhan <jhalleux@microsoft.com>
2020-12-15 09:25:51 +0100

    adding grid

--- docs/src/pages/tools/makecode-sim.tsx
+++ docs/src/pages/tools/makecode-sim.tsx
@@ -1,7 +1,52 @@
-import React from "react"
-import { createMuiTheme, responsiveFontSizes } from "@material-ui/core";
+import React, { useContext, useState } from "react"
+import { Card, CardContent, CardHeader, Collapse, createMuiTheme, Grid, GridSize, responsiveFontSizes, Typography } from "@material-ui/core";
 import ThemedLayout from "../../components/ThemedLayout";
 import JDomTreeView from "../../components/JDomTreeView";
+import { JDService } from "../../../../src/jdom/service";
+import useDeviceName from "../../components/useDeviceName";
+import { JDRegister } from "../../../../src/jdom/register";
+import { useRegisterHumanValue } from "../../jacdac/useRegisterValue";
+import useChange from "../../jacdac/useChange";
+import JACDACContext, { JDContextProps } from "../../../../src/react/Context";
+import { isSensor } from "../../../../src/jdom/spec";
+
+const ReadingItem = (props: { register: JDRegister, expanded: boolean, onExpanded: () => void }) => {
+    const { register, expanded, onExpanded } = props;
+    const { service, specification } = register;
+    const deviceName = useDeviceName(service.device);
+    const breakPoints: { xs: GridSize } = expanded ? { xs: 12 }
+        : { xs: 6 };
+    const humanValue = useRegisterHumanValue(register)
+
+    return <Grid item {...breakPoints}>
+        <Card onClick={!expanded && onExpanded}>
+            <CardHeader title={expanded ? service.name : humanValue}
+                subheader={deviceName}
+            />
+            <CardContent>
+                <Collapse in={expanded}>
+                    <Typography variant="h1">{humanValue}</Typography>
+                </Collapse>
+            </CardContent>
+        </Card>
+    </Grid>
+}
+
+const ReadingGrid = () => {
+    const { bus } = useContext<JDContextProps>(JACDACContext)
+    const [expanded, setExpanded] = useState<any>(undefined)
+    const readingRegisters = useChange(bus, bus =>
+        bus.devices().map(device => device
+            .services().find(srv => isSensor(srv.specification))
+            ?.readingRegister
+        ).filter(reg => !!reg))
+    const handleSetExpanded = (i: any) => () => setExpanded(i)
+    return <Grid container spacing={1}>
+        {readingRegisters.map(reg => <ReadingItem key={reg.id} register={reg}
+            expanded={reg === expanded}
+            onExpanded={handleSetExpanded(reg)} />)}
+    </Grid>
+}
 
 export default function Page() {
     const rawTheme = createMuiTheme({
@@ -16,6 +61,7 @@ export default function Page() {
     })
     const theme = responsiveFontSizes(rawTheme);
     return <ThemedLayout theme={theme}>
-        <JDomTreeView dashboard={true}/>
+        <JDomTreeView dashboard={true} />
+        <ReadingGrid />
     </ThemedLayout>
 }
