f4441e39e913512b034d17a5e7c14fd468b90950
pelikhan <jhalleux@microsoft.com>
2020-12-19 23:33:11 +0100

    ssr fix

--- docs/src/components/useLocalStorage.ts
+++ docs/src/components/useLocalStorage.ts
@@ -6,7 +6,7 @@ export default function useLocalStorage<T>(key: string, initialValue: T) {
     const [storedValue, setStoredValue] = useState<T>(() => {
         try {
             // Get from local storage by key
-            const item = window.localStorage.getItem("jacdac:" + key);
+            const item = typeof window !== "undefined" && window.localStorage.getItem("jacdac:" + key);
             // Parse stored json or if none return initialValue
             return (item && JSON.parse(item)) || initialValue;
         } catch (error) {
@@ -25,7 +25,8 @@ export default function useLocalStorage<T>(key: string, initialValue: T) {
             // Save state
             setStoredValue(valueToStore);
             // Save to local storage
-            window.localStorage.setItem("jacdac:" + key, JSON.stringify(valueToStore));
+            if(typeof window !== "undefined") 
+                window.localStorage.setItem("jacdac:" + key, JSON.stringify(valueToStore));
         } catch (error) {
             // A more advanced implementation would handle the error case
             console.log(error);
