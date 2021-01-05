5d1e94c59dcf9f1bd18bc260860c568c0dae9cc0
pelikhan <jhalleux@microsoft.com>
2021-01-05 23:30:48 +0100

    better logic for first poll

--- src/jdom/bus.ts
+++ src/jdom/bus.ts
@@ -695,7 +695,8 @@ export class JDBus extends JDNode {
                 const age = this.timestamp - register.lastGetTimestamp;
                 const backoff = register.lastGetAttempts;
                 const expiration = Math.min(REGISTER_POLL_REPORT_MAX_INTERVAL, REGISTER_POLL_REPORT_INTERVAL * (1 << backoff))
-                if (register.lastGetAttempts === 0 || age > expiration) {
+                const neverTriedYet = !register.data && register.lastGetAttempts === 0;
+                if (neverTriedYet || age > expiration) {
                     //console.log(`bus: poll ${register.id}`, register, age, backoff, expiration)
                     await register.sendGetAsync();
                 }
