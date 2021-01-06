00c8e190268cb6f0cf8c27624d24135868a2d23a
peli <pelikhan@users.noreply.github.com>
2021-01-06 02:51:36 -0800

    added inputsliders

--- docs/src/components/fields/MemberInput.tsx
+++ docs/src/components/fields/MemberInput.tsx
@@ -5,7 +5,8 @@ import { MenuItem, Select, Switch, TextField } from "@material-ui/core";
 import { flagsToValue, prettyMemberUnit, prettyUnit, valueToFlags } from "../../../../src/jdom/pretty";
 import { memberValueToString, scaleFloatToInt, scaleIntToFloat, tryParseMemberValue } from "../../../../src/jdom/spec";
 import IDChip from "../IDChip";
-import { roundWithPrecision } from "../../../../src/jdom/utils";
+import { isSet, roundWithPrecision } from "../../../../src/jdom/utils";
+import InputSlider from "../ui/InputSlider";
 
 export default function MemberInput(props: {
     specification: jdspec.PacketMember,
@@ -43,9 +44,13 @@ export default function MemberInput(props: {
         const v = enumInfo.isFlags ? flagsToValue(event.target.value) : event.target.value
         setValue(!!v)
     }
-    const handleSliderChange = async (event: any, newValue: number | number[]) => {
+    const handleScaledSliderChange = (event: any, newValue: number | number[]) => {
         const scaled = scaleFloatToInt((newValue as number), specification);
         setValue(scaled);
+    }
+    const handleSliderChange = (newValue: number) => {
+        const v = (newValue as number);
+        setValue(v);
     }
 
     const valueLabelFormat = (value: number) => {
@@ -81,9 +86,44 @@ export default function MemberInput(props: {
             disabled={disabled}
             value={scaleIntToFloat(value, specification)}
             valueLabelFormat={valueLabelFormat}
-            onChange={handleSliderChange}
+            onChange={handleScaledSliderChange}
             min={0} max={1} step={0.01}
             valueLabelDisplay="auto"
+        />
+    } else if (isSet(specification.typicalMin) && isSet(specification.typicalMax)) {
+        const step = (specification.typicalMax - specification.typicalMin) / 100;
+        // TODO: make step nicer
+        return <InputSlider
+            disabled={disabled}
+            value={value}
+            valueLabelFormat={valueLabelFormat}
+            onChange={handleSliderChange}
+            min={specification.typicalMin}
+            max={specification.typicalMax}
+            step={step}
+        />
+    } else if (isSet(specification.typicalMin) && isSet(specification.typicalMax)
+        && isSet(specification.absoluteMin) && isSet(specification.absoluteMax)) {
+        const step = (specification.absoluteMax - specification.absoluteMin) / 100;
+        // TODO: make step nicer
+        const marks = [
+            {
+                value: specification.typicalMin,
+                label: 'min',
+            },
+            {
+                value: specification.typicalMax,
+                label: 'max',
+            }
+        ];
+        return <InputSlider
+            value={value}
+            valueLabelFormat={valueLabelFormat}
+            onChange={disabled ? undefined : handleSliderChange}
+            min={specification.absoluteMin}
+            max={specification.absoluteMax}
+            step={step}
+            marks={marks}
         />
     }
     else // numbers or string
---
+++ docs/src/components/ui/InputSlider.tsx
@@ -0,0 +1,71 @@
+import { Grid, Input, Mark, Slider, Typography } from '@material-ui/core';
+import React from 'react';
+
+export default function InputSlider(props: {
+    value: number,
+    valueLabelFormat?: string | ((value: number, index: number) => React.ReactNode),
+    onChange?: (newValue: number) => void,
+    min?: number,
+    max?: number,
+    step?: number,
+    label?: string,
+    disabled?: boolean,
+    marks?: boolean | Mark[]
+}) {
+    const { min, max, step, label, disabled, marks, onChange, value, valueLabelFormat } = props;
+
+    const handleSliderChange = (event: any, newValue: number | number[]) => {
+        onChange(newValue as number);
+    };
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const n = parseFloat(event.target.value as string);
+        if (!isNaN(n))
+            onChange(n);
+    };
+
+    const handleBlur = () => {
+        if (value < min) {
+            onChange(min);
+        } else if (value > max) {
+            onChange(max);
+        }
+    };
+
+    return (
+        <Grid container spacing={2} alignItems="center">
+            {label && <Grid item xs={12}>
+                <Typography variant="caption">{label}</Typography>
+            </Grid>}
+            <Grid item xs>
+                <Slider
+                    disabled={disabled}
+                    valueLabelFormat={valueLabelFormat}
+                    value={value}
+                    onChange={handleSliderChange}
+                    valueLabelDisplay="auto"
+                    marks={marks}
+                    min={min}
+                    max={max}
+                    step={step}
+                />
+            </Grid>
+            <Grid item>
+                <Input
+                    disabled={disabled}
+                    value={value}
+                    margin="dense"
+                    onChange={handleInputChange}
+                    onBlur={handleBlur}
+                    inputProps={{
+                        step: step,
+                        min: min,
+                        max: max,
+                        type: 'number',
+                        'aria-labelledby': 'input-slider',
+                    }}
+                />
+            </Grid>
+        </Grid>
+    );
+}
