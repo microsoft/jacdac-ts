a4f090da06f2ff26d9dc5bebf991af31b0b768d5
peli <pelikhan@users.noreply.github.com>
2021-01-07 10:35:20 -0800

    move to dashboard

--- docs/src/pages/tools/makecode-sim.tsx
+++ docs/src/pages/tools/makecode-sim.tsx
@@ -2,6 +2,7 @@ import React, {  } from "react"
 import { Box, createMuiTheme, responsiveFontSizes, Typography } from "@material-ui/core";
 import ThemedLayout from "../../components/ui/ThemedLayout";
 import JDomTreeView from "../../components/JDomTreeView";
+import Dashboard from "../../components/views/Dashboard"
 
 export default function Page() {
     const rawTheme = createMuiTheme({
@@ -16,11 +17,6 @@ export default function Page() {
     })
     const theme = responsiveFontSizes(rawTheme);
     return <ThemedLayout theme={theme}>
-        <Box m={0.5}>
-            <Typography variant="subtitle2">
-                Modules
-            </Typography>
-        </Box>
-        <JDomTreeView dashboard={true} />
+        <Dashboard />
     </ThemedLayout>
 }
