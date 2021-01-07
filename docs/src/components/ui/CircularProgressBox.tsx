d2e8b60795f0cb03f6912abd3872fc61cd115c63
pelikhan <jhalleux@microsoft.com>
2021-01-07 13:02:35 +0100

    refactor circular ui

---
+++ docs/src/components/ui/CircularProgressBox.tsx
@@ -0,0 +1,36 @@
+import { Box, CircularProgress, useTheme } from "@material-ui/core";
+import React from "react";
+
+export interface CircularProgressBoxProps {
+    progress?: number;
+    progressSize?: React.ReactText,
+    progressColor?: "inherit" | "primary" | "secondary",
+    progressStyle?: React.CSSProperties
+}
+
+export default function CircularProgressBox(props: { children: JSX.Element | JSX.Element[] } & CircularProgressBoxProps) {
+    const { progress, progressColor, progressStyle, progressSize, children } = props;
+    const hasProgress = progress !== undefined;
+    const theme = useTheme();
+
+    return <Box position="relative" display="inline-flex">
+        <CircularProgress variant={hasProgress ? "determinate" : "indeterminate"}
+            disableShrink={!hasProgress}
+            value={progress}
+            size={progressSize || theme.spacing(3)}
+            color={progressColor}
+            style={progressStyle} />
+        <Box
+            top={0}
+            left={0}
+            bottom={0}
+            right={0}
+            position="absolute"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+        >
+            {children}
+        </Box>
+    </Box>
+}
--- docs/src/components/ui/IconButtonWithProgress.tsx
+++ docs/src/components/ui/IconButtonWithProgress.tsx
@@ -1,21 +1,16 @@
 import { Badge, Box, CircularProgress, IconButtonProps, Tooltip, useTheme } from "@material-ui/core";
 import { IconButton } from "gatsby-theme-material-ui";
 import React from "react";
+import CircularProgressBox, { CircularProgressBoxProps } from "./CircularProgressBox";
 
 export type IconButtonWithProgressProps = {
-    indeterminate?: boolean,
     badgeColor?: "default" | "primary" | "secondary",
     badgeCount?: number;
-    progress?: number;
-    progressSize?: React.ReactText,
-    progressColor?: "inherit" | "primary" | "secondary",
-    progressStyle?: React.CSSProperties
-} & IconButtonProps;
+    indeterminate?: boolean,
+} & IconButtonProps & CircularProgressBoxProps;
 
 export default function IconButtonWithProgress(props: IconButtonWithProgressProps) {
-    const { indeterminate, progress, title, children, progressColor, progressStyle, progressSize, badgeCount, badgeColor, ...others } = props;
-    const theme = useTheme()
-    const hasProgress = progress !== undefined;
+    const { indeterminate, progress, title, children, badgeCount, badgeColor, ...others } = props;
 
     const badge = <Badge color={badgeColor}
         badgeContent={badgeCount}
@@ -26,26 +21,7 @@ export default function IconButtonWithProgress(props: IconButtonWithProgressProp
     return <Tooltip title={title}>
         <span><IconButton {...others}>
             {!indeterminate && badge}
-            {indeterminate && <Box position="relative" display="inline-flex">
-                <CircularProgress variant={hasProgress ? "determinate" : "indeterminate"}
-                    disableShrink={!hasProgress}
-                    value={progress}
-                    size={progressSize || theme.spacing(3)}
-                    color={progressColor}
-                    style={progressStyle} />
-                <Box
-                    top={0}
-                    left={0}
-                    bottom={0}
-                    right={0}
-                    position="absolute"
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                >
-                    {badge}
-                </Box>
-            </Box>}
+            {indeterminate && <CircularProgressBox {...others} children={badge} />}
         </IconButton></span>
     </Tooltip>
 }
