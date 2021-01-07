0c98a96a6e753242093006b71b5ea66c966cbefc
pelikhan <jhalleux@microsoft.com>
2021-01-07 14:55:14 +0100

    more grid fun

--- docs/src/components/views/Dashboard.tsx
+++ docs/src/components/views/Dashboard.tsx
@@ -133,9 +133,9 @@ export default function Dashboard() {
         xl?: GridSize
     } => {
         if (selected(device))
-            return { xs: 12, sm: 8, xl: 6 };
+            return { xs: 12, sm: 6, md: 6, lg: 6, xl: 4 };
         else
-            return { xs: 6, sm: 4, xl: 3 };
+            return { xs: 6, sm: 3, md: 3, lg: 3, xl: 2 };
     }
 
     if (!devices.length)
