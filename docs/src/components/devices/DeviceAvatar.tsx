23176835d0724279063a364452f6216071104808
pelikhan <jhalleux@microsoft.com>
2021-01-07 12:55:28 +0100

    center image

--- docs/src/components/devices/DeviceAvatar.tsx
+++ docs/src/components/devices/DeviceAvatar.tsx
@@ -5,6 +5,9 @@ import useDeviceSpecification from "../../jacdac/useDeviceSpecification";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    img: {
+      marginTop: "58%",
+    },
     small: {
       width: theme.spacing(3),
       height: theme.spacing(3),
@@ -17,13 +20,20 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 export default function DeviceAvatar(props: { device: JDDevice, showMissing?: boolean, size?: "small" | "large" }) {
-    const { device, showMissing, size } = props;
-    const { specification, imageUrl } = useDeviceSpecification(device);
-    const classes = useStyles();
-    const sizeClassName = size === "small" ? classes.small : size === "large" ? classes.large : undefined;
+  const { device, showMissing, size } = props;
+  const { specification, imageUrl } = useDeviceSpecification(device);
+  const classes = useStyles();
+  const sizeClassName = size === "small" ? classes.small : size === "large" ? classes.large : undefined;
 
-    if (!showMissing && (!specification || !imageUrl))
-        return null;
+  if (!showMissing && (!specification || !imageUrl))
+    return null;
 
-    return <Avatar className={sizeClassName} alt={specification?.description || "Avatar of the device"} src={imageUrl} />
+  return <Avatar
+    className={sizeClassName}
+    alt={specification?.name || "Image of the device"}
+    src={imageUrl}
+    classes={{
+      img: classes.img
+    }}
+    />
 }
