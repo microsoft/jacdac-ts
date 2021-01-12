788875ed81c690381d3994db293091a09cfa11f2
pelikhan <jhalleux@microsoft.com>
2021-01-12 09:42:29 +0100

    fixing build

--- src/jdom/client.ts
+++ src/jdom/client.ts
@@ -7,6 +7,10 @@ export class JDClient extends JDEventSource {
         super()
     }
 
+    protected log(msg: any) {
+        console.log(msg);
+    }
+
     mount(unsubscribe: () => void): () => void {
         if (unsubscribe && this.unsubscribers.indexOf(unsubscribe) < 0)
             this.unsubscribers.push(unsubscribe)
