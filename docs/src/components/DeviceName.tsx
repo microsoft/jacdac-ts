71659c166dbac5154423ff290afded89c1330750
peli <pelikhan@users.noreply.github.com>
2021-01-12 06:47:03 -0800

    added hint

--- docs/src/components/DeviceName.tsx
+++ docs/src/components/DeviceName.tsx
@@ -1,6 +1,7 @@
 import { Typography } from "@material-ui/core";
 import React from "react"
 import { JDDevice } from "../../../src/jdom/device";
+import useDeviceHost from "./hooks/useDeviceHost";
 import useDeviceName from "./useDeviceName";
 
 export default function DeviceName(props: { device: JDDevice, serviceIndex?: number, 
@@ -8,11 +9,13 @@ export default function DeviceName(props: { device: JDDevice, serviceIndex?: num
     const { device, serviceIndex, showShortId } = props
     const name = useDeviceName(device)
     const { shortId } = device
+    const host = useDeviceHost(device)
 
     return <span>
         {name || shortId}
         {showShortId && name && name !== shortId &&
             <Typography component="span" variant="body2"> {shortId}</Typography>}
         {serviceIndex !== undefined && `[${serviceIndex}]`}
+        {host && <Typography component="span" variant="caption">(virtual)</Typography>}
     </span>
 }
