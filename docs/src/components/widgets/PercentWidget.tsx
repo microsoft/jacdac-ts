6f9f7c4105fda4f67e27de1746989332db5ba5d1
pelikhan <jhalleux@microsoft.com>
2021-01-12 23:29:57 +0100

    more dashboard tinkering

--- docs/src/components/widgets/PercentWidget.tsx
+++ docs/src/components/widgets/PercentWidget.tsx
@@ -4,10 +4,11 @@ import { Grid, Typography } from "@material-ui/core";
 export default function PercentWidget(props: {
     value?: number;
     label: string,
+    secondaryLabel?: string,
     color?: "primary" | "secondary",
     size?: string
 }) {
-    const { value, color, label } = props;
+    const { value, secondaryLabel, color, label } = props;
     const valueVariant = "h1"
     const labelVariant = "h6"
 
@@ -17,9 +18,9 @@ export default function PercentWidget(props: {
             <Grid item xs>
                 <Typography variant={valueVariant}>{percent}</Typography>
             </Grid>
-            <Grid xs container direction="column" justify="space-between">
-                {label && <Grid item><Typography variant={labelVariant}>{label}</Typography></Grid>}
-                <Grid item><Typography variant={labelVariant}>%</Typography></Grid>
+            <Grid container direction="column" justify="space-between">
+                <Grid item><Typography variant={labelVariant}>{label}</Typography></Grid>
+                {secondaryLabel && <Grid item><Typography variant={labelVariant}>{secondaryLabel}</Typography></Grid>}
             </Grid>
         </Grid>
 }
