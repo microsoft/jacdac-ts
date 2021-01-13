4b6d5b384823724489e7080b7d12cb0727c713a7
peli <pelikhan@users.noreply.github.com>
2021-01-13 03:30:50 -0800

    better support for starts repeating

--- docs/src/components/PacketSpecification.tsx
+++ docs/src/components/PacketSpecification.tsx
@@ -31,20 +31,34 @@ function MemberType(props: { member: jdspec.PacketMember }) {
     const classes = useStyles();
     const helperText = prettyMemberUnit(member, true);
 
-    return <li className={classes.field}>
-        {member.name !== "_" && `${member.name}: `}<code>{helperText}</code>
-        {member.startRepeats ? ", starts repeating" : ""}
-    </li>
+    return <>
+        <li className={classes.field}>
+            {member.name !== "_" && `${member.name}: `}<code>{helperText}</code>
+        </li>
+    </>
 }
 
 function MembersType(props: { members: jdspec.PacketMember[], title?: string }) {
     const { members, title } = props;
 
+    let repeatIndex = members.findIndex(m => !!m.startRepeats);
+    if (repeatIndex < 0) repeatIndex = members.length;
+    const beforeRepeat = members.slice(0, repeatIndex)
+    const afterRepeat = members.slice(repeatIndex)
+
     return <>
         {!!title && <h4>{title}</h4>}
-        <ul>
-            {members.map((member, i) => <MemberType key={`member${member.name}`} member={member} />)}
-        </ul>
+        {!!beforeRepeat.length && <ul>
+            {beforeRepeat
+                .map((member, i) => <MemberType key={`member${member.name}`} member={member} />)}
+        </ul>}
+        {!!afterRepeat.length && <>
+            <h5>starts repeating</h5>
+            <ul>
+                {afterRepeat
+                    .map((member, i) => <MemberType key={`member${member.name}`} member={member} />)}
+            </ul>
+        </>}
     </>
 }
 
