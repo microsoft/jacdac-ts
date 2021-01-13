40fbb1e8c715fe34b776077856b0bf60bba9fc5d
pelikhan <jhalleux@microsoft.com>
2021-01-13 10:39:28 +0100

    use grid layout for values

--- docs/src/components/widgets/ValueWithUnitWidget.tsx
+++ docs/src/components/widgets/ValueWithUnitWidget.tsx
@@ -1,32 +1,49 @@
 import React from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { createStyles, Grid, makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => createStyles({
+    gridContainer: {
+        display: "grid",
+        gridTemplateColumns: "4fr 1fr",
+        gridTemplateRows: "3.5fr 0.5fr",
+        gap: `${theme.spacing(0.5)} ${theme.spacing(0.5)}`,
+        gridTemplateAreas: `
+        "value upper-unit"
+        "value lower-unit"
+`
+    },
+    value: { gridArea: "value" },
+    upperUnit: { gridArea: "upper-unit" },
+    lowerUnit: { gridArea: "lower-unit" }
+}));
 
 export default function ValueWithUnitWidget(props: {
     value?: number;
     label: string,
     secondaryLabel?: string,
-    helperText?: string,
     color?: "primary" | "secondary",
     size?: string
 }) {
-    const { value, secondaryLabel, helperText, color, label } = props;
-    const labelVariant = "h6"
+    const { value, secondaryLabel, color, label } = props;
+    const labelVariant = "body2"
+    const classes = useStyles();
 
     const percent = isNaN(value) ? "--" : value.toLocaleString();
     const valueVariant = percent.length < 3 ? "h1"
         : percent.length < 5 ? "h2"
-        : percent.length < 7 ? "h3"
-        : percent.length < 9 ? "h4"
-        : "h5";
+            : percent.length < 7 ? "h3"
+                : percent.length < 9 ? "h4"
+                    : "h5";
 
-    return <Grid container direction="row" justify="center" alignItems="center">
-            <Grid item xs>
-                <Typography variant={valueVariant}>{percent}</Typography>
-                {helperText && <Typography variant="caption">{helperText}</Typography>}
-            </Grid>
-            <Grid container direction="column" justify="space-between">
-                <Grid item><Typography variant={labelVariant}>{label}</Typography></Grid>
-                {secondaryLabel && <Grid item><Typography variant={labelVariant}>{secondaryLabel}</Typography></Grid>}
-            </Grid>
-        </Grid>
+    return <div className={classes.gridContainer}>
+        <div className={classes.value}>
+            <Typography align="right" variant={valueVariant}>{percent}</Typography>
+        </div>
+        {label && <div className={classes.upperUnit}>
+            <Typography variant={labelVariant}>{label}</Typography>
+        </div>}
+        {secondaryLabel && <div className={classes.lowerUnit}>
+            <Typography variant={labelVariant}>{secondaryLabel}</Typography>
+        </div>}
+    </div>;
 }
