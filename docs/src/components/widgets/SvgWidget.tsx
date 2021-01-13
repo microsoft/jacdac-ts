49a473df335f46b4034862ae92775868fe06b29a
peli <pelikhan@users.noreply.github.com>
2021-01-13 07:58:24 -0800

    fix svg height

--- docs/src/components/widgets/SvgWidget.tsx
+++ docs/src/components/widgets/SvgWidget.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function SvgWidget(props: { width: number, height?: number, size?: string, children: JSX.Element | JSX.Element[] }) {
     const { width, height, size, children } = props;
     const h = height || width;
-    return <svg viewBox={`0 0 ${width} ${height}`} style={size && {
+    return <svg viewBox={`0 0 ${width} ${h}`} style={size && {
         width: size
     }}>
         {children}
