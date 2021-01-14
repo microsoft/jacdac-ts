fc1dc6539d7c3176cbfa91aab4abac86c1f7ef32
pelikhan <jhalleux@microsoft.com>
2021-01-14 23:14:52 +0100

    namespace

--- docs/src/components/widgets/SvgWidget.tsx
+++ docs/src/components/widgets/SvgWidget.tsx
@@ -3,9 +3,10 @@ import React from "react";
 export function SvgWidget(props: { width: number, height?: number, size?: string, children: JSX.Element | JSX.Element[] }) {
     const { width, height, size, children } = props;
     const h = height || width;
-    return <svg viewBox={`0 0 ${width} ${h}`} style={size && {
-        width: size
-    }}>
+    return <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox={`0 0 ${width} ${h}`}
+        style={size && { width: size }}>
         {children}
     </svg>
 }
