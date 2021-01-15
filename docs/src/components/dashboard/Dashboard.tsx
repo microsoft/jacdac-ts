0b10384b86368cb7bdb9af2b0be75c8462688e41
peli <pelikhan@users.noreply.github.com>
2021-01-15 07:25:02 -0800

    more sizing

--- docs/src/components/dashboard/Dashboard.tsx
+++ docs/src/components/dashboard/Dashboard.tsx
@@ -40,11 +40,11 @@ function DeviceItem(props: {
         .reduce((c: number, v) => c + v, 0);
     const breakpoints: GridBreakpoints = useMemo(() => {
         if (readingCount > 2)
-            return { xs: 12, sm: 12, md: 12, lg: "auto", xl: "auto" };
+            return { xs: 12, sm: 12, md: 12, lg: 8, xl: 6 };
         else if (readingCount == 2)
-            return { xs: 12, sm: 6, md: 6, lg: "auto", xl: "auto" };
+            return { xs: 12, sm: 6, md: 6, lg: 6, xl: 4 };
         else
-            return { xs: expanded ? 12 : 6, sm: 6, md: 6, lg: "auto", xl: "auto" };
+            return { xs: expanded ? 12 : 6, sm: 6, md: 6, lg: 4, xl: "auto" };
     }, [expanded, readingCount]);
 
     // based on size, expanded or reduce widget size
