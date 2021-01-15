bae1ab876765c5ea018db7e62187bb9bfabd71dd
peli <pelikhan@users.noreply.github.com>
2021-01-15 09:30:56 -0800

    rotated servo

--- docs/src/components/widgets/useWidgetSize.ts
+++ docs/src/components/widgets/useWidgetSize.ts
@@ -3,6 +3,6 @@ import { useMediaQuery, useTheme } from "@material-ui/core";
 export default function useWidgetSize() {
     const theme = useTheme();
     const mobile = useMediaQuery(theme.breakpoints.down("xs"));
-    const widgetSize = mobile ? "11vh" : "15vh";
+    const widgetSize = mobile ? "14vh" : "25vh";
     return widgetSize;
 }
