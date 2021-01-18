f92747586d98c0d363773c173863b08cdd48b530
pelikhan <jhalleux@microsoft.com>
2021-01-18 23:19:39 +0100

    contrast issue with tooltips

--- docs/src/components/ui/IconButtonWithTooltip.tsx
+++ docs/src/components/ui/IconButtonWithTooltip.tsx
@@ -1,14 +1,23 @@
-import { IconButtonProps, Tooltip } from "@material-ui/core";
+import { IconButtonProps, Theme, Tooltip, withStyles } from "@material-ui/core";
 import { IconButton } from "gatsby-theme-material-ui";
 import React from "react";
+import Zoom from '@material-ui/core/Zoom';
+
+// fix for contrast issue
+const LightTooltip = withStyles((theme: Theme) => ({
+    tooltip: {
+      backgroundColor: theme.palette.background.default,
+      color: theme.palette.text.primary,
+    },
+  }))(Tooltip);
 
 export default function IconButtonWithTooltip(props: { to?: string, disabled?: boolean } & IconButtonProps) {
     const { title, children, disabled, ...others } = props;
 
-    return <Tooltip title={title}>
+    return <LightTooltip TransitionComponent={Zoom} title={title}>
         <span>
             <IconButton aria-label={title} disabled={disabled} {...others}>
                 {children}
             </IconButton></span>
-    </Tooltip>
+    </LightTooltip>
 }
