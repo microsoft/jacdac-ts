d0daa47d6d8ee28e2a4aff17ee86a38165c3db6b
Peli de Halleux <pelikhan@users.noreply.github.com>
2021-01-20 07:07:29 +0100

    implement resetin service (#146)

--- src/jdom/controlservicehost.ts
+++ src/jdom/controlservicehost.ts
@@ -1,4 +1,4 @@
-import { ControlAnnounceFlags, ControlCmd, ControlReg, IDENTIFY, RESET, SRV_CTRL } from "./constants";
+import { ControlAnnounceFlags, ControlCmd, ControlReg, IDENTIFY, REPORT_RECEIVE, RESET, SRV_CTRL } from "./constants";
 import Packet from "./packet";
 import JDRegisterHost from "./registerhost";
 import JDServiceHost from "./servicehost";
@@ -12,16 +12,20 @@ export default class ControlServiceHost extends JDServiceHost {
     readonly uptime: JDRegisterHost<[number]>;
     readonly startTime: number;
     readonly statusLight: JDRegisterHost<[[number, number, number, number][]]>;
+    private _resetTimeOut: any;
 
     constructor() {
         super(SRV_CTRL)
 
         this.startTime = Date.now();
+        this.resetIn = this.addRegister<[number]>(ControlReg.ResetIn, [0]);
         this.deviceDescription = this.addRegister<[string]>(ControlReg.DeviceDescription);
         this.mcuTemperature = this.addRegister<[number]>(ControlReg.McuTemperature, [25]);
         this.resetIn = this.addRegister<[number]>(ControlReg.ResetIn);
         this.uptime = this.addRegister<[number]>(ControlReg.Uptime);
         this.statusLight = this.addRegister<[[number, number, number, number][]]>(ControlReg.StatusLight, [[]]);
+
+        this.resetIn.on(REPORT_RECEIVE, this.handleResetIn.bind(this));
 
         this.addCommand(ControlCmd.Services, this.announce.bind(this));
         this.addCommand(ControlCmd.Identify, this.identify.bind(this));
@@ -57,5 +61,15 @@ export default class ControlServiceHost extends JDServiceHost {
         this.emit(RESET);
         this.restartCounter = 0;
         this.packetCount = 0;
+        this._resetTimeOut = undefined;
+        // TODO reset all registers
+    }
+
+    private handleResetIn() {
+        const [t] = this.resetIn.values();
+        if (this._resetTimeOut)
+            clearTimeout(this._resetTimeOut);
+        if (t)
+            this._resetTimeOut = setTimeout(() => this.reset(), t);
     }
 }
