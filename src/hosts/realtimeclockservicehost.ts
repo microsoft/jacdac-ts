5463f6303852b5dcb9910b5ce2a9510996a8c5b0
peli <pelikhan@users.noreply.github.com>
2021-01-26 12:44:31 -0800

    remove epoch

--- src/hosts/realtimeclockservicehost.ts
+++ src/hosts/realtimeclockservicehost.ts
@@ -4,10 +4,9 @@ import JDRegisterHost from "../jdom/registerhost";
 import { JDBus } from "../jdom/bus";
 import Packet from "../jdom/packet";
 
-export type RealTimeClockReadingType = [number, number, number, number, number, number, number, number];
+export type RealTimeClockReadingType = [number, number, number, number, number, number, number];
 
 export function dateToClock(n: Date): RealTimeClockReadingType {
-    const epoch = (n.getTime() / 1000) >> 0;
     const year = n.getFullYear();
     const month = n.getMonth();
     const date = n.getDate();
@@ -16,7 +15,7 @@ export function dateToClock(n: Date): RealTimeClockReadingType {
     const min = n.getMinutes()
     const sec = n.getSeconds()
 
-    return [epoch, year, month, date, day, hour, min, sec];
+    return [year, month, date, day, hour, min, sec];
 }
 
 export default class RealTimeClockServiceHost
@@ -42,7 +41,7 @@ export default class RealTimeClockServiceHost
     static async syncTime(bus: JDBus) {
         const values = dateToClock(new Date());
         const pkt = Packet.jdpacked<RealTimeClockReadingType>(RealTimeClockCmd.SetTime,
-            "u32 u16 u8 u8 u8 u8 u8 u8",
+            "u16 u8 u8 u8 u8 u8 u8",
             values);
         await pkt.sendAsMultiCommandAsync(bus, SRV_REAL_TIME_CLOCK);
     }
