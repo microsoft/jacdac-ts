3b92e6e368744a6d449db6c03c0912bdd5343dcf
pelikhan <jhalleux@microsoft.com>
2021-01-26 11:22:45 +0100

    update naming dialog

--- docs/src/components/dialogs/DeviceRenameDialog.tsx
+++ docs/src/components/dialogs/DeviceRenameDialog.tsx
@@ -1,14 +1,16 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@material-ui/core";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, Typography } from "@material-ui/core";
 import React, { useState } from "react";
 import { useId } from "react-use-id-hook";
 import { JDDevice } from "../../../../src/jacdac";
 import DeviceActions from "../DeviceActions";
+import useDeviceHost from "../hooks/useDeviceHost";
 
 export default function DeviceRenameDialog(props: { device: JDDevice, onClose: () => void }) {
     const { device, onClose } = props;
     const dialogId = useId()
     const open = !!device
     const [name, setName] = useState(device.name || "");
+    const host = useDeviceHost(device)
 
     const handleCancel = (ev) => {
         ev.preventDefault()
@@ -24,17 +26,18 @@ export default function DeviceRenameDialog(props: { device: JDDevice, onClose: (
     }
 
     return <Dialog open={open} onClose={handleClose} aria-labelledby={dialogId}>
-        <DialogTitle id={dialogId}>Name your device</DialogTitle>
+        <DialogTitle id={dialogId}>Name this {host ? "simulator" : "device"}</DialogTitle>
         <DialogContent>
             <DialogContentText>
                 <span>Give a friendly name to </span>
-                <strong>{device.shortId}</strong> ({device.deviceId}).
+                <strong>{device.shortId}</strong>
+                <Typography variant="caption" component="span">({device.deviceId})</Typography>.
             <DeviceActions device={device} />
             </DialogContentText>
             <TextField
                 autoFocus
                 margin="dense"
-                label="Device name"
+                label="name"
                 fullWidth
                 value={name}
                 onChange={handleChange}
