f1c404404fe66ff23b509b5f09f13246ace38af3
pelikhan <jhalleux@microsoft.com>
2021-01-27 07:11:45 +0100

    better sizing issues

--- docs/src/components/widgets/ValueWithUnitWidget.tsx
+++ docs/src/components/widgets/ValueWithUnitWidget.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid, Slider, Typography } from "@material-ui/core";
 import { isSet, roundWithPrecision } from "../../../../src/jdom/utils";
+import { CSSProperties } from "@material-ui/core/styles/withStyles";
 
 export default function ValueWithUnitWidget(props: {
     value: number,
@@ -20,7 +21,7 @@ export default function ValueWithUnitWidget(props: {
     const hasValue = !isNaN(value);
     const valueText = hasValue ? roundWithPrecision(value, precision).toLocaleString() : "--"
     const valueTextLength = isSet(min) && isSet(max) ? [min, max]
-        .map(v => roundWithPrecision(v, precision).toLocaleString().length)
+        .map(v => roundWithPrecision(v, precision).toLocaleString().replace(/[,\.]/, '').length)
         .reduce((l, r) => Math.max(l, r), 0)
         + precision : valueText.length;
 
@@ -30,12 +31,16 @@ export default function ValueWithUnitWidget(props: {
             : valueTextLength < 9 ? "h3"
                 : valueTextLength < 12 ? "h4"
                     : "h6";
+    const valueStyle: CSSProperties = {
+        minWidth: `${Math.max(2, valueTextLength - 1)}em`
+    }
 
     return <Grid container direction="column" tabIndex={tabIndex} aria-label={`${valueText} ${label}`}>
         <Grid item xs={12}>
             <Grid container direction="row" alignContent="flex-end">
                 <Grid item>
-                    <Typography role="timer" align="right" variant={valueVariant}>{valueText}</Typography>
+                    <Typography role="timer" align="right" variant={valueVariant}
+                        style={valueStyle}>{valueText}</Typography>
                 </Grid>
                 <Grid item>
                     <Grid container direction="column" alignContent="space-between">
