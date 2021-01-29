d35f0b74c2263ee91f12820ce6532723f078bf06
Peli de Halleux <pelikhan@users.noreply.github.com>
2021-01-29 15:13:23 +0100

    Services/led (#154)
    * support for new leds
    
    * updated
    
    * updated
    
    * better support
    
    * fix led count
    
    * tming issues
    
    * timing fixes
    
    * more animation support
    
    * udpated specs

--- docs/src/components/dashboard/DashboardLED.tsx
+++ docs/src/components/dashboard/DashboardLED.tsx
@@ -11,6 +11,7 @@ import JACDACContext, { JDContextProps } from "../../../../src/react/Context";
 import { Grid, Slider } from "@material-ui/core";
 import LEDServiceHost, { LedAnimation, LedAnimationStepsType } from "../../../../src/hosts/ledservicehost";
 import { LedReg } from "../../../../src/jdom/constants";
+import { hsvToCss } from "../../../../src/jacdac";
 
 export default function DashboardLED(props: DashboardServiceProps) {
     const { bus } = useContext<JDContextProps>(JACDACContext)
@@ -19,63 +20,63 @@ export default function DashboardLED(props: DashboardServiceProps) {
     const host = useServiceHost<LEDServiceHost>(service);
     const color = host ? "secondary" : "primary";
     const { active } = useWidgetTheme(color);
-    const [maxIterations] = useRegisterUnpackedValue<[number]>(service.register(LedReg.MaxIterations));
     const brightnessRegister = service.register(LedReg.Brightness);
     const [brightness] = useRegisterUnpackedValue<[number]>(brightnessRegister);
-    const [currentIteration] = useRegisterUnpackedValue<[number]>(service.register(LedReg.CurrentIteration));
     const [steps] = useRegisterUnpackedValue<LedAnimationStepsType>(service.register(LedReg.Steps));
+    const [waveLength] = useRegisterUnpackedValue<[number]>(service.register(LedReg.WaveLength));
+    const [ledCount] = useRegisterUnpackedValue<[number]>(service.register(LedReg.LedCount));
 
     // restart animation with steps
-    const animation = useMemo(() => new LedAnimation(maxIterations, brightness, currentIteration, steps)
-        , steps || [])
-    // update animation on the fly
-    useEffect(() => {
-        animation.maxIterations = maxIterations
-        animation.brightness = brightness
-        animation.currentIteration = currentIteration
-    }, [animation, maxIterations, currentIteration, brightness]);
+    const animation = useMemo(() => new LedAnimation(steps), [steps])
     // animate
     useAnimationFrame(() => {
         animation.update(bus.timestamp); return true;
     }, [animation])
 
-    const intensity = useChange(animation, a => a.intensity)
+    const hsv = useChange(animation, a => a?.hsv)
 
     const handleBrightnessChange = async (ev: unknown, newValue: number | number[]) => {
-        await brightnessRegister.sendSetPackedAsync("u16", [newValue as number], true);
+        await brightnessRegister.sendSetPackedAsync("u0.16", [newValue as number], true);
     }
 
     // nothing to see
-    if (intensity === undefined)
+    if (hsv === undefined)
         return null;
 
-    const w = 15.5;
+    const opacity = brightness;
+    const fill = hsvToCss(hsv[0], hsv[1], hsv[2], brightness * 0xff, waveLength);
+
+    const ln = Math.min(ledCount || 1, 5)
+    const lw = 15.5;
+    const m = 1;
+    const w = (lw + m) * ln;
     const h = 42;
-    const r = w / 2;
-    const sw = 2;
     return <Grid container direction="row">
-        <Grid item>
-            <Slider
+        <Grid item xs={2}>
+            {brightness !== undefined && <Slider
                 orientation="vertical"
-                min={0} max={0xffff}
-                step={0xffff / 0xfff}
+                min={0} max={1}
+                step={0.01}
                 color={color}
                 valueLabelDisplay="off"
                 value={brightness}
-                onChange={handleBrightnessChange} />
+                onChange={handleBrightnessChange} />}
         </Grid>
-        <Grid item>
+        <Grid item xs={10}>
             <SvgWidget width={w} height={h} size={widgetSize}>
-                <path fill="#999" d="M14.2 13V7.1C14.2 3.2 11 0 7.1 0 3.2 0 0 3.2 0 7.1v13.7c1.9 1.9 4.4 2.9 7.1 2.8 4.6 0 8.4-2.6 8.4-5.9v-1.5c0-1.2-.5-2.3-1.3-3.2z" opacity=".65" />
-                <path fill="#8c8c8c" d="M2.8 17.5l-1.2-1.4h1L5 17.5v18.6c0 .3-.5.5-1.1.5-.6 0-1.1-.2-1.1-.5zm10.1 6.7c0-.7-1.1-1.3-2.1-1.8-.4-.2-1.2-.6-1.2-.9v-3.4l2.5-2h-.9l-3.7 2v3.5c0 .7.9 1.2 1.9 1.7.4.2 1.3.8 1.3 1.1v16.9c0 .4.5.7 1.1.7.6 0 1.1-.3 1.1-.7z" />
-                <path opacity={intensity} fill={active} d="M14.2 13V7.1C14.2 3.2 11 0 7.1 0 3.2 0 0 3.2 0 7.1v13.7c1.9 1.9 4.4 2.9 7.1 2.8 4.6 0 8.4-2.6 8.4-5.9v-1.5c0-1.2-.5-2.3-1.3-3.2z" />
-                <path fill="#d1d1d1" d="M14.2 13v3.1c0 2.7-3.2 5-7.1 5-3.9 0-7.1-2.2-7.1-5v4.6c1.9 1.9 4.4 2.9 7.1 2.8 4.6 0 8.4-2.6 8.4-5.9v-1.5c0-1.1-.5-2.2-1.3-3.1z" opacity=".9" />
-                <path fill="#e6e6e6" d="M14.2 13v3.1c0 2.7-3.2 5-7.1 5-3.9 0-7.1-2.2-7.1-5v4.6c1.9 1.9 4.4 2.9 7.1 2.8 4.6 0 8.4-2.6 8.4-5.9v-1.5c0-1.1-.5-2.2-1.3-3.1z" opacity=".7" />
-                <path fill="#e6e6e6" d="M14.2 13v3.1c0 2.7-3.2 5-7.1 5-3.9 0-7.1-2.2-7.1-5v3.1c1.9 1.9 4.4 2.9 7.1 2.8 4.6 0 8.4-2.6 8.4-5.9 0-1.1-.5-2.2-1.3-3.1z" opacity=".25" />
-                <ellipse cx="-7.2" cy="-16.1" fill="#e6e6e6" opacity=".25" rx="7.1" ry="5" transform="scale(-1)" />
-                <ellipse cx="-7.2" cy="-16.1" fill="#e6e6e6" opacity=".25" rx="7.1" ry="5" transform="scale(-1)" />
-                <path opacity=".61" fill="#fff" d="M7.2 22c-4.3 0-6.1-2-6.1-2l.7-.7S3.4 21 7.2 21c2.4.1 4.7-.9 6.3-2.7l.8.6C12.4 21 9.9 22.1 7.2 22z" />
-                <path opacity=".61" fill="#fff" d="M2.6 3.2C1.5 4.3.9 5.8.9 7.4v10.3l1.9 1.5V8.8c-.9-2.8-.3-4.2.7-5.2-.3-.1-.6-.2-.9-.4zm7.3-1.6l-.3.9c1.5.7 2.6 2.1 2.8 3.7h1c-.3-2-1.6-3.7-3.5-4.6z" />
+                {Array(ln).fill(0).map((_, i) =>
+                    <g key={i} transform={`translate(${i * (lw + m)}, 0)`}>
+                        <path fill="#999" d="M14.2 13V7.1C14.2 3.2 11 0 7.1 0 3.2 0 0 3.2 0 7.1v13.7c1.9 1.9 4.4 2.9 7.1 2.8 4.6 0 8.4-2.6 8.4-5.9v-1.5c0-1.2-.5-2.3-1.3-3.2z" opacity=".65" />
+                        <path fill="#8c8c8c" d="M2.8 17.5l-1.2-1.4h1L5 17.5v18.6c0 .3-.5.5-1.1.5-.6 0-1.1-.2-1.1-.5zm10.1 6.7c0-.7-1.1-1.3-2.1-1.8-.4-.2-1.2-.6-1.2-.9v-3.4l2.5-2h-.9l-3.7 2v3.5c0 .7.9 1.2 1.9 1.7.4.2 1.3.8 1.3 1.1v16.9c0 .4.5.7 1.1.7.6 0 1.1-.3 1.1-.7z" />
+                        <path opacity={opacity} fill={fill} d="M14.2 13V7.1C14.2 3.2 11 0 7.1 0 3.2 0 0 3.2 0 7.1v13.7c1.9 1.9 4.4 2.9 7.1 2.8 4.6 0 8.4-2.6 8.4-5.9v-1.5c0-1.2-.5-2.3-1.3-3.2z" />
+                        <path fill="#d1d1d1" d="M14.2 13v3.1c0 2.7-3.2 5-7.1 5-3.9 0-7.1-2.2-7.1-5v4.6c1.9 1.9 4.4 2.9 7.1 2.8 4.6 0 8.4-2.6 8.4-5.9v-1.5c0-1.1-.5-2.2-1.3-3.1z" opacity=".9" />
+                        <path fill="#e6e6e6" d="M14.2 13v3.1c0 2.7-3.2 5-7.1 5-3.9 0-7.1-2.2-7.1-5v4.6c1.9 1.9 4.4 2.9 7.1 2.8 4.6 0 8.4-2.6 8.4-5.9v-1.5c0-1.1-.5-2.2-1.3-3.1z" opacity=".7" />
+                        <path fill="#e6e6e6" d="M14.2 13v3.1c0 2.7-3.2 5-7.1 5-3.9 0-7.1-2.2-7.1-5v3.1c1.9 1.9 4.4 2.9 7.1 2.8 4.6 0 8.4-2.6 8.4-5.9 0-1.1-.5-2.2-1.3-3.1z" opacity=".25" />
+                        <ellipse cx="-7.2" cy="-16.1" fill="#e6e6e6" opacity=".25" rx="7.1" ry="5" transform="scale(-1)" />
+                        <ellipse cx="-7.2" cy="-16.1" fill="#e6e6e6" opacity=".25" rx="7.1" ry="5" transform="scale(-1)" />
+                        <path opacity=".61" fill="#fff" d="M7.2 22c-4.3 0-6.1-2-6.1-2l.7-.7S3.4 21 7.2 21c2.4.1 4.7-.9 6.3-2.7l.8.6C12.4 21 9.9 22.1 7.2 22z" />
+                        <path opacity=".61" fill="#fff" d="M2.6 3.2C1.5 4.3.9 5.8.9 7.4v10.3l1.9 1.5V8.8c-.9-2.8-.3-4.2.7-5.2-.3-.1-.6-.2-.9-.4zm7.3-1.6l-.3.9c1.5.7 2.6 2.1 2.8 3.7h1c-.3-2-1.6-3.7-3.5-4.6z" />
+                    </g>)}
             </SvgWidget>
         </Grid>
     </Grid>
--- src/hosts/ledservicehost.ts
+++ src/hosts/ledservicehost.ts
@@ -1,64 +1,47 @@
-import { CHANGE, LedReg, REPORT_UPDATE, SRV_LED } from "../jdom/constants";
+import { CHANGE, LedReg, LedVariant, REPORT_UPDATE, SRV_LED } from "../jdom/constants";
 import { JDEventSource } from "../jdom/eventsource";
 import JDRegisterHost from "../jdom/registerhost";
 import JDServiceHost from "../jdom/servicehost";
 
-export type LedAnimationStep = [number, number];
-export type LedAnimationStepsType = [LedAnimationStep[]];
+export type LedAnimationFrame = [number, number, number, number]
+export type LedAnimationStepsType = [LedAnimationFrame[]];
 
 export class LedAnimation extends JDEventSource {
     private _currentStep: number;
     private _currentStepStartTime: number;
-    private _currentItensity: number;
+    private _currentHsv: number;
 
-    constructor(
-        public maxIterations: number,
-        public brightness: number,
-        public currentIteration: number,
-        public steps: LedAnimationStep[]
-        ) {
+    constructor(public steps: LedAnimationFrame[]) {
         super();
-
-        this.maxIterations = this.maxIterations || 1;
-        this.brightness = this.brightness !== undefined ? this.brightness : 0xffff;
-        this.currentIteration = this.currentIteration || 0;
 
         this._currentStep = 0;
         this._currentStepStartTime = 0;
-        this._currentItensity = 0;
+        this._currentHsv = 0;
     }
 
-    get intensity() {
-        return (this._currentItensity * this.brightness) / (0xffff * 0xffff);
+    get hsv(): [number, number, number] {
+        return [
+            (this._currentHsv >> 16) & 0xff,
+            (this._currentHsv >> 8) & 0xff,
+            (this._currentHsv) & 0xff
+        ]
     }
 
     update(now: number) {
-        // check iteration count
-        const currentIteration = this.currentIteration;
-        const maxIterations = this.maxIterations;
-
-        if (currentIteration > maxIterations)
-            return;
-
-        // don't animate when brightness is 0
-        const brightness = this.brightness;
-        if (brightness === 0)
-            return;
-
         // grab current step
         const steps = this.steps;
         if (!steps?.length) {
-            this._currentItensity = 0;
+            // nothing to do
             return;
         }
 
         // find the step we are in
         if (this._currentStepStartTime == 0)
             this._currentStepStartTime = now;
-        let iteration = currentIteration;
 
         while (this._currentStep < steps.length) {
-            const [, duration] = steps[this._currentStep];
+            const [h, s, v, duration8] = steps[this._currentStep];
+            const duration = duration8 << 3;
             if (duration === 0)
                 break;
             const elapsed = now - this._currentStepStartTime;
@@ -68,12 +51,6 @@ export class LedAnimation extends JDEventSource {
 
             // restart iteration if needed
             if (this._currentStep === steps.length - 1) {
-                iteration++;
-
-                // done iterating?
-                if (iteration >= maxIterations)
-                    break;
-
                 // restart
                 this._currentStep = 0;
                 this._currentStepStartTime = now;
@@ -86,15 +63,21 @@ export class LedAnimation extends JDEventSource {
 
         // render
         if (this._currentStep < steps.length) {
-            const [startIntensity, duration] = steps[this._currentStep];
-            const [endItensity,] = steps[(this._currentStep + 1) % steps.length]
+            const [startHue, startSat, startValue, duration8] = steps[this._currentStep];
+            const duration = duration8 << 3;
+            const [endHue, endSat, endValue,] = steps[(this._currentStep + 1) % steps.length]
 
             const elapsed = now - this._currentStepStartTime;
-            const alpha = elapsed / duration;
-            const intensity = (1 - alpha) * startIntensity + alpha * endItensity;
+            const alpha = elapsed / (duration);
+            const oneAlpha = 1 - alpha;
 
-            if (intensity !== this._currentItensity) {
-                this._currentItensity = intensity;
+            const h = oneAlpha * startHue + alpha * endHue;
+            const s = oneAlpha * startSat + alpha * endSat;
+            const v = oneAlpha * startValue + alpha * endValue;
+            const hsv = ((h & 0xff) << 16) | ((s & 0xff) << 8) | (v & 0xff)
+
+            if (hsv !== this._currentHsv) {
+                this._currentHsv = hsv;
                 this.emit(CHANGE);
             }
         }
@@ -105,35 +88,31 @@ export default class LEDServiceHost extends JDServiceHost {
     readonly steps: JDRegisterHost<LedAnimationStepsType>;
     readonly brightness: JDRegisterHost<[number]>;
     readonly maxPower: JDRegisterHost<[number]>;
-    readonly maxSteps: JDRegisterHost<[number]>;
-    readonly currentIteration: JDRegisterHost<[number]>;
-    readonly maxIterations: JDRegisterHost<[number]>;
+    readonly ledCount: JDRegisterHost<[number]>;
+    readonly luminousIntensity: JDRegisterHost<[number]>;
+    readonly waveLength: JDRegisterHost<[number]>;
+    readonly variant: JDRegisterHost<[LedVariant]>;
 
-    constructor() {
+    constructor(options?: {
+        ledCount?: number,
+        brightness?: number,
+        variant?: LedVariant,
+        luminousIntensity?: number,
+        waveLength?: number,
+        steps?: LedAnimationFrame[]
+    }) {
         super(SRV_LED);
+        const { ledCount, variant, brightness,
+            luminousIntensity, waveLength, steps } = options || {};
 
-        this.steps = this.addRegister<LedAnimationStepsType>(LedReg.Steps, [
-            [
-                [0, 1000],
-                [0xffff, 1000],
-            ]
-        ]);
-        this.brightness = this.addRegister(LedReg.Brightness, [0xffff]);
+        this.steps = this.addRegister<LedAnimationStepsType>(LedReg.Steps, [steps || []])
+        this.brightness = this.addRegister(LedReg.Brightness, [brightness || 0.5]);
         this.maxPower = this.addRegister(LedReg.MaxPower, [200]);
-        this.maxSteps = this.addRegister(LedReg.MaxSteps, [10]);
-        this.currentIteration = this.addRegister(LedReg.CurrentIteration, [0]);
-        this.maxIterations = this.addRegister(LedReg.MaxIterations, [0xffff]);
-
-        this.steps.on(REPORT_UPDATE, this.handleSteps.bind(this));
-    }
-
-    private handleSteps() {
-        // drop extras steps
-        const [maxSteps] = this.maxSteps.values();
-        const [steps] = this.steps.values();
-        if (steps.length > maxSteps)
-            this.steps.setValues([steps.slice(0, maxSteps)], false);
-        // reset counter
-        this.currentIteration.setValues([0])
+        this.ledCount = this.addRegister(LedReg.LedCount, [ledCount || 1]);
+        if (luminousIntensity !== undefined)
+            this.luminousIntensity = this.addRegister(LedReg.LuminousIntensity, [luminousIntensity])
+        if (waveLength !== undefined)
+            this.waveLength = this.addRegister(LedReg.WaveLength, [waveLength]);
+        this.variant = this.addRegister(LedReg.Variant, [variant || LedVariant.ThroughHole])
     }
 }
