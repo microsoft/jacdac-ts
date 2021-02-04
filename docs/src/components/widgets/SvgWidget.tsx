b3dd09f40d21e79e64fe900915d4b351acc9a32f
peli <pelikhan@users.noreply.github.com>
2021-02-04 14:14:24 -0800

    potentail fix

--- docs/src/components/widgets/SvgWidget.tsx
+++ docs/src/components/widgets/SvgWidget.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from "react";
 export default function SvgWidget(props: {
     width: number,
     height?: number,
-    size?: string,
+    size: string,
     role?: string,
     title?: string,
     viewBox?: string,
@@ -16,7 +16,7 @@ export default function SvgWidget(props: {
         xmlns="http://www.w3.org/2000/svg"
         tabIndex={tabIndex}
         viewBox={viewBox || `0 0 ${width} ${h}`}
-        style={size && { height: size, maxWidth: "100%" }}
+        style={size ? { height: size, maxWidth: "100%" } : undefined}
         aria-label={title}
         role={role || "group"}>
         {children}
