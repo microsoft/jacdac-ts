3310ccda7f34e90f56f069c122e6ec422c7f6817
Michal Moskal <michal@moskal.me>
2021-02-05 09:11:32 -0800

    Port fix from the other deviceSort()

--- docs/src/components/dashboard/Dashboard.tsx
+++ docs/src/components/dashboard/Dashboard.tsx
@@ -16,11 +16,11 @@ import AddIcon from '@material-ui/icons/Add';
 
 function deviceSort(l: JDDevice, r: JDDevice): number {
     const srvScore = (srv: jdspec.ServiceSpec) => srv.packets
-        .reduce((prev, pkt) => prev + (isReading(pkt) ? 10 : isValueOrIntensity(pkt) ? 1 : 0), 0)
+        .reduce((prev, pkt) => prev + (isReading(pkt) ? 10 : isValueOrIntensity(pkt) ? 1 : 0), 0) || 0;
     const score = (srvs: jdspec.ServiceSpec[]) => srvs.reduce((prev, srv) => srvScore(srv), 0)
 
-    const ls = score(l.services().slice(1).map(srv => srv.specification))
-    const rs = score(r.services().slice(1).map(srv => srv.specification))
+    const ls = score(l.services().slice(1).map(srv => srv.specification).filter(spec => !!spec))
+    const rs = score(r.services().slice(1).map(srv => srv.specification).filter(spec => !!spec))
     if (ls !== rs)
         return -ls + rs;
     return strcmp(l.deviceId, r.deviceId);
