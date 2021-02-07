4de45ca071018af65f1f4ea62da8d22fe9934279
pelikhan <jhalleux@microsoft.com>
2021-02-07 09:33:44 +0100

    highlighted color text

---
+++ docs/src/components/ui/HighlightTextField.tsx
@@ -0,0 +1,47 @@
+import React, { useContext, useRef } from 'react'
+import Highlight, { defaultProps, Language, PrismTheme } from 'prism-react-renderer'
+// tslint:disable-next-line: no-submodule-imports match-default-export-name
+import LIGHT_THEME from 'prism-react-renderer/themes/github';
+// tslint:disable-next-line: no-submodule-imports match-default-export-name
+import DARK_THEME from 'prism-react-renderer/themes/vsDark';
+import DarkModeContext from './DarkModeContext';
+import { useEditable } from "use-editable"
+
+export default function HighlightTextField(props: {
+    language: string,
+    code: string,
+    onChange: (newValue: string) => void
+}) {
+    const { code, onChange, language } = props;
+    const { darkMode } = useContext(DarkModeContext)
+    const theme = (darkMode === "dark" ? DARK_THEME : LIGHT_THEME) as PrismTheme;
+    const editorRef = useRef(null);
+
+    useEditable(editorRef, onChange, {
+        disabled: false,
+        indentation: 4
+    });
+
+    return (
+        <Highlight {...defaultProps}
+            code={code}
+            language={language as Language}
+            theme={theme}
+        >
+            {({ className, style, tokens, getLineProps, getTokenProps }) => (
+                <pre ref={editorRef} className={className} spellCheck={false} style={{ ...style, ...{ minHeight: "12rem" } }}>
+                    {tokens.map((line, i) => (
+                        <React.Fragment key={i}>
+                            {line
+                                .filter((token) => !token.empty)
+                                .map((token, key) => (
+                                    <span {...getTokenProps({ token, key })} />
+                                ))}
+                            {i < tokens.length - 1 ? "\n" : null}
+                        </React.Fragment>
+                    ))}
+                </pre>
+            )}
+        </Highlight>
+    )
+}
