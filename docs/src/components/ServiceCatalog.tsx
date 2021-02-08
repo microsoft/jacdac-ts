bd75152e9e60e3470afa2e0cdf5b408bbaf3fe5d
pelikhan <jhalleux@microsoft.com>
2021-02-08 22:33:15 +0100

    add tag filter

--- docs/src/components/ServiceCatalog.tsx
+++ docs/src/components/ServiceCatalog.tsx
@@ -1,15 +1,16 @@
-import { Grid, InputAdornment, TextField } from "@material-ui/core";
-import React, { useCallback, useMemo, useState } from "react";
+import { Box, Chip, Grid, InputAdornment, TextField } from "@material-ui/core";
+import React, { useMemo, useState } from "react";
 import ServiceSpecificationList from "./ServiceSpecificationList";
 import { useDebounce } from 'use-debounce';
-import { serviceSpecifications } from "../../../src/jacdac";
+import { arrayConcatMany, serviceSpecifications, unique } from "../../../src/jacdac";
 import SearchIcon from '@material-ui/icons/Search';
-import ClearIcon from '@material-ui/icons/Clear';
-import IconButtonWithTooltip from "./ui/IconButtonWithTooltip";
 
-export default function ServiceCatalog(props: {}) {
+export default function ServiceCatalog() {
     const [query, setQuery] = useState("");
     const [dquery] = useDebounce(query, 500);
+    const [tags, setTags] = useState<string[]>([]);
+    const allTags = useMemo(() => unique(arrayConcatMany(serviceSpecifications().map(srv => srv.tags))), [])
+    console.log({ allTags })
     const services = useMemo(() => {
         const m = dquery.toLowerCase();
         let r = serviceSpecifications();
@@ -17,13 +18,23 @@ export default function ServiceCatalog(props: {}) {
             const filter = (s: string) => s?.toLowerCase().indexOf(m) > -1;
             r = r.filter(srv => filter(srv.name) || filter(srv.notes["short"]));
         }
+        if (tags.length) {
+            r = r.filter(srv => tags.every(tag => srv.tags.indexOf(tag) > -1))
+        }
         return r;
-    }, [dquery]);
+    }, [dquery, tags]);
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setQuery(event.target.value)
     }
+    const handleClick = (tag: string) => () => {
+        const i = tags.indexOf(tag);
+        if (i < 0)
+            setTags([...tags, tag]);
+        else
+            setTags([...tags.slice(0, i), ...tags.slice(i + 1)])
+    }
 
-    return <Grid container>
+    return <Grid container spacing={1}>
         <Grid item xs={12}>
             <TextField
                 margin="normal"
@@ -42,6 +53,10 @@ export default function ServiceCatalog(props: {}) {
                     ),
                 }}
             />
+        </Grid>
+        <Grid item xs={12}>
+            {allTags.map(tag => <Box component="span" key={tag} m={0.5}><Chip label={tag} onClick={handleClick(tag)}
+                color={tags.indexOf(tag) > -1 ? "primary" : undefined} /></Box>)}
         </Grid>
         {!services.length && <Grid item>There are no services matching this request.</Grid>}
         <Grid item xs={12}>
