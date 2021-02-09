7421491368d58558f3634b83e14a3641c900c1db
pelikhan <jhalleux@microsoft.com>
2021-02-09 09:46:08 +0100

    support for off register in compass

--- docs/src/components/dashboard/DashboardCompass.tsx
+++ docs/src/components/dashboard/DashboardCompass.tsx
@@ -9,12 +9,16 @@ import { useId } from "react-use-id-hook"
 import { Grid, Slider } from "@material-ui/core";
 import SensorServiceHost from "../../../../src/hosts/sensorservicehost";
 import { CompassReg } from "../../../../src/jdom/constants";
+import PowerButton from "../widgets/PowerButton";
 
 export default function DashboardCompass(props: DashboardServiceProps) {
     const { service, services, variant } = props;
 
     const headingRegister = service.register(CompassReg.Heading);
+    const enabledRegister = service.register(CompassReg.Enabled);
     const [heading] = useRegisterUnpackedValue<[number]>(headingRegister)
+    const [enabled] = useRegisterUnpackedValue<[boolean]>(enabledRegister)
+    const off = !enabled;
 
     const host = useServiceHost<SensorServiceHost<[number]>>(service);
     const color = host ? "secondary" : "primary";
@@ -32,24 +36,31 @@ export default function DashboardCompass(props: DashboardServiceProps) {
     const cx = w >> 1
     const cy = h >> 1
     const sp = 1;
+    const pr = 6;
+    const pri = 4;
 
     const handleChange = async (ev: unknown, newValue: number | number[]) => {
         await host?.reading.setValues([newValue as number])
         headingRegister.refresh()
     }
     const valueDisplay = (angle: number) => `${Math.round(angle)}°`;
+    const toggleOff = () => enabledRegister.sendSetBoolAsync(off, true);
 
     return <Grid container direction="column">
         <Grid item xs={12}>
             <SvgWidget width={w} height={h} size={widgetSize}>
                 <circle cx={w >> 1} cy={h >> 1} r={r} fill={controlBackground}
                     stroke={background} strokeWidth={sw} />
-                <g transform={`rotate(${heading}, ${w >> 1}, ${h >> 1})`}>
+                <g transform={`rotate(${off ? 0 : heading}, ${w >> 1}, ${h >> 1})`}>
                     <path d={`M ${cx - mw} ${cy + sp / 2} l ${mw} ${r} l ${mw} ${-r} z`} 
                         fill={background} stroke={background} strokeWidth={sp} />
                     <path d={`M ${cx - mw} ${cy - sp / 2} l ${mw} ${-r} l ${mw} ${r} z`} 
                         stroke={background} fill={active} strokeWidth={sp} />
                 </g>
+                <PowerButton r={pr} ri={pri} cx={cx} cy={cy}
+                    color={color}
+                    strokeWidth={1.5}
+                    off={off} onClick={host && toggleOff} />
             </SvgWidget >
         </Grid>
         {host && <Grid item>
