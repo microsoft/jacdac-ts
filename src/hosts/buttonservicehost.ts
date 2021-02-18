22e47df215fec37ca75af4c8c7ee87262acef464
Michal Moskal <michal@moskal.me>
2021-02-18 12:14:51 -0800

    Adapt to new button behavior; fix missing click

--- src/hosts/buttonservicehost.ts
+++ src/hosts/buttonservicehost.ts
@@ -2,26 +2,30 @@ import { ButtonEvent, REFRESH, SRV_BUTTON } from "../jdom/constants";
 import SensorServiceHost from "./sensorservicehost";
 
 const LONG_CLICK_DELAY = 500
-const CLICK_DELAY = 100
 const HOLD_DELAY = 1500
 
 export default class ButtonServiceHost extends SensorServiceHost<[boolean]> {
     private _downTime: number;
-    private _held: boolean;
+    private _held = false;
+    private _longClick = false;
 
     constructor(instanceName?: string) {
         super(SRV_BUTTON, { instanceName, readingValues: [false], streamingInterval: 50 });
-        this._downTime = undefined;
-        this._held = false;
-
         this.on(REFRESH, this.handleRefresh.bind(this));
     }
 
     private async handleRefresh() {
         const [v] = this.reading.values();
-        if (v && !this._held && this.device.bus.timestamp - this._downTime > HOLD_DELAY) {
-            this._held = true;
-            await this.sendEvent(ButtonEvent.Hold);
+        if (v) {
+            const delay = this.device.bus.timestamp - this._downTime;
+            if (!this._longClick && delay > LONG_CLICK_DELAY) {
+                this._longClick = true;
+                await this.sendEvent(ButtonEvent.LongClick);
+            }
+            if (!this._held && delay > HOLD_DELAY) {
+                this._held = true;
+                await this.sendEvent(ButtonEvent.Hold);
+            }
         }
     }
 
@@ -30,6 +34,7 @@ export default class ButtonServiceHost extends SensorServiceHost<[boolean]> {
         if (v) return;
         this._downTime = this.device.bus.timestamp;
         this._held = false;
+        this._longClick = false;
         this.reading.setValues([true]);
         await this.sendEvent(ButtonEvent.Down);
     }
@@ -43,11 +48,7 @@ export default class ButtonServiceHost extends SensorServiceHost<[boolean]> {
 
         // generate clicks
         if (this._downTime !== undefined) {
-            const dt = upTime - this._downTime;
-            this._downTime = undefined;
-            if (dt > LONG_CLICK_DELAY)
-                await this.sendEvent(ButtonEvent.LongClick);
-            else if (dt > CLICK_DELAY)
+            if (!this._longClick)
                 await this.sendEvent(ButtonEvent.Click);
         }
     }
