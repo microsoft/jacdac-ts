c6ef4dd8db9d6471c7319cc622882f26347aad36
pelikhan <jhalleux@microsoft.com>
2021-02-23 22:36:18 +0100

    updated rng service

--- docs/src/components/dashboard/DashboardRandomNumberGenerator.tsx
+++ docs/src/components/dashboard/DashboardRandomNumberGenerator.tsx
@@ -7,33 +7,25 @@ import Packet from "../../../../src/jdom/packet";
 import RefreshIcon from '@material-ui/icons/Refresh';
 import IconButtonWithTooltip from "../ui/IconButtonWithTooltip";
 import useMounted from "../hooks/useMounted";
-import { RngCmd } from "../../../../src/jdom/constants";
 import { jdpack } from "../../../../src/jdom/pack";
 import { toHex } from "../../../../src/jdom/utils";
+import { useRegisterUnpackedValue } from "../../jacdac/useRegisterValue";
+import { RngReg } from "../../../../src/jdom/constants";
 
 export default function DashboardRandomNumberGenerator(props: DashboardServiceProps) {
     const { service } = props;
-    const [rnd, setRnd] = useState<Uint8Array>(undefined);
+    const randomRegister = service.register(RngReg.Random);
+    const [rnd] = useRegisterUnpackedValue<[Uint8Array]>(randomRegister);
 
-    const n = 8;
-
-    const mounted = useMounted();
-
-    const handleRefresh = async () => {
-        const pkt = Packet.from(RngCmd.Random, jdpack<[number]>("u8", [n]));
-        const resp = await service.sendCmdAwaitResponseAsync(pkt)
-        if (mounted())
-            setRnd(resp.data);
-    }
+    const handleRefresh = () => randomRegister.refresh();
 
     return <Grid container spacing={2} direction="row">
         <Grid item xs>
             <TextField
-                disabled={true}
                 fullWidth={true}
                 variant={"outlined"}
                 helperText={"generated random number"}
-                value={toHex(rnd)} />
+                value={toHex(rnd?.slice(0, 8))} />
         </Grid>
         <Grid item>
             <IconButtonWithTooltip title="generate new number" onClick={handleRefresh}>
--- src/hosts/randomnumbergeneratorservicehost.ts
+++ src/hosts/randomnumbergeneratorservicehost.ts
@@ -1,29 +1,22 @@
-import { RngCmd, RngReg, RngVariant, SRV_RNG } from "../jdom/constants";
-import Packet from "../jdom/packet";
-import ServiceHost from "../jdom/servicehost";
-import { delay, randomRange } from "../jdom/utils";
+import { REFRESH, RngVariant, SRV_RNG } from "../jdom/constants";
+import SensorServiceHost from "./sensorservicehost";
 
-export default class RandomNumberGeneratorServiceHost extends ServiceHost {
+export default class RandomNumberGeneratorServiceHost extends SensorServiceHost<[Uint8Array]> {
     constructor() {
         super(SRV_RNG, {
-            variant: RngVariant.WebCrypto
+            variant: RngVariant.WebCrypto,
+            streamingInterval: 10000,
         })
 
-        this.addCommand(RngCmd.Random, this.handleRandom.bind(this));
+        this.on(REFRESH, this.handleRefresh.bind(this));
     }
 
-    private async handleRandom(pkt: Packet) {
-        const [length] = pkt.jdunpack<[number]>("u8");
-
-        // simulate delay
-        await delay(randomRange(1, 30));
-
-        // respond
-        const data = new Uint8Array(length);
+    private handleRefresh() {
+        // generate new data
+        const data = new Uint8Array(64);
         console.log("rnd", { data })
         if (typeof window !== "undefined")
             window.crypto.getRandomValues(data);
-        const resp = Packet.from(RngCmd.Random, data);
-        await this.sendPacketAsync(resp);
+        this.reading.setValues([data], true);
     }
 }
