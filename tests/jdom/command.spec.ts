970f8a7ff089a3020e174834db23d59fcf9773f7
pelikhan <jhalleux@microsoft.com>
2021-02-24 20:07:07 +0100

    using types

--- tests/jdom/command.spec.ts
+++ tests/jdom/command.spec.ts
@@ -1,11 +1,13 @@
+/// <reference path="../../dist/types/src/jdom/jacdac-jdom.d.ts" />
+
 import { describe, it } from "mocha"
 import {
     packArguments, serviceSpecificationFromClassIdentifier,
-    ProtoTestCmd, SRV_PROTO_TEST, ArgType
+    ProtoTestCmd, SRV_PROTO_TEST
 } from "../../dist/jacdac-jdom.cjs";
 
 describe('packArguments', () => {
-    function testOne(cmdid: number, args: ArgType[]) {
+    function testOne(cmdid: number, args) {
         const service = serviceSpecificationFromClassIdentifier(SRV_PROTO_TEST);
         const cmd = service.packets.find(pkt => pkt.kind === 'command' && pkt.identifier === cmdid);
         const pkt = packArguments(cmd, args);
@@ -14,7 +16,7 @@ describe('packArguments', () => {
     it("cbool", () => testOne(ProtoTestCmd.CBool, [true]));
     it("c32", () => testOne(ProtoTestCmd.CU32, [42]));
     it("cString", () => testOne(ProtoTestCmd.CString, ["hi"]));
-    it("cI8U8U16I32", () => testOne(ProtoTestCmd.CI8U8U16I32, [-1,2,3,4]));
+    it("cI8U8U16I32", () => testOne(ProtoTestCmd.CI8U8U16I32, [-1, 2, 3, 4]));
     // failing test
     //it("cU8String", () => testOne(ProtoTestCmd.CU8String, [42, "hi"]));
 })
