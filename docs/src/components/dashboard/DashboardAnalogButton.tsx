dd0298a3b3bfc42aa6dab95f831641fd298cabce
peli <pelikhan@users.noreply.github.com>
2021-02-25 12:12:20 -0800

    better layout

--- docs/src/components/dashboard/DashboardAnalogButton.tsx
+++ docs/src/components/dashboard/DashboardAnalogButton.tsx
@@ -20,7 +20,7 @@ export default function DashboardAnalogButton(props: DashboardServiceProps) {
     const [pressure] = useRegisterUnpackedValue<[number]>(pressureRegister)
     const [activeThreshold] = useRegisterUnpackedValue<[number]>(service.register(AnalogButtonReg.ActiveThreshold))
     //const [buttonVariant] = useRegisterUnpackedValue<[AnalogButtonVariant]>(service.register(AnalogButtonReg.Variant));
-    const widgetSize = `clamp(5em, 25vw, 18em)`
+    const widgetSize = `clamp(5em, 25vw, 100%)`
     const host = useServiceHost<AnalogSensorServiceHost>(service);
     const [down, setDown] = useState(false)
     const color = host ? "secondary" : "primary";
