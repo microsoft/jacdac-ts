8552ac0f1b12db394698888bbf3cac9d6e862744
pelikhan <jhalleux@microsoft.com>
2021-02-26 23:39:35 +0100

    test ui cleanout

--- docs/src/components/ServiceTest.tsx
+++ docs/src/components/ServiceTest.tsx
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from "react"
-import { Link } from "gatsby-theme-material-ui"
 import useGridBreakpoints from "./useGridBreakpoints"
 import JacdacContext, { JacdacContextProps } from "../jacdac/Context"
 import useChange from "../jacdac/useChange"
@@ -28,27 +27,96 @@ import {
     addHost,
     hostDefinitionFromServiceClass,
 } from "../../../src/hosts/hosts"
-import KindIcon from "./KindIcon"
 import IconButtonWithTooltip from "./ui/IconButtonWithTooltip"
 // tslint:disable-next-line: match-default-export-name no-submodule-imports
-import InfoIcon from '@material-ui/icons/Info';
+import InfoIcon from "@material-ui/icons/Info"
+
+function SelectService(props: {
+    serviceClass: number
+    onSelect: (service: JDService) => void
+}) {
+    const { bus } = useContext<JacdacContextProps>(JacdacContext)
+    const { serviceClass, onSelect } = props
+    const services = useChange(bus, n => n.services({ serviceClass }), [])
+    const gridBreakpoints = useGridBreakpoints()
+
+    const handleSelect = (service: JDService) => () => onSelect(service)
+
+    return (
+        <>
+            {!!services.length && (
+                <Grid container spacing={2}>
+                    {services.map(service => (
+                        <Grid key={service.id} item {...gridBreakpoints}>
+                            <Card>
+                                <DeviceCardHeader device={service.device} />
+                                <CardActions>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={handleSelect(service)}
+                                    >
+                                        Select
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            )}
+            {!services?.length && (
+                <Alert severity="info">
+                    Not seeing your device? Try some of the following.
+                    <ul>
+                        <li>Check that your device is connected</li>
+                        <li>
+                            Use the <strong>packet console</strong> to monitor
+                            packets on the bus
+                        </li>
+                        <li>
+                            Check the class identifier in your annoucement
+                            packets
+                        </li>
+                    </ul>
+                </Alert>
+            )}
+        </>
+    )
+}
+
+function Diagnostics(props: { serviceClass: number }) {
+    const { serviceClass } = props
+    const { bus } = useContext<JacdacContextProps>(JacdacContext)
+
+    const hostDefinition = hostDefinitionFromServiceClass(serviceClass)
+    const handleStartSimulator = () => addHost(bus, hostDefinition.services())
+
+    if (!hostDefinition)
+        return null;
+
+    return (
+        <Alert severity="info">
+            <AlertTitle>Developer zone</AlertTitle>
+            <Button variant="outlined" onClick={handleStartSimulator}>
+                start simulator
+            </Button>
+        </Alert>
+    )
+}
 
 export default function ServiceTest(props: {
     serviceSpec: jdspec.ServiceSpec
 }) {
     const { serviceSpec } = props
     const { classIdentifier: serviceClass } = serviceSpec
-    const { bus } = useContext<JacdacContextProps>(JacdacContext)
+    const serviceTest = serviceTestFromServiceSpec(serviceSpec)
     const [selectedService, setSelectedService] = useState<JDService>(undefined)
     const [activeStep, setActiveStep] = useState(0)
     const [activeTest, setActiveTest] = useState(-1)
-    const serviceTest = serviceTestFromServiceSpec(serviceSpec)
-    const testStatuses = serviceTest.tests.map(() => TestStatus.Inactive)
-    const gridBreakpoints = useGridBreakpoints()
-    const hostDefinition = hostDefinitionFromServiceClass(serviceClass)
+    //const testStatuses = serviceTest.tests.map(() => TestStatus.Inactive)
+
     // devices that implement serviceSpec
-    const services = useChange(bus, n => n.services({ serviceClass }), [])
-    const handleSelect = (service: JDService) => () => {
+    const handleSelect = (service: JDService) => {
         setSelectedService(service)
         handleNext()
     }
@@ -58,18 +126,17 @@ export default function ServiceTest(props: {
     const handleNext = () => {
         setActiveStep(prevActiveStep => prevActiveStep + 1)
     }
-    const handleBack = () => {
-        setActiveStep(prevActiveStep => prevActiveStep - 1)
-    }
+    //const handleBack = () => {
+    //    setActiveStep(prevActiveStep => prevActiveStep - 1)
+    //}
     const handleReset = () => {
         setActiveStep(0)
     }
-    const handleClose = (status: TestStatus) => {
-        if (activeStep != -1) testStatuses[activeStep] = status
-        setActiveTest(t => -1)
+    const handleClose = () => {
+        //if (activeStep != -1) testStatuses[activeStep] = status
+        setActiveTest(() => -1)
         handleNext()
     }
-    const handleStartSimulator = () => addHost(bus, hostDefinition.services())
 
     return (
         <>
@@ -79,76 +146,24 @@ export default function ServiceTest(props: {
                     <InfoIcon />
                 </IconButtonWithTooltip>
             </h1>
-            {Flags.diagnostics && hostDefinition && (
-                <Alert severity="info">
-                    <AlertTitle>Developer zone</AlertTitle>
-                    <Button variant="outlined" onClick={handleStartSimulator}>
-                        start simulator
-                    </Button>
-                </Alert>
-            )}
+            {Flags.diagnostics && <Diagnostics serviceClass={serviceClass} />}
             <Grid container spacing={2}>
                 <Grid item xs>
                     <Stepper activeStep={activeStep} orientation="vertical">
                         <Step key="device">
                             <StepLabel>Select a device to test</StepLabel>
                             <StepContent>
-                                {!!services.length && (
-                                    <Grid container spacing={2}>
-                                        {services.map(service => (
-                                            <Grid
-                                                key={service.id}
-                                                item
-                                                {...gridBreakpoints}
-                                            >
-                                                <Card>
-                                                    <DeviceCardHeader
-                                                        device={service.device}
-                                                    />
-                                                    <CardActions>
-                                                        <Button
-                                                            variant="contained"
-                                                            color="primary"
-                                                            onClick={handleSelect(
-                                                                service
-                                                            )}
-                                                        >
-                                                            Select
-                                                        </Button>
-                                                    </CardActions>
-                                                </Card>
-                                            </Grid>
-                                        ))}
-                                    </Grid>
-                                )}
-                                {!services?.length && (
-                                    <Alert severity="info">
-                                        Not seeing your device? Try some of the
-                                        following.
-                                        <ul>
-                                            <li>
-                                                Check that your device is
-                                                connected
-                                            </li>
-                                            <li>
-                                                Use the{" "}
-                                                <strong>packet console</strong>{" "}
-                                                to monitor packets on the bus
-                                            </li>
-                                            <li>
-                                                Check the class identifier in
-                                                your annoucement packets
-                                            </li>
-                                        </ul>
-                                    </Alert>
-                                )}
+                                <SelectService
+                                    serviceClass={serviceClass}
+                                    onSelect={handleSelect}
+                                />
                             </StepContent>
                         </Step>
-                        {serviceTest.tests.map((test, index) => (
+                        {serviceTest?.tests.map((test, index) => (
                             <Step key={index}>
                                 <StepLabel>{test.description}</StepLabel>
                                 <StepContent>
-                                    {index != activeTest && (
+                                    {index !== activeTest && (
                                         <Button
                                             onClick={handleStartTest(index)}
                                         >
