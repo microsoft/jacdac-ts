057b9b6de5d79ddf955cac037b96fe8492fe44ff
pelikhan <jhalleux@microsoft.com>
2021-02-28 22:42:56 +0100

    rename device use contenteditable

--- docs/src/components/DeviceName.tsx
+++ docs/src/components/DeviceName.tsx
@@ -1,7 +1,8 @@
 import { Typography } from "@material-ui/core";
-import React from "react"
+import React, { useRef } from "react"
 import { JDDevice } from "../../../src/jdom/device";
 import useDeviceName from "./useDeviceName";
+import { useEditable } from "use-editable"
 
 export default function DeviceName(props: {
     device: JDDevice,
@@ -10,11 +11,15 @@ export default function DeviceName(props: {
     showShortId?: boolean
 }) {
     const { device, serviceIndex, showShortId } = props
-    const name = useDeviceName(device)
+    const name = useDeviceName(device) || ""
+    const nameRef = useRef<HTMLSpanElement>()
     const { shortId } = device
 
+    useEditable(nameRef, (newName) => { device.name = newName }, { disabled: !device });
+
     return <span>
-        {name || (showShortId && shortId)}
+        <span ref={nameRef}>{name}</span>
+        {!name && showShortId && shortId}
         {showShortId && name && name !== shortId &&
             <Typography component="span" variant="body2"> {shortId}</Typography>}
         {serviceIndex !== undefined && `[${serviceIndex}]`}
