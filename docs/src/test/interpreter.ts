2604054665d50684f8e6cf533604aab47523f89f
Tom Ball <tballmsft@users.noreply.github.com>
2021-02-28 20:44:05 -0800

    start on interpreter

---
+++ docs/src/test/interpreter.ts
@@ -0,0 +1,9 @@
+import { testCommandFunctions } from "../../../jacdac-spec/spectool/jdtestfuns"
+
+export function cmdToTestFunction(cmd: jsep.CallExpression) {
+    let id = (<jsep.Identifier>cmd.callee).name;
+    return testCommandFunctions.find(t => t.id == id).prompt;
+}
+
+// TODO: find registers to watch
+// TODO: prompts
