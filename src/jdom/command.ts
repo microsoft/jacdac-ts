05a0fbe964231ae030bf4a53119c437fd712908b
Michal Moskal <michal@moskal.me>
2021-03-01 16:59:33 -0800

    Add string0 support to packArgs and fix https://github.com/microsoft/jacdac/issues/261

--- src/jdom/command.ts
+++ src/jdom/command.ts
@@ -13,26 +13,31 @@ export type ArgType = number | boolean | string | Uint8Array
 export function packArguments(info: jdspec.PacketInfo, args: ArgType[]) {
     let repeatIdx = -1
     let numReps = 0
-    const argIdx = 0
+    let argIdx = 0
     let dst = 0
 
     const buf = new Uint8Array(256)
 
-    for (let i = 0; i < info.fields.length; ++i) {
+    for (let i = 0; i < info.fields.length; ++argIdx, ++i) {
         if (argIdx >= args.length && numReps > 0) break
         const arg0 = argIdx < args.length ? args[argIdx] : 0
         const fld = info.fields[i]
 
         if (repeatIdx == -1 && fld.startRepeats) repeatIdx = i
 
+        const arg1 =
+            fld.type == "string0" && typeof arg0 == "string"
+                ? arg0 + "\u0000"
+                : arg0
+
         const arg =
-            typeof arg0 == "boolean"
-                ? arg0
+            typeof arg1 == "boolean"
+                ? arg1
                     ? 1
                     : 0
-                : typeof arg0 == "string"
-                ? stringToUint8Array(toUTF8(arg0))
-                : arg0
+                : typeof arg1 == "string"
+                ? stringToUint8Array(toUTF8(arg1))
+                : arg1
 
         if (typeof arg == "number") {
             const intVal = scaleFloatToInt(arg, fld)
@@ -43,7 +48,13 @@ export function packArguments(info: jdspec.PacketInfo, args: ArgType[]) {
             setNumber(buf, fmt, dst, clampToStorage(intVal, fld.storage))
             dst += sizeOfNumberFormat(fmt)
         } else {
-            if (fld.storage == 0 || Math.abs(fld.storage) == arg.length) {
+            let size = Math.abs(fld.storage)
+            if (typeof arg1 == "string") {
+                if (size == 0) size = arg.length
+                const argCut = arg.slice(0, size)
+                buf.set(argCut, dst)
+                dst += size
+            } else if (size == 0 || size == arg.length) {
                 buf.set(arg, dst)
                 dst += arg.length
             } else {
