702b1833c35c88609c55b61e36b477aa0c3d08ed
peli <pelikhan@users.noreply.github.com>
2021-03-03 07:47:53 -0800

    add extensions directly

--- docs/src/components/makecode/MakeCodeSim.tsx
+++ docs/src/components/makecode/MakeCodeSim.tsx
@@ -40,7 +40,7 @@ function Carousel() {
     const handleAdd = () => {
         // list all devices connected to the bus
         // and query for them, let makecode show the missing ones
-        const query = unique(
+        const extensions = unique(
             arrayConcatMany(
                 devices.map(device => device.services()
                     .map(srv => resolveMakecodeServiceFromClassIdentifier(srv.serviceClass))
@@ -48,13 +48,15 @@ function Carousel() {
                     .filter(q => !!q)
                 )
             )
-        ).join('|');
-        // send message to makecode
-        window.parent.postMessage({
-            type: "extensionsdialog",
-            query: query || `jacdac|${makeCodeServices().map(info => info?.client.repo).filter(r => !!r).join("|")}`,
-            broadcast: true
-        }, "*")
+        );
+        if (extensions?.length) {
+            // send message to makecode
+            window.parent.postMessage({
+                type: "addextensions",
+                extensions,
+                broadcast: true
+            }, "*")
+        }
     }
 
     return <Grid container alignItems="flex-start" spacing={1}>
