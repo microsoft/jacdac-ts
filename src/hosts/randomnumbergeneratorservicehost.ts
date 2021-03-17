83f83ffb081c2911cdbdf54a08abd38421ccae4f
Tom Ball <tballmsft@users.noreply.github.com>
2021-03-17 11:15:37 -0700

    merge

--- src/hosts/randomnumbergeneratorservicehost.ts
+++ src/hosts/randomnumbergeneratorservicehost.ts
@@ -1,22 +1,27 @@
-import { REFRESH, RngVariant, SRV_RNG } from "../jdom/constants";
-import SensorServiceHost from "./sensorservicehost";
+import {
+    REGISTER_PRE_GET,
+    RngReg,
+    RngVariant,
+    SRV_RNG,
+} from "../jdom/constants"
+import RegisterHost from "../jdom/registerhost"
+import ServiceHost from "../jdom/servicehost"
 
-export default class RandomNumberGeneratorServiceHost extends SensorServiceHost<[Uint8Array]> {
+export default class RandomNumberGeneratorServiceHost extends ServiceHost {
+    readonly reading: RegisterHost<[Uint8Array]>
     constructor() {
         super(SRV_RNG, {
             variant: RngVariant.WebCrypto,
-            streamingInterval: 10000,
         })
 
-        this.on(REFRESH, this.handleRefresh.bind(this));
+        this.reading = this.addRegister(RngReg.Random, [new Uint8Array(64)])
+        this.reading.on(REGISTER_PRE_GET, this.handleRefresh.bind(this))
     }
 
     private handleRefresh() {
         // generate new data
-        const data = new Uint8Array(64);
-        console.log("rnd", { data })
-        if (typeof window !== "undefined")
-            window.crypto.getRandomValues(data);
-        this.reading.setValues([data], true);
+        const data = new Uint8Array(64)
+        if (typeof window !== "undefined") window.crypto.getRandomValues(data)
+        this.reading.setValues([data], true)
     }
 }
