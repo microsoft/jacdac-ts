b52c4aeda077eae27e484ae38d3b831a422139ed
pelikhan <jhalleux@microsoft.com>
2021-03-19 08:37:26 +0100

    place holder for set_status_light

--- src/jdom/controlservicehost.ts
+++ src/jdom/controlservicehost.ts
@@ -1,52 +1,74 @@
-import { LedAnimationData } from "../hosts/ledservicehost";
-import { ControlAnnounceFlags, ControlCmd, ControlReg, IDENTIFY, REPORT_RECEIVE, RESET, SRV_CTRL } from "./constants";
-import Packet from "./packet";
-import RegisterHost from "./registerhost";
-import ServiceHost from "./servicehost";
+import {
+    ControlAnnounceFlags,
+    ControlCmd,
+    ControlReg,
+    IDENTIFY,
+    SRV_CTRL,
+} from "./constants"
+import Packet from "./packet"
+import RegisterHost from "./registerhost"
+import ServiceHost from "./servicehost"
 
 export default class ControlServiceHost extends ServiceHost {
-    readonly deviceDescription: RegisterHost<[string]>;
-    readonly mcuTemperature: RegisterHost<[number]>;
-    readonly resetIn: RegisterHost<[number]>;
-    readonly uptime: RegisterHost<[number]>;
-    readonly startTime: number;
+    readonly deviceDescription: RegisterHost<[string]>
+    readonly mcuTemperature: RegisterHost<[number]>
+    readonly resetIn: RegisterHost<[number]>
+    readonly uptime: RegisterHost<[number]>
+    readonly startTime: number
 
     constructor() {
         super(SRV_CTRL)
 
-        this.startTime = Date.now();
-        this.resetIn = this.addRegister<[number]>(ControlReg.ResetIn, [0]);
-        this.deviceDescription = this.addRegister<[string]>(ControlReg.DeviceDescription);
-        this.mcuTemperature = this.addRegister<[number]>(ControlReg.McuTemperature, [25]);
-        this.resetIn = this.addRegister<[number]>(ControlReg.ResetIn);
-        this.uptime = this.addRegister<[number]>(ControlReg.Uptime);
+        this.startTime = Date.now()
+        this.resetIn = this.addRegister<[number]>(ControlReg.ResetIn, [0])
+        this.deviceDescription = this.addRegister<[string]>(
+            ControlReg.DeviceDescription
+        )
+        this.mcuTemperature = this.addRegister<[number]>(
+            ControlReg.McuTemperature,
+            [25]
+        )
+        this.resetIn = this.addRegister<[number]>(ControlReg.ResetIn)
+        this.uptime = this.addRegister<[number]>(ControlReg.Uptime)
 
-        this.addCommand(ControlCmd.Services, this.announce.bind(this));
-        this.addCommand(ControlCmd.Identify, this.identify.bind(this));
-        this.addCommand(ControlCmd.Reset, this.handleReset.bind(this));
-        this.addCommand(ControlCmd.Noop, null);
+        this.addCommand(ControlCmd.Services, this.announce.bind(this))
+        this.addCommand(ControlCmd.Identify, this.identify.bind(this))
+        this.addCommand(ControlCmd.Reset, this.handleReset.bind(this))
+        this.addCommand(ControlCmd.Noop, null)
+        this.addCommand(
+            ControlCmd.SetStatusLight,
+            this.handleSetStatusLight.bind(this)
+        )
     }
 
     async announce() {
         // restartCounter, flags, packetCount, serviceClass
-        const pkt = Packet.jdpacked<[number, ControlAnnounceFlags, number, number[]]>(ControlCmd.Services, "u8 u8 u8 x[1] u32[]",
-            [
-                this.device.restartCounter,
-                ControlAnnounceFlags.SupportsACK,
-                this.device.packetCount + 1,
-                this.device.services().slice(1).map(srv => srv.serviceClass)
-            ])
+        const pkt = Packet.jdpacked<
+            [number, ControlAnnounceFlags, number, number[]]
+        >(ControlCmd.Services, "u8 u8 u8 x[1] u32[]", [
+            this.device.restartCounter,
+            ControlAnnounceFlags.SupportsACK,
+            this.device.packetCount + 1,
+            this.device
+                .services()
+                .slice(1)
+                .map(srv => srv.serviceClass),
+        ])
 
-        await this.sendPacketAsync(pkt);
+        await this.sendPacketAsync(pkt)
 
-        this.uptime.setValues([Date.now() - this.startTime], true);
+        this.uptime.setValues([Date.now() - this.startTime], true)
     }
 
     async identify() {
-        this.emit(IDENTIFY);
+        this.emit(IDENTIFY)
     }
 
     private handleReset() {
-        this.device.reset();
+        this.device.reset()
+    }
+
+    private handleSetStatusLight(pkt: Packet) {
+        // TODO
     }
 }
