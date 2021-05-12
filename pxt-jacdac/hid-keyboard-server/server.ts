23c1a08cb546e1b34be05d43a59ac9d339cca9a9
pelikhan <pelikhan@users.noreply.github.com>
2021-05-12 23:15:19 +0000

    deploy: ef9ab14185cc48d3b001095b1e8b55ad42e8de69

--- pxt-jacdac/hid-keyboard-server/server.ts
+++ pxt-jacdac/hid-keyboard-server/server.ts
@@ -12,24 +12,67 @@ namespace servers {
             if (selector >= 0x04 && selector <= 0x1d) // a-z
                 return String.fromCharCode("a".charCodeAt(0) + (selector - 0x04))
             if (selector >= 0x1e && selector <= 0x26) // 1-9
-                return String.fromCharCode("1".charCodeAt(0) + (selector - 0x1e))            
-            switch(selector) {
+                return String.fromCharCode("1".charCodeAt(0) + (selector - 0x1e))
+            switch (selector) {
                 case 0x27: return "0"
             }
             return ""
         }
 
-        handleKeyCommand(packet: jacdac.JDPacket) {
-            let upacked = packet.jdunpack<number[]>("r: u16 u8 u8")
+        private selectorToFunction(selector: number): KeyboardFunctionKey {
+            if (selector >= 0x28 && selector <= 0x73)
+                return selector
+            else
+                return 0
+        }
 
+        private selectorToMedia(selector: number): KeyboardMediaKey {
+            if (selector >= 0x74 && selector <= 0x81)
+                return selector
+            else
+                return 0
+        }
+
+        handleKeyCommand(packet: jacdac.JDPacket) {
+            const [keys] = packet.jdunpack<number[][][]>("r: u16 u8 u8")
+
+            const REPORT_DELAY = 20
             // each key press is represented by 32 bits, unpacked into three "numbers"
-            for (let i = 0; i < upacked.length / 3; i += 3) {
+            for (let i = 0; i < keys.length; i++) {
+                const upacked = keys[i]
                 const selector = upacked[i]
                 const key = this.selectorToKey(selector)
+                const fcn = this.selectorToFunction(selector)
+                const media = this.selectorToMedia(selector)
                 const modifier = upacked[i + 1] & ~0xe
                 const action = upacked[i + 2]
-                if (modifier) keyboard.modifierKey(modifier, action)
-                if (key) keyboard.key(key, action)
+
+                this.log(`mods ${modifier}`)
+                this.log(`key ${key}`)
+                this.log(`fcn ${fcn}`)
+                this.log(`media ${media}`)
+
+                if (action === jacdac.HidKeyboardAction.Press) {
+                    if (modifier) keyboard.modifierKey(modifier, KeyboardKeyEvent.Down)
+                    pause(REPORT_DELAY)
+                    if (key) keyboard.key(key, KeyboardKeyEvent.Down)
+                    if (fcn) keyboard.functionKey(fcn, KeyboardKeyEvent.Down)
+                    if (media) keyboard.mediaKey(media, KeyboardKeyEvent.Down)
+                    pause(REPORT_DELAY)
+                    if (key) keyboard.key(key, KeyboardKeyEvent.Up)
+                    if (fcn) keyboard.functionKey(fcn, KeyboardKeyEvent.Up)
+                    if (media) keyboard.mediaKey(media, KeyboardKeyEvent.Up)
+                    pause(REPORT_DELAY)
+                    if (modifier) keyboard.modifierKey(modifier, KeyboardKeyEvent.Up)
+                    pause(REPORT_DELAY)
+                } else {
+                    if (modifier) keyboard.modifierKey(modifier, action)
+                    pause(REPORT_DELAY)
+                    if (key) keyboard.key(key, action)
+                    if (fcn) keyboard.functionKey(fcn, action)
+                    if (media) keyboard.mediaKey(media, action)
+                    pause(REPORT_DELAY)
+                }
             }
         }
 
--- pxt-jacdac/hid-keyboard-server/test.ts
+++ pxt-jacdac/hid-keyboard-server/test.ts
@@ -1,1 +1,4 @@
+jacdac.consolePriority = ConsolePriority.Log
+jacdac.loggerServer.minPriority = jacdac.LoggerPriority.Debug
+game.consoleOverlay.setVisible(true)
 servers.hidKeyboardServer.start()
