8ddaa59909e0f0c4e278e4975b4d24e0aaddbe3e
pelikhan <jhalleux@microsoft.com>
2021-05-11 20:04:33 -0700

    patch: added objectToPacked

--- src/jdom/packobject.ts
+++ src/jdom/packobject.ts
@@ -1,4 +1,4 @@
-import { PackedSimpleValue, PackedValues } from "./pack"
+import { jdpack, PackedSimpleValue, PackedValues } from "./pack"
 
 export interface PackedObject {
     [index: string]: PackedSimpleValue | PackedObject | PackedObject[]
@@ -33,4 +33,34 @@ export function unpackedToObject(
         } else r[prettyName] = value
     }
     return r
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function objectToPacked(pkt: jdspec.PacketInfo, msg: any): Uint8Array {
+    if (!msg) return new Uint8Array(0)
+
+    const { fields } = pkt
+    const r: PackedValues = []
+    for (let i = 0; i < fields.length; ++i) {
+        const field = fields[i]
+        const value = msg[field.name]
+        if (field.startRepeats) {
+            const repeatFields = fields.slice(i)
+            r.push(
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                (value as any[]).map(vrow => {
+                    const arow: PackedSimpleValue[] = []
+                    for (let j = 0; j < repeatFields.length; ++j) {
+                        const rfield = repeatFields[j]
+                        arow.push(vrow[rfield.name])
+                    }
+                    return arow
+                })
+            )
+            break
+        } else {
+            r.push(value)
+        }
+    }
+    return jdpack(pkt.packFormat, r)
 }
