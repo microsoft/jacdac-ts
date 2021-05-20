883818cdb4570c20103e6f771b4a3d8a746d6860
Tom Ball <tballmsft@users.noreply.github.com>
2021-05-20 13:19:12 -0700

    reformat

--- src/vm/rolemanager.ts
+++ src/vm/rolemanager.ts
@@ -16,45 +16,58 @@ export class MyRoleManager extends JDEventSource {
 
     constructor(
         private readonly bus: JDBus,
-        private readonly notify: (role:string, service: JDService, added:boolean) => void
+        private readonly notify: (
+            role: string,
+            service: JDService,
+            added: boolean
+        ) => void
     ) {
         super()
         this.bus.on(DEVICE_ANNOUNCE, (dev: JDDevice) => this.addServices(dev))
-        this.bus.on(DEVICE_DISCONNECT, (dev: JDDevice) => this.removeServices(dev))
+        this.bus.on(DEVICE_DISCONNECT, (dev: JDDevice) =>
+            this.removeServices(dev)
+        )
     }
 
     private addServices(dev: JDDevice) {
         dev.services().forEach(s => {
-            Object.keys(this._roles).forEach(key => {
-                if (typeof(this._roles[key]) === "string" && 
-                    this.nameMatch(this._roles[key] as string, s.specification.shortName)) {
-                    if (this._devices.indexOf(dev) === -1) {
-                        this._roles[key] = s
-                        this._devices.push(dev)
-                        if (this.notify) this.notify(key, s, true)
-                    }
-                }
-            })
+            let key = Object.keys(this._roles).find(
+                k =>
+                    typeof this._roles[k] === "string" &&
+                    this.nameMatch(
+                        this._roles[k] as string,
+                        s.specification.shortName
+                    )
+            )
+            if (key && this._devices.indexOf(dev) === -1) {
+                this._roles[key] = s
+                this._devices.push(dev)
+                if (this.notify) this.notify(key, s, true)
+            }
         })
     }
 
     private removeServices(dev: JDDevice) {
         if (this._devices.indexOf(dev) >= 0) {
             this._devices = this._devices.filter(d => d !== dev)
-            Object.keys(this._roles).forEach(key => {
-                if (typeof(this._roles[key]) !== "string" &&
-                    dev.services().indexOf(this._roles[key] as JDService) >= 0) {
-                        let service = this._roles[key] as JDService
-                        this._roles[key] = (this._roles[key] as JDService).specification.shortName
-                        if (this.notify) this.notify(key, service, false)
-                }
-            })
+            let key = Object.keys(this._roles).find(
+                k =>
+                    typeof this._roles[k] !== "string" &&
+                    dev.services().indexOf(this._roles[k] as JDService) >= 0
+            )
+            if (key) {
+                let service = this._roles[key] as JDService
+                this._roles[key] = (this._roles[
+                    key
+                ] as JDService).specification.shortName
+                if (this.notify) this.notify(key, service, false)
+            }
         }
     }
-    
+
     public getService(role: string): JDService {
-        let s = this._roles[role] 
-        return (!s || typeof s === "string") ? undefined : s
+        let s = this._roles[role]
+        return !s || typeof s === "string" ? undefined : s
     }
 
     private nameMatch(n1: string, n2: string) {
@@ -62,7 +75,7 @@ export class MyRoleManager extends JDEventSource {
         const cn2 = n2.slice(0).toLowerCase().replace("_", " ").trim()
         return cn1 === cn2
     }
-    
+
     private getServiceFromName(root: string): JDService[] {
         return this.bus
             .services()
@@ -70,18 +83,24 @@ export class MyRoleManager extends JDEventSource {
     }
 
     public addRoleService(role: string, serviceShortName: string) {
-        if (this._roles[role])
+        const s = this._roles[role]
+        if (s && typeof(s) !== "string") 
             return
-        let existingInstance = Object.values(this._roles).findIndex(r =>
-                typeof r === "string" && this.nameMatch(r,serviceShortName) ||
-                typeof r === "object" && this.nameMatch(r.specification.shortName, serviceShortName)
+        let existingInstance = Object.values(this._roles).findIndex(
+            r =>
+                (typeof r === "string" &&
+                    this.nameMatch(r, serviceShortName)) ||
+                (typeof r === "object" &&
+                    this.nameMatch(r.specification.shortName, serviceShortName))
         )
         this._roles[role] = serviceShortName
         let ret = this.getServiceFromName(serviceShortName)
         if (existingInstance || ret.length === 0) {
             // spin up a new simulator
             let service = serviceSpecificationFromName(serviceShortName)
-            let provider = serviceProviderDefinitionFromServiceClass(service.classIdentifier)
+            let provider = serviceProviderDefinitionFromServiceClass(
+                service.classIdentifier
+            )
             addServiceProvider(this.bus, provider)
         } else {
             this._roles[role] = ret[0]
