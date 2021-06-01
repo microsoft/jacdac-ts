ef6258e2052416ec0a2897bcd138bb02483946ce
mmoskal <mmoskal@users.noreply.github.com>
2021-06-01 21:58:00 +0000

    deploy: e971add221655d7f3ecf51af878d79c2fb7f2595

--- src/cli/jacdac-cli.ts
+++ src/cli/jacdac-cli.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const cli = require("cli")
 const fs = require("fs-extra")
-import { DEVICE_ANNOUNCE, PACKET_PROCESS } from "../jdom/constants"
+import { DEVICE_ANNOUNCE, PACKET_PROCESS, PACKET_RECEIVE, PACKET_RECEIVE_ANNOUNCE } from "../jdom/constants"
 import { createUSBTransport } from "../jdom/transport/usb"
 import { createNodeUSBOptions } from "../jdom/transport/nodewebusb"
 import {
@@ -10,16 +10,30 @@ import {
 } from "../azure-iot/dtdl"
 import { deviceSpecifications, serviceSpecifications } from "../jdom/spec"
 import { JDBus } from "../jdom/bus"
+import { printPacket } from "../jdom/pretty"
+import { parseLogicLog, replayLog } from "../jdom/logparser"
 
 cli.setApp("jacdac", "1.0.6")
 cli.enable("version")
 
-const options = cli.parse({
+interface OptionsType {
+    usb?: boolean
+    packets?: boolean
+    dtdl?: boolean
+    devices?: string
+    services?: string
+    rm?: boolean
+    parse?: string
+}
+
+const options: OptionsType = cli.parse({
     usb: ["u", "listen to Jacdac over USB", true],
     packets: ["p", "show/hide all packets", true],
     dtdl: [false, "generate DTDL files", "file"],
     devices: ["d", "regular expression filter for devices", "string"],
+    services: [false, "regular expression filter for services", "string"],
     rm: [false, "delete files from output folder", true],
+    parse: ["l", "parse logic analyzer log file", "string"],
 })
 
 // DTDL
@@ -76,9 +90,24 @@ if (options.usb) {
     const transport = createUSBTransport(opts)
     const bus = new JDBus([transport])
     bus.on(DEVICE_ANNOUNCE, dev => console.debug(`new device ${dev}`))
-    if (options.pkt) bus.on(PACKET_PROCESS, pkt => console.debug(pkt))
+    if (options.packets) bus.on(PACKET_PROCESS, pkt => console.debug(pkt))
     const run = async () => {
         await bus.connect()
     }
     run()
+}
+
+// Logic parsing
+if (options.parse) {
+    let jd = new JDBus([])
+
+    const opts = {
+        skipRepeatedAnnounce: false,
+        showTime: true
+    }
+    jd.on(PACKET_RECEIVE, pkt => console.log(printPacket(pkt, opts)))
+    jd.on(PACKET_RECEIVE_ANNOUNCE, pkt => console.log(printPacket(pkt, opts)))
+
+    const text = fs.readFileSync(options.parse, "utf8")
+    replayLog(jd, parseLogicLog(text), Number.POSITIVE_INFINITY);
 }
