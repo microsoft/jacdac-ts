47b652511b0dd9ded3e988af7225ef947512629b
tballmsft <tballmsft@users.noreply.github.com>
2021-06-03 22:24:37 +0000

    deploy: c6af24acba52004644a40ee4fb1916f436a3e252

---
+++ src/vm/VMutils.ts
@@ -0,0 +1,18 @@
+export const VM_ERROR = "vmError"
+export const VM_WATCH_CHANGE = "vmWatchChange"
+export const VM_MISSING_ROLE_WARNING = "vmMissingRole"
+export const VM_MISSING_DEVICE_WARNING = "vmMissingDevice"
+export const VM_BREAKPOINT = "vmBreakpoint"
+export const VM_COMMAND_ATTEMPTED = "vmCommandAttempted"
+export const VM_COMMAND_COMPLETED = "vmCommandCompleted"
+
+export class VMError extends Error {
+    constructor(message: string, readonly jacdacName?: string) {
+        super(message)
+        this.name = VM_ERROR
+    }
+}
+
+export default function errorPath(e: VMError): string {
+    return (e as VMError)?.jacdacName
+}
