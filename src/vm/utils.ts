5e1e464447ee4d732a3ed03f95e2fc0bfc4675aa
Tom Ball <tballmsft@users.noreply.github.com>
2021-06-03 09:55:22 -0700

    add some new events

--- src/vm/utils.ts
+++ src/vm/utils.ts
@@ -1,8 +1,10 @@
-export const VM_ERROR = "JacdacVMError"
+export const VM_ERROR = "vmError"
 export const VM_WATCH_CHANGE = "vmWatchChange"
+export const VM_MISSING_ROLE_WARNING = "vmMissingRole"
+export const VM_MISSING_DEVICE_WARNING = "vmMissingDevice"
+export const VM_BREAKPOINT = "vmBreakpoint"
 export const VM_COMMAND_ATTEMPTED = "vmCommandAttempted"
 export const VM_COMMAND_COMPLETED = "vmCommandCompleted"
-
 
 export class JDVMError extends Error {
     constructor(message: string, readonly jacdacName?: string) {
