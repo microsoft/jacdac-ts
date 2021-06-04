c95cae746a1ce0cecbf1c85686cb185f59a17a4d
Tom Ball <tballmsft@users.noreply.github.com>
2021-06-04 10:12:58 -0700

    add mutex from Michal

--- src/vm/VMutils.ts
+++ src/vm/VMutils.ts
@@ -17,3 +17,29 @@ export class VMError extends Error {
 export default function errorPath(e: VMError): string {
     return (e as VMError)?.jacdacName
 }
+
+
+export class Mutex {
+    private promises: (() => Promise<any>)[] = []
+    private shift() {
+        this.promises.shift()
+        if (this.promises[0]) this.promises[0]()
+    }
+    acquire<T>(f: () => Promise<T>): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            this.promises.push(() =>
+                f().then(
+                    v => {
+                        this.shift()
+                        resolve(v)
+                    },
+                    e => {
+                        this.shift()
+                        reject(e)
+                    }
+                )
+            )
+            if (this.promises.length == 1) this.promises[0]()
+        })
+    }
+}
