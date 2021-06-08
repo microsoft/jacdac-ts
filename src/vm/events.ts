a095c3486dc4c97afe5e15dbcd0300b1c3199c05
tballmsft <tballmsft@users.noreply.github.com>
2021-06-08 23:24:15 +0000

    deploy: 4be2f747afdfd7aefdb62ea83b7dafaa6d0f73e6

---
+++ src/vm/events.ts
@@ -0,0 +1,12 @@
+
+export const VM_EVENT = "vmEvent"
+
+export enum VMCode {
+    WatchChange = "vmWatchChange",
+    Breakpoint = "vmBreakpoint",
+    CommandStarted = "vmCommandStarted",
+    CommandCompleted = "vmCommandCompleted",
+    CommandFailed = "vmCommandFailed",
+    RoleMissing = "vmRoleMissing",
+    InternalError = "vmInternalError",
+}
