6ec8f77a0b3be2ae73dc565cd74496e0579cf6b7
Richard Lin <t-richardlin@microsoft.com>
2021-06-29 16:56:59 -0700

    WIP bus setup helpers

--- tests/jdom/adapters.spec.ts
+++ tests/jdom/adapters.spec.ts
@@ -8,6 +8,26 @@ import ButtonGestureAdapter from "../../src/servers/buttongestureadapter"
 import ButtonServer from "../../src/servers/buttonserver"
 import { JDEvent } from "../../src/jdom/event";
 import { JDDevice } from "../../src/jdom/device";
+import RoleManager from "../../src/servers/rolemanager"
+import JDServiceServer from "../../src/jdom/serviceserver";
+
+interface BusDevice {
+    server: JDServiceServer,
+    roleName?: string,
+}
+
+// Creates a bus with the specified servers, bound to the specified files.
+// Returns once all devices are registeres, and adapters are ready.
+function createBus(busDevices: BusDevice[]): JDBus {
+    const bus = mkBus()
+    const roleManager = new RoleManager(bus)
+
+    busDevices.forEach(busDevice => {
+        bus.addServiceProvider(new JDServiceProvider([busDevice.server]))  // TODO support multi-service devices?
+    })
+
+    return bus
+}
 
 suite('adapters', () => {
     let bus: JDBus;
@@ -16,11 +36,19 @@ suite('adapters', () => {
 
     test('click detect event', function(done) {
         console.log("start test")
-        bus = mkBus();
+
+        // These are here so we have a handle
+        const buttonServer = new ButtonServer("button")  // interface name is just a human-friendly name, not functional
+        // TODO pending role name refactor
+        // const gestureAdapter = new ButtonGestureAdapter("button", "gestureAdapter")
+
+        bus = createBus([
+            {server: buttonServer, roleName: "button"},
+            // {server: gestureAdapter},
+        ])
         console.log("bus created")
 
-        const buttonServer = new ButtonServer("B0")  // TODO does the name do anything?
-        bus.addServiceProvider(new JDServiceProvider([buttonServer]))
+        const roleManager = new RoleManager(bus)
 
         bus.on(DEVICE_ANNOUNCE, (device: JDDevice) => {
             // When the button is connected, setup the button adapter and queue test stimulus
@@ -43,7 +71,7 @@ suite('adapters', () => {
             // When the adapter is connected, set up the event handler and dump events
             if (device.services({serviceClass: SRV_BUTTON_GESTURE}).length) {
                 const buttonGesture = bus.services({serviceClass: SRV_BUTTON_GESTURE})[0]
-                
+
                 let eventCounts = 0
                 buttonGesture.on(EVENT, (evs: JDEvent[]) => {  // TODO make sure we're expecting the right events
                     console.log(`${evs[0].name}  ${evs[0].code}`)
