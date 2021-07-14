a93806ca17a5cc38ef7869d3ab6805b17a2ac71a
Richard Lin <t-richardlin@microsoft.com>
2021-07-14 13:44:20 -0700

    wip broken

--- tests/jdom/csvtrace.spec.ts
+++ tests/jdom/csvtrace.spec.ts
@@ -1,4 +1,4 @@
-import { DEVICE_CHANGE, REFRESH, SensorReg, SystemReg } from "../../src/jdom/constants";
+import { DEVICE_CHANGE, REFRESH, SensorReg, SRV_POTENTIOMETER, SystemReg } from "../../src/jdom/constants";
 import { jdpack, PackedValues } from "../../src/jdom/pack";
 import JDServiceServer from "../../src/jdom/serviceserver";
 import { isRegister, serviceSpecificationFromClassIdentifier } from "../../src/jdom/spec";
@@ -17,7 +17,10 @@ import SensorServer, { SensorServiceOptions } from "../../src/servers/sensorserv
  * ]
  * 
  * The first column is the timestamp, in ms, and the second column is the reading.
- * The number of specified registers must match exactly the number of columns
+ * The number of specified registers must match exactly the number of columns.
+ * 
+ * Note that this only writes to registers and DOES NOT generate derived events.
+ * For example, if used with the button, it will not generate up and down events.
  */
 export class JDCsvSensorServer extends SensorServer<[number]> {
     protected nextDataIndex = 0  // index (in this.data) of next value
@@ -41,3 +44,31 @@ export class JDCsvSensorServer extends SensorServer<[number]> {
         }   
     }
 }
+
+
+suite('"CSV" trace server', () => {
+    test('reads from CSVs', async function() {
+        const buttonServer = new JDCsvSensorServer(SRV_POTENTIOMETER, [
+            [0,   0.0],
+            [0.3, 0.5],
+            [0.6, 1.0],
+            [0.9, 0.8],
+            [1.2, 0.6],
+        ], {})
+
+        await withBus([
+            {server: buttonServer},
+        ], async (bus, serviceMap) => {
+            const busTest = new JDBusTestUtil(bus)  // TODO needs better name, also boilerplate?
+            const buttonService = serviceMap.get(buttonServer)  // TODO boilerplate, think about how to eliminate
+
+            buttonServer.down()
+            assert((await busTest.nextEventWithin(buttonService, {within: 100})).code == 
+                ButtonEvent.Down)
+
+            buttonServer.up()
+            assert((await busTest.nextEventWithin(buttonService, {within: 100})).code == 
+                ButtonEvent.Up)
+        })
+    })
+});
