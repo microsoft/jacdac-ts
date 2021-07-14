4e4d09e1572ddf551d8e6446cbc052112630b399
Richard Lin <t-richardlin@microsoft.com>
2021-07-14 14:41:05 -0700

    test repeated holds

--- tests/jdom/button.spec.ts
+++ tests/jdom/button.spec.ts
@@ -16,14 +16,14 @@ suite("button server", () => {
 
             buttonServer.down()
             assert(
-                (await nextEventFrom(buttonService, { within: 100 }))
-                    .code == ButtonEvent.Down
+                (await nextEventFrom(buttonService, { within: 100 })).code ==
+                    ButtonEvent.Down
             )
 
             buttonServer.up()
             assert(
-                (await nextEventFrom(buttonService, { within: 100 }))
-                    .code == ButtonEvent.Up
+                (await nextEventFrom(buttonService, { within: 100 })).code ==
+                    ButtonEvent.Up
             )
         })
     })
@@ -36,14 +36,59 @@ suite("button server", () => {
 
             buttonServer.down()
             assert(
-                (await nextEventFrom(buttonService, { within: 100 }))
-                    .code == ButtonEvent.Down
+                (await nextEventFrom(buttonService, { within: 100 })).code ==
+                    ButtonEvent.Down
             )
             assert(
-                (await nextEventFrom(buttonService, { after: 500, tolerance: 100 }))
-                    .code == ButtonEvent.Hold
-            )  
+                (
+                    await nextEventFrom(buttonService, {
+                        after: 500,
+                        tolerance: 100,
+                    })
+                ).code == ButtonEvent.Hold
+            )
         })
     })
 
+    test("detects repeated holds", async function () {
+        const buttonServer = new ButtonServer("button", false)
+
+        await withBus([{ server: buttonServer }], async (bus, serviceMap) => {
+            const buttonService = serviceMap.get(buttonServer) // TODO boilerplate, think about how to eliminate
+
+            buttonServer.down()
+            assert(
+                (await nextEventFrom(buttonService, { within: 100 })).code ==
+                    ButtonEvent.Down
+            )
+            assert(
+                (
+                    await nextEventFrom(buttonService, {
+                        after: 500,
+                        tolerance: 100,
+                    })
+                ).code == ButtonEvent.Hold
+            )
+
+            buttonServer.up()
+            assert(
+                (await nextEventFrom(buttonService, { within: 100 })).code ==
+                    ButtonEvent.Up
+            )
+
+            buttonServer.down()
+            assert(
+                (await nextEventFrom(buttonService, { within: 100 })).code ==
+                    ButtonEvent.Down
+            )
+            assert(
+                (
+                    await nextEventFrom(buttonService, {
+                        after: 500,
+                        tolerance: 100,
+                    })
+                ).code == ButtonEvent.Hold
+            )
+        })
+    })
 })
