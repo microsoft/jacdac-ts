e444ca83763b9f3da9fb2bfa0ef9ce53be6ca644
Richard Lin <t-richardlin@microsoft.com>
2021-07-14 14:07:16 -0700

    prettier

--- tests/jdom/button.spec.ts
+++ tests/jdom/button.spec.ts
@@ -2,29 +2,30 @@
 // Yes, we're meta-testing!
 
 import { suite, test } from "mocha"
-import { ButtonEvent } from "../../src/jdom/constants";
-import {withBus, JDBusTestUtil} from "./tester";
-import { assert } from "../../src/jdom/utils";
-import ButtonServer from "../../src/servers/buttonserver";
+import { ButtonEvent } from "../../src/jdom/constants"
+import { withBus, JDBusTestUtil } from "./tester"
+import { assert } from "../../src/jdom/utils"
+import ButtonServer from "../../src/servers/buttonserver"
 
-
-suite('button server', () => {
-    test('fires edge events after changing state', async function() {
+suite("button server", () => {
+    test("fires edge events after changing state", async function () {
         const buttonServer = new ButtonServer("button", false)
 
-        await withBus([
-            {server: buttonServer},
-        ], async (bus, serviceMap) => {
-            const busTest = new JDBusTestUtil(bus)  // TODO needs better name, also boilerplate?
-            const buttonService = serviceMap.get(buttonServer)  // TODO boilerplate, think about how to eliminate
+        await withBus([{ server: buttonServer }], async (bus, serviceMap) => {
+            const busTest = new JDBusTestUtil(bus) // TODO needs better name, also boilerplate?
+            const buttonService = serviceMap.get(buttonServer) // TODO boilerplate, think about how to eliminate
 
             buttonServer.down()
-            assert((await busTest.nextEventWithin(buttonService, {within: 100})).code == 
-                ButtonEvent.Down)
+            assert(
+                (await busTest.nextEventWithin(buttonService, { within: 100 }))
+                    .code == ButtonEvent.Down
+            )
 
             buttonServer.up()
-            assert((await busTest.nextEventWithin(buttonService, {within: 100})).code == 
-                ButtonEvent.Up)
+            assert(
+                (await busTest.nextEventWithin(buttonService, { within: 100 }))
+                    .code == ButtonEvent.Up
+            )
         })
     })
-});
+})
