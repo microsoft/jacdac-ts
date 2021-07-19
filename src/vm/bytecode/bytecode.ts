2c58a5e7e88808bfcb2c9b99c58fa1767fe1735e
Tom Ball <you@example.com>
2021-07-19 15:36:41 -0700

    start with flat encoding of instructions

--- src/vm/bytecode/bytecode.ts
+++ src/vm/bytecode/bytecode.ts
@@ -1,32 +1,68 @@
 // binary bytecode definition
 
+// lookup tables
+
+// role -> { name: string, serviceClass: uint32, client: boolean }
+
+// types
+// - int, float, string, boolean
+// - single record (JSON, JD Event/Command payload)
+
 // anything that is computed uses the stack (no registers)
 
-// instruction types
+enum BytecodeInstructions {
+    NOP = 0,      // do nothing
+    HALT,         // halt the handler
+    RESET,        // reset the handler
+    FAIL,         // fail the handler
+    JUMP_UNCOND,
+    JUMP_COND,    // EQ0, GT0, LT0, GE0, LE0
+    JUMP_ERROR,
 
-// 0. control-flow
+    USER_VAR_SET = 16,
+    USER_VAR_GET,
+    JD_SERVICE,     // send to network
+    ON_JD_SERVICE,  // wait for register get/set, event, command
+    ROLE_BOUND,     // query if role is bound
 
-//    -	Nop
-//    - Halt (handler doesn't run again)
-//    - Reset (start handler again, jump to first instruction)
-//    - Unconditional Jump
-//    - Conditional Jump (pop?)
-//    - Jump on error code
+    LOAD_INT = 32,
+    LOAD_FLOAT,
+    LOAD_STRING,
+    
+    UNARY_MINUS = 40,
+    UNARY_ABS,
+    UNARY_BANG,
+    UNARY_NOT,
+    UNARY_PLUS,
 
-// 1. pure stack computation
+    BINARY_PLUS = 56,
+    BINARY_MINUS,
+    BINARY_DIV,
+    BINARY_MULT,
+    BINARY_MOD,
+    BINARY_RSHIFT,
+    BINARY_LSHIFT,
+    BINARY_OR,
+    BINARY_AND,
+    BINARY_XOR,
+    BINARY_EQ,
+    BINARY_NEQ,
+    BINARY_TEQ,
+    BINARY_TNE,
+    BINARY_LT,
+    BINARY_GT,
+    BINARY_LE,
+    BINARY_GE,
 
-//    - Push constant
-//    - Function application (pop M arguments, push N arguments)
+    SLEEP = 96,
+    WAIT_REGISTER_CHANGE,     
 
-// 2. user variables
-
-//    - read variable (push)
-//    - write variable (pop?)
+    REST = 128,
+}
 
 // 3. JD service (the only way to interact with the "environment")
 
 //    - all instructions have a role operand (1 byte)
-//    - role bound (poll/event)
 //    - service command (16 bits, 4-bit operation, 12-bit code)
 //      - action
 //      - register read
@@ -34,4 +70,14 @@
 //      - reserved
 //      - events
 
-// some instructions wait (events, state change, )
+// 4. JD runtime
+//    - role bound (poll/event)
+
+// some instructions wait
+
+// - await service_command
+// - wait time
+// - higher-level
+//   - wait for register to change by <expr>
+//   - wait for condition
+//   - wait for event with condition
