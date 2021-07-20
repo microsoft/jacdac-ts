84d287488245b785a319d75e370cb243ec35654b
Richard Lin <t-richardlin@microsoft.com>
2021-07-20 15:44:18 -0700

    prettier?

--- tests/vm/buttonservo.spec.ts
+++ tests/vm/buttonservo.spec.ts
@@ -1,20 +1,31 @@
 import { suite, test } from "mocha"
-import { readdirSync, readFileSync } from "fs"
-import { join } from "path"
+import { readFileSync } from "fs"
 import { VMProgram } from "../../src/vm/ir"
 import { VMProgramRunner } from "../../src/vm/runner"
 import { JDBus } from "../../src/jdom/bus"
 
-import { withTestBus, createServices, CreatedServerService } from "../jdom/tester"
+import {
+    withTestBus,
+    createServices,
+    CreatedServerService,
+} from "../jdom/tester"
 import ButtonServer from "../../src/servers/buttonserver"
 import RoleManager from "../../src/servers/rolemanager"
 import ServoServer from "../../src/servers/servoserver"
 import { FastForwardScheduler } from "../jdom/scheduler"
 
 suite("button servo", () => {
-    const program: VMProgram = JSON.parse(readFileSync("vm/suites/buttonservo.json", "utf8"))
-            
-    async function withHarness(testBody: (bus: JDBus, button: CreatedServerService<ButtonServer>, servo: CreatedServerService<ServoServer>) => void) {
+    const program: VMProgram = JSON.parse(
+        readFileSync("vm/suites/buttonservo.json", "utf8")
+    )
+
+    async function withHarness(
+        testBody: (
+            bus: JDBus,
+            button: CreatedServerService<ButtonServer>,
+            servo: CreatedServerService<ServoServer>
+        ) => void
+    ) {
         await withTestBus(async bus => {
             const { button, servo } = await createServices(bus, {
                 button: new ButtonServer("button", false),
@@ -26,13 +37,13 @@ suite("button servo", () => {
                     role: "button 1",
                     serviceClass: button.service.serviceClass,
                     preferredDeviceId: button.service.device.deviceId,
-                    service: button.service
+                    service: button.service,
                 },
                 {
                     role: "servo 1",
                     serviceClass: servo.service.serviceClass,
                     preferredDeviceId: servo.service.device.deviceId,
-                    service: servo.service
+                    service: servo.service,
                 },
             ])
 
@@ -48,7 +59,7 @@ suite("button servo", () => {
             servo.server.angle.setValues([50])
             await (bus.scheduler as FastForwardScheduler).runForDelay(100)
             console.log(servo.server.angle.values())
-            
+
             button.server.down()
             await (bus.scheduler as FastForwardScheduler).runForDelay(100)
             button.server.up()
