ee84e729b4ab040f269dac1460129c2f09573c57
Richard Lin <t-richardlin@microsoft.com>
2021-07-21 16:58:11 -0700

    add button-to-switch and getRoles infra

---
+++ tests/vm/buttontoswitch.spec.ts
@@ -0,0 +1,61 @@
+import { suite, test } from "mocha"
+import { readFileSync } from "fs"
+import { VMProgram } from "../../src/vm/ir"
+import { VMProgramRunner } from "../../src/vm/runner"
+import { JDBus } from "../../src/jdom/bus"
+
+import {
+    withTestBus,
+    createServices,
+    CreatedServerService,
+    runForDelay,
+} from "../jdom/tester"
+import ButtonServer from "../../src/servers/buttonserver"
+import RoleManager from "../../src/servers/rolemanager"
+import ServoServer from "../../src/servers/servoserver"
+import { assert } from "../../src/jdom/utils"
+import { bindRoles, getRoles } from "./vmtester"
+import { RoleManagerReg } from "../../jacdac-spec/dist/specconstants"
+import { ROLE_BOUND } from "../../src/jdom/constants"
+import { JDService } from "../../src/jdom/service"
+
+suite("button servo", () => {
+    const program: VMProgram = JSON.parse(
+        readFileSync("vm/suites/buttontoswitch.json", "utf8")
+    )
+
+    async function withHarness(
+        testBody: (
+            bus: JDBus,
+            button: CreatedServerService<ButtonServer>,
+            sw: JDService
+        ) => void
+    ) {
+        await withTestBus(async bus => {
+            const { button } = await createServices(bus, {
+                button: new ButtonServer("button", false),
+            })
+            const roleMgr = new RoleManager(bus)
+            bindRoles(roleMgr, program, {
+                "button 1": button.service,
+            })
+
+            const runner = new VMProgramRunner(roleMgr, program)
+            await runner.startAsync()
+
+            const { "switch server 1": sw } = await getRoles(roleMgr, program)
+
+            await testBody(bus, button, sw)
+        })
+    }
+
+    test("inverts when pressed", async () => {
+        await withHarness(async (bus, button, sw) => {
+            button.server.down()
+            await runForDelay(bus, 1000)
+            button.server.up()
+
+        })
+    })
+
+})
--- tests/vm/vmtester.ts
+++ tests/vm/vmtester.ts
@@ -1,9 +1,12 @@
 // Helper methods for unit testing VM programs
 
+import { instanceOf } from "prop-types"
+import { ROLE_BOUND } from "../../src/jdom/constants"
 import { JDService } from "../../src/jdom/service"
 import { assert } from "../../src/jdom/utils"
 import RoleManager, { RoleBinding } from "../../src/servers/rolemanager"
 import { VMProgram } from "../../src/vm/ir"
+import { FastForwardScheduler } from "../jdom/scheduler"
 
 // Binds roles (using a provided role manager), checking that the roles provided
 // are exhaustive and correct for a given VMProgram
@@ -35,4 +38,37 @@ export function bindRoles(
 
     // Bind roles
     roleMgr.setRoles(serverRolesMap)
+}
+
+// For a VM program, returns an object mapping server service names to services once the roles have been bound.
+// This should be done before any test code runs, so servers have time to initialize and announce.
+export async function getRoles(
+    roleMgr: RoleManager,
+    program: VMProgram
+): Promise<Record<string, JDService>> {
+    const outputMap: Record<string, JDService> = {}
+    assert(Object.keys(outputMap).length == 0)  // we rely on length to be accurate later
+
+    const serverRoles = program.serverRoles.map(role => role.role)
+    const promise = new Promise(resolve => {
+        const handler = (role: string) => {
+            if (serverRoles.includes(role) && !(role in outputMap)) {
+                outputMap[role] = roleMgr.getService(role)
+                if (Object.keys(outputMap).length == serverRoles.length) {
+                    roleMgr.off(ROLE_BOUND, handler)
+                    resolve(undefined)
+                }
+            }
+        }
+        roleMgr.on(ROLE_BOUND, handler)
+    })
+
+    // TODO should probably start fundamentally rethinking this abstraction
+    if (roleMgr.bus.scheduler instanceof FastForwardScheduler) {
+        await roleMgr.bus.scheduler.runToPromise(promise)
+    } else {
+        await promise
+    }
+
+    return outputMap
 }
