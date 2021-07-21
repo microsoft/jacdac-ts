7fc0e92f60b6aee323a52691c6193457b05feb9c
ducky64 <ducky64@users.noreply.github.com>
2021-07-21 18:06:56 +0000

    deploy: fde399cf2bb1994fec4f52109fe90fa1a6657853

--- tests/jdom/button.spec.ts
+++ tests/jdom/button.spec.ts
@@ -3,47 +3,50 @@
 
 import { suite, test } from "mocha"
 import { ButtonEvent } from "../../src/jdom/constants"
-import { withBus, createServices, nextEventFrom } from "./tester"
+import { withTestBus, createServices, nextEventFrom } from "./tester"
 import { assert } from "../../src/jdom/utils"
 import ButtonServer from "../../src/servers/buttonserver"
 
 suite("button server", () => {
+    // Note that tolerances are set at 60 ms here, because the button updates on a handleRefresh,
+    // which runs every 50ms. So it's possible here for the handleRefresh to run before the stimulus,
+    // which adds a 50ms delay.
     test("fires edge events after changing state", async function () {
-        await withBus(async bus => {
+        await withTestBus(async bus => {
             const { button } = await createServices(bus, {
                 button: new ButtonServer("button", false),
             })
 
             button.server.down()
             assert(
-                (await nextEventFrom(button.service, { within: 100 })).code ==
+                (await nextEventFrom(button.service, { within: 60 })).code ==
                     ButtonEvent.Down
             )
 
             button.server.up()
             assert(
-                (await nextEventFrom(button.service, { within: 100 })).code ==
+                (await nextEventFrom(button.service, { within: 60 })).code ==
                     ButtonEvent.Up
             )
         })
     })
 
     test("fires both down and hold events when held", async function () {
-        await withBus(async bus => {
+        await withTestBus(async bus => {
             const { button } = await createServices(bus, {
                 button: new ButtonServer("button", false),
             })
 
             button.server.down()
             assert(
-                (await nextEventFrom(button.service, { within: 100 })).code ==
+                (await nextEventFrom(button.service, { within: 60 })).code ==
                     ButtonEvent.Down
             )
             assert(
                 (
                     await nextEventFrom(button.service, {
                         after: 500,
-                        tolerance: 100,
+                        tolerance: 60,
                     })
                 ).code == ButtonEvent.Hold
             )
@@ -51,21 +54,21 @@ suite("button server", () => {
     })
 
     test("repeatedly raise hold events when held", async function () {
-        await withBus(async bus => {
+        await withTestBus(async bus => {
             const { button } = await createServices(bus, {
                 button: new ButtonServer("button", false),
             })
 
             button.server.down()
             assert(
-                (await nextEventFrom(button.service, { within: 100 })).code ==
+                (await nextEventFrom(button.service, { within: 60 })).code ==
                     ButtonEvent.Down
             )
             assert(
                 (
                     await nextEventFrom(button.service, {
                         after: 500,
-                        tolerance: 100,
+                        tolerance: 60,
                     })
                 ).code == ButtonEvent.Hold
             )
@@ -73,14 +76,14 @@ suite("button server", () => {
                 (
                     await nextEventFrom(button.service, {
                         after: 500,
-                        tolerance: 100,
+                        tolerance: 60,
                     })
                 ).code == ButtonEvent.Hold
             )
 
             button.server.up()
             assert(
-                (await nextEventFrom(button.service, { within: 100 })).code ==
+                (await nextEventFrom(button.service, { within: 60 })).code ==
                     ButtonEvent.Up
             )
             await nextEventFrom(button.service, { within: 1000 }).then(
@@ -90,44 +93,44 @@ suite("button server", () => {
                 () => {}
             ) // rejection is the expected case and nothing happens there
         })
-    }).timeout(3000) // TODO remove when we can fast-forward tests
+    })
 
     test("detects repeated holds", async function () {
-        await withBus(async bus => {
+        await withTestBus(async bus => {
             const { button } = await createServices(bus, {
                 button: new ButtonServer("button", false),
             })
 
             button.server.down()
             assert(
-                (await nextEventFrom(button.service, { within: 100 })).code ==
+                (await nextEventFrom(button.service, { within: 60 })).code ==
                     ButtonEvent.Down
             )
             assert(
                 (
                     await nextEventFrom(button.service, {
                         after: 500,
-                        tolerance: 100,
+                        tolerance: 60,
                     })
                 ).code == ButtonEvent.Hold
             )
 
             button.server.up()
             assert(
-                (await nextEventFrom(button.service, { within: 100 })).code ==
+                (await nextEventFrom(button.service, { within: 60 })).code ==
                     ButtonEvent.Up
             )
 
             button.server.down()
             assert(
-                (await nextEventFrom(button.service, { within: 100 })).code ==
+                (await nextEventFrom(button.service, { within: 60 })).code ==
                     ButtonEvent.Down
             )
             assert(
                 (
                     await nextEventFrom(button.service, {
                         after: 500,
-                        tolerance: 100,
+                        tolerance: 60,
                     })
                 ).code == ButtonEvent.Hold
             )
