5c9a8f2ba0d777647e3d125df4fb2ce20b1fc436
Richard Lin <t-richardlin@microsoft.com>
2021-07-22 17:17:25 -0700

    move everything to TS

--- src/tstester/jacdac-tstester.ts
+++ src/tstester/jacdac-tstester.ts
@@ -1,4 +1,38 @@
+import { createUSBBus } from "../jdom/jacdac-jdom"
 
-export function main(log: (msg: string) => void) {
-    log(`ducks go quack`)
+class ConsoleUi {
+    readonly logDiv: HTMLElement
+    constructor(protected readonly document: Document) {
+        this.logDiv = document.getElementById("log")
+
+    }
+
+    public log(msg: string) {  // TS reimplementation of console from packets.html
+        const line = this.document.createElement("div")
+        line.innerText = "" + msg
+        line.style.whiteSpace = "pre-wrap"
+        this.logDiv.appendChild(line)
+        while (this.logDiv.childElementCount > 100) {
+            this.logDiv.firstChild.remove()
+        }
+    }
+}
+
+export function main(document: Document) {
+    const bus = createUSBBus()
+    const ui = new ConsoleUi(document)
+
+    document.getElementById("connect").onclick = async () => {
+        ui.log("disconnecting ...")
+        await bus.disconnect();
+        ui.log("connecting ...")
+        await bus.connect();
+        ui.log("connected")
+    }
+
+    document.getElementById("disconnect").onclick = async () => {
+        ui.log("disconnecting ...")
+        await bus.disconnect()
+        ui.log("disconnected")
+    }
 }
