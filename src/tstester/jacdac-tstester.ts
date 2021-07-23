ac8ecd0bc530cbc4147695e357608d50e0e2bd4e
Richard Lin <t-richardlin@microsoft.com>
2021-07-22 17:41:06 -0700

    Update jacdac-tstester.ts

--- src/tstester/jacdac-tstester.ts
+++ src/tstester/jacdac-tstester.ts
@@ -1,4 +1,4 @@
-import { createUSBBus, DeviceFilter, JDBus, JDDevice, JDService, ServiceFilter } from "../jdom/jacdac-jdom"
+import { createUSBBus, DeviceFilter, DEVICE_ANNOUNCE, JDBus, JDDevice, JDService, ServiceFilter } from "../jdom/jacdac-jdom"
 
 class ConsoleUi {
     readonly logDiv: HTMLElement
@@ -24,21 +24,27 @@ export function main(document: Document) {
     const ui = new ConsoleUi(document)
 
     document.getElementById("connect").onclick = async () => {
+        ui.log("")
         ui.log("disconnecting ...")
         await bus.disconnect();
         ui.log("connecting ...")
         await bus.connect();
         ui.log("connected")
 
+        ui.log("devices:")
         tester.devices().forEach(deviceTester => {
             const deviceServiceNames = deviceTester.services().map(service =>
                 service.service.specification?.name
             )
-            ui.log(`${deviceTester.device.shortId}: ${deviceServiceNames.join(",")}`)
+            ui.log(`- ${deviceTester.device.shortId}: ${deviceServiceNames.join(",")}`)
         })
+
+        const device = await tester.nextConnected()
+        ui.log(`connected: ${device.name()}`)
     }
 
     document.getElementById("disconnect").onclick = async () => {
+        ui.log("")
         ui.log("disconnecting ...")
         await bus.disconnect()
         ui.log("disconnected")
@@ -52,11 +58,25 @@ class BusTester {
     public devices(options?: DeviceFilter) {
         return this.bus.devices(options).map(device => new DeviceTester(device))
     }
+
+    public async nextConnected(): Promise<DeviceTester> {
+        const promise = new Promise<DeviceTester>(resolve => {
+            this.bus.once(DEVICE_ANNOUNCE, (device: JDDevice) => {
+                resolve(new DeviceTester(device))
+            })
+        })
+        
+        return promise
+    }
 }
 
 class DeviceTester {
     constructor(readonly device: JDDevice) {
 
+    }
+
+    public name() {
+        return this.device.shortId
     }
 
     public services(options?: ServiceFilter) {
@@ -67,5 +87,9 @@ class DeviceTester {
 class ServiceTester {
     constructor(readonly service: JDService) {
 
+    }
+
+    public name() {
+        return this.service.specification.name
     }
 }
