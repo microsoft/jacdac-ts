b22b1241bd57d572cc2b08b120c4b09c99f23d8a
Richard Lin <t-richardlin@microsoft.com>
2021-08-10 16:47:08 -0700

    port VM tests

--- tests/vm/buttonservo.spec.ts
+++ tests/vm/buttonservo.spec.ts
@@ -2,38 +2,35 @@ import { suite, test } from "mocha"
 import { readFileSync } from "fs"
 import { VMProgram } from "../../src/vm/ir"
 import { VMProgramRunner } from "../../src/vm/runner"
-import { JDBus } from "../../src/jdom/bus"
 
 import {
-    withTestBus,
-    createServices,
     CreatedServerService,
-    runForDelay,
 } from "../jdom/tester"
 import ButtonServer from "../../src/servers/buttonserver"
 import RoleManager from "../../src/servers/rolemanager"
 import ServoServer from "../../src/servers/servoserver"
 import { assert } from "../../src/jdom/utils"
 import { bindRoles } from "./vmtester"
+import { FastForwardTester } from "../jdom/fastforwardtester"
 
 suite("button servo", () => {
     const program: VMProgram = JSON.parse(
         readFileSync("vm/suites/buttonservo.json", "utf8")
     )
 
-    async function withHarness(
+    function makeVmTest(
         testBody: (
-            bus: JDBus,
+            tester: FastForwardTester,
             button: CreatedServerService<ButtonServer>,
             servo: CreatedServerService<ServoServer>
         ) => void
     ) {
-        await withTestBus(async bus => {
-            const { button, servo } = await createServices(bus, {
+        return FastForwardTester.makeTest(async tester => {
+            const { button, servo } = await tester.createServices({
                 button: new ButtonServer("button", false),
                 servo: new ServoServer(),
             })
-            const roleMgr = new RoleManager(bus)
+            const roleMgr = new RoleManager(tester.bus)
             bindRoles(roleMgr, program, {
                 "button 1": button.service,
                 "servo 1": servo.service,
@@ -42,42 +39,36 @@ suite("button servo", () => {
             const runner = new VMProgramRunner(roleMgr, program)
             await runner.startAsync()
 
-            await testBody(bus, button, servo)
+            await testBody(tester, button, servo)
         })
     }
 
-    test("inverts when pressed", async () => {
-        await withHarness(async (bus, button, servo) => {
-            servo.server.angle.setValues([50])
+    test("inverts when pressed", makeVmTest(async (tester, button, servo) => {
+        servo.server.angle.setValues([50])
 
-            button.server.down()
-            await runForDelay(bus, 100)
-            button.server.up()
-            assert(servo.server.angle.values()[0] == -50.0)
-        })
-    })
+        button.server.down()
+        await tester.waitForDelay(100)
+        button.server.up()
+        assert(servo.server.angle.values()[0] == -50.0)
+    }))
 
-    test("does not invert when not pressed", async () => {
-        await withHarness(async (bus, button, servo) => {
-            servo.server.angle.setValues([50])
+    test("does not invert when not pressed", makeVmTest(async (tester, button, servo) => {
+        servo.server.angle.setValues([50])
 
-            await runForDelay(bus, 100)
-            assert(servo.server.angle.values()[0] == 50)
-        })
-    })
+        await tester.waitForDelay(100)
+        assert(servo.server.angle.values()[0] == 50)
+    }))
 
-    test("inverts twice when pressed twice", async () => {
-        await withHarness(async (bus, button, servo) => {
-            servo.server.angle.setValues([50])
+    test("inverts twice when pressed twice", makeVmTest(async (tester, button, servo) => {
+        servo.server.angle.setValues([50])
 
-            button.server.down()
-            await runForDelay(bus, 100)
-            button.server.up()
-            await runForDelay(bus, 100)
-            button.server.down()
-            await runForDelay(bus, 100)
-            button.server.up()
-            assert(servo.server.angle.values()[0] == 50)
-        })
-    })
+        button.server.down()
+        await tester.waitForDelay(100)
+        button.server.up()
+        await tester.waitForDelay(100)
+        button.server.down()
+        await tester.waitForDelay(100)
+        button.server.up()
+        assert(servo.server.angle.values()[0] == 50)
+    }))
 })
--- tests/vm/rememberservo.spec.ts
+++ tests/vm/rememberservo.spec.ts
@@ -2,40 +2,37 @@ import { suite, test } from "mocha"
 import { readFileSync } from "fs"
 import { VMProgram } from "../../src/vm/ir"
 import { VMProgramRunner } from "../../src/vm/runner"
-import { JDBus } from "../../src/jdom/bus"
 
 import {
-    withTestBus,
-    createServices,
     CreatedServerService,
-    runForDelay,
 } from "../jdom/tester"
 import ButtonServer from "../../src/servers/buttonserver"
 import RoleManager from "../../src/servers/rolemanager"
 import ServoServer from "../../src/servers/servoserver"
 import { assert } from "../../src/jdom/utils"
 import { bindRoles } from "./vmtester"
+import { FastForwardTester } from "../jdom/fastforwardtester"
 
 suite("remember servo", () => {
     const program: VMProgram = JSON.parse(
         readFileSync("vm/suites/rememberservo.json", "utf8")
     )
 
-    async function withHarness(
+    function makeVmTest(
         testBody: (
-            bus: JDBus,
+            tester: FastForwardTester,
             recall: CreatedServerService<ButtonServer>,
             set: CreatedServerService<ButtonServer>,
             servo: CreatedServerService<ServoServer>
         ) => void
     ) {
-        await withTestBus(async bus => {
-            const { recall, set, servo } = await createServices(bus, {
+        return FastForwardTester.makeTest(async tester => {
+            const { recall, set, servo } = await tester.createServices({
                 recall: new ButtonServer("button", false),
                 set: new ButtonServer("button", false),
                 servo: new ServoServer(),
             })
-            const roleMgr = new RoleManager(bus)
+            const roleMgr = new RoleManager(tester.bus)
             bindRoles(roleMgr, program, {
                 recall: recall.service,
                 set: set.service,
@@ -45,47 +42,46 @@ suite("remember servo", () => {
             const runner = new VMProgramRunner(roleMgr, program)
             await runner.startAsync()
 
-            await testBody(bus, recall, set, servo)
+            await testBody(tester, recall, set, servo)
         })
     }
 
-    test("sets and recalls", async () => {
-        await withHarness(async (bus, recall, set, servo) => {
+    test("sets and recalls", makeVmTest(async (tester, recall, set, servo) => {
             servo.server.angle.setValues([50])
             set.server.down()
-            await runForDelay(bus, 100)
+            
+            await tester.waitForDelay(100)
             set.server.up()
 
             servo.server.angle.setValues([0]) // make sure the difference on recall is visible
             assert(servo.server.angle.values()[0] == 0.0)
 
             recall.server.down()
-            await runForDelay(bus, 100)
+            await tester.waitForDelay(100)
             recall.server.up()
             assert(servo.server.angle.values()[0] == 50.0)
         })
-    })
+    )
 
-    test("sets, re-sets, and recalls", async () => {
-        await withHarness(async (bus, recall, set, servo) => {
+    test("sets, re-sets, and recalls", makeVmTest(async (tester, recall, set, servo) => {
             servo.server.angle.setValues([50])
             set.server.down()
-            await runForDelay(bus, 100)
+            await tester.waitForDelay(100)
             set.server.up()
 
-            await runForDelay(bus, 100)
+            await tester.waitForDelay(100)
             servo.server.angle.setValues([-50])
             set.server.down()
-            await runForDelay(bus, 100)
+            await tester.waitForDelay(100)
             set.server.up()
 
             servo.server.angle.setValues([0]) // make sure the difference on recall is visible
             assert(servo.server.angle.values()[0] == 0.0)
 
             recall.server.down()
-            await runForDelay(bus, 100)
+            await tester.waitForDelay(100)
             recall.server.up()
             assert(servo.server.angle.values()[0] == -50.0)
         })
-    })
+    )
 })
