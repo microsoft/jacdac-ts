7e52352d3d0601d4ba20d6ed4d02f11ed9616f7e
pelikhan <jhalleux@microsoft.com>
2021-08-23 06:09:06 -0700

    more docs

--- src/jdom/trace/trace.ts
+++ src/jdom/trace/trace.ts
@@ -2,17 +2,30 @@ import Packet from "../packet"
 import { printPacket } from "../pretty"
 import { toHex } from "../utils"
 
+const TRACE_OVERSHOOT = 1.1
+
 /**
  * A sequence of packets.
  * @category Trace
  */
 export class Trace {
+    /**
+     * Constructs a new empty trace or from an existing list of packets
+     * @param packets list of packets
+     * @param description description of the trace
+     */
     constructor(public packets: Packet[] = [], public description?: string) {}
 
+    /**
+     * Number of packets in trace
+     */
     get length() {
         return this.packets.length
     }
 
+    /**
+     * Duration in milliseconds between the first and last packet.
+     */
     get duration() {
         if (!this.packets.length) return 0
         return (
@@ -21,22 +34,40 @@ export class Trace {
         )
     }
 
+    /**
+     * Timestamp of the first packet, defaults to 0 if trace is empty.
+     */
     get startTimestamp() {
         return this.packets[0]?.timestamp || 0
     }
 
+    /**
+     * Timestamp of the last packet, defaults to 0 if trace is empty.
+     */
     get endTimestamp() {
         return this.packets[this.packets.length - 1]?.timestamp || 0
     }
 
-    addPacket(pkt: Packet, maxLength = -1) {
-        this.packets.push(pkt)
-        if (maxLength > 0 && this.packets.length > maxLength * 1.1) {
+    /**
+     * Appends a packet to the trace
+     * @param packet packet to add
+     * @param maxLength If positive, prunes older packets when the length reaches maxLength
+     */
+    addPacket(packet: Packet, maxLength = -1) {
+        this.packets.push(packet)
+        if (
+            maxLength > 0 &&
+            this.packets.length > maxLength * TRACE_OVERSHOOT
+        ) {
             // 10% overshoot of max
             this.packets = this.packets.slice(-maxLength)
         }
     }
 
+    /**
+     * Gets a text-rendered view of the trace
+     * @returns text where each line is a packet
+     */
     serializeToText() {
         const start = this.packets[0]?.timestamp || 0
         const text = this.packets.map(
