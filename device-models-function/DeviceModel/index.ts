e5f5eb78bcff7402cf65df37b5cd2ad13d324c29
pelikhan <jhalleux@microsoft.com>
2021-08-25 12:42:56 -0700

    fix proxy settings

--- device-models-function/DeviceModel/index.ts
+++ device-models-function/DeviceModel/index.ts
@@ -1,17 +1,15 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context } from "@azure/functions"
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+const httpTrigger: AzureFunction = async function (
+    context: Context
+): Promise<void> {
+    const { bindingData } = context
+    const dtmi = bindingData.dtmi as string
+    const responseMessage = JSON.stringify(dtmi)
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+        body: responseMessage,
+    }
+}
 
-};
-
-export default httpTrigger;
+export default httpTrigger
