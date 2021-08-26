59031f7f7407991b65d0131bd9aa24bf0e1b3972
pelikhan <jhalleux@microsoft.com>
2021-08-26 11:12:51 -0700

    updated proxy

--- src/azure-iot/dtdlproxy.ts
+++ src/azure-iot/dtdlproxy.ts
@@ -4,31 +4,40 @@ import JDBus from "../jdom/bus"
 import JDClient from "../jdom/client"
 import { CHANGE, DEVICE_CHANGE } from "../jdom/constants"
 import { isInfrastructure } from "../jdom/spec"
-import JDService from "../jdom/service"
+
+export const DTDL_DEVICE_MODELS_REPOSITORY =
+    "https://jacdac-device-models.azurewebsites.net/"
 
 /**
  * Tracks the devices on the bus and automatically generate DTDL information
+ * @internal
  */
 export default class DTDLProxy extends JDClient {
+    private _dtdl: DTDLInterface
+
     constructor(readonly bus: JDBus) {
         super()
 
         this.bus.on(DEVICE_CHANGE, this.updateDTDL.bind(this))
+        this.updateDTDL()
     }
 
-    private dtdl: DTDLInterface
+    get dtdl() {
+        return this._dtdl
+    }
 
     private updateDTDL() {
         const newServices = this.bus
             .services({ specification: true, ignoreSelf: true })
             .filter(srv => !isInfrastructure(srv.specification))
+            .sort((l, r) => l.compareTo(r))
         const device: jdspec.DeviceClassSpec = {
             name: "Azure IoT Jacdac Device",
             services: newServices.map(srv => srv.serviceClass),
         }
         const newDTDL = deviceClassToDTDL(device)
-        if (JSON.stringify(newDTDL) !== JSON.stringify(this.dtdl)) {
-            this.dtdl = newDTDL
+        if (JSON.stringify(newDTDL) !== JSON.stringify(this._dtdl)) {
+            this._dtdl = newDTDL
             console.log(`new DTDL`, { newDTDL })
             this.emit(CHANGE)
         }
