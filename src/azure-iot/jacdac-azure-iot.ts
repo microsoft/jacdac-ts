5ca0ec2cc8f38f665f9730c70ea241548fa30aa4
pelikhan <jhalleux@microsoft.com>
2021-08-31 21:35:18 -0700

    removing all dtdl code

--- src/azure-iot/jacdac-azure-iot.ts
+++ src/azure-iot/jacdac-azure-iot.ts
@@ -1,4 +1,1 @@
-export * from "./dtdl"
-export * from "./dtdlspec"
-export * from "./dtdlproxy"
 export * from "./devicetwin"
--- src/cli/jacdac-cli.ts
+++ src/cli/jacdac-cli.ts
@@ -13,11 +13,6 @@ import {
 import { createUSBTransport } from "../jdom/transport/usb"
 import { createNodeUSBOptions } from "../jdom/transport/nodewebusb"
 import { clone } from "../jdom/utils"
-import {
-    routeToDTDL,
-    serviceSpecificationsWithDTDL,
-    serviceSpecificationToDTDL,
-} from "../azure-iot/dtdlspec"
 import JDBus from "../jdom/bus"
 import { printPacket } from "../jdom/pretty"
 import { parseLogicLog, replayLogicLog } from "../jdom/logparser"
@@ -41,8 +36,6 @@ interface OptionsType {
     ws?: boolean
     port?: number
     packets?: boolean
-    dtdl?: boolean
-    sdmi?: string
     devicetwin?: boolean
     devices?: string
     services?: string
@@ -57,8 +50,6 @@ const options: OptionsType = cli.parse({
     ws: [false, "start web socket server"],
     port: [false, "specify custom web socket server port", "int"],
     packets: ["p", "show/hide all packets"],
-    dtdl: [false, "generate DTDL files", "file"],
-    sdmi: [false, "generate dynamic DTDL files", "string"],
     devicetwin: [false, "generate device twin files", "file"],
     devices: ["d", "regular expression filter for devices", "string"],
     services: [false, "regular expression filter for services", "string"],
@@ -66,42 +57,6 @@ const options: OptionsType = cli.parse({
     parse: ["l", "parse logic analyzer log file", "string"],
     catalog: [false, "generate .json files for device catalog"],
 })
-// SDMI
-if (options.sdmi) {
-    console.log(`sdmi: generate DTDL for ${options.sdmi}`)
-    const dtdl = routeToDTDL(options.sdmi)
-    console.log(dtdl)
-}
-
-// DTDL
-if (options.dtdl) {
-    cli.info(`generating DTDL models`)
-    const run = async () => {
-        const dir = options.dtdl
-        fs.mkdirpSync(dir)
-        if (options.rm) fs.emptyDirSync(dir)
-        // generate services
-        {
-            let services = serviceSpecificationsWithDTDL()
-            if (options.services) {
-                const rx = new RegExp(options.services, "i")
-                services = services.filter(dev => rx.test(dev.name))
-            }
-            cli.info(`${services.length} services`)
-            services.forEach((srv, i) => {
-                const fn = `${dir}/${dashify(srv.shortName)}.json`
-                cli.debug(`${srv.name} => ${fn}`)
-                cli.progress(i / (services.length - 1))
-                const dtdl = serviceSpecificationToDTDL(srv)
-                fs.writeJSONSync(fn, dtdl, { spaces: 2 })
-            })
-        }
-
-        // all done
-        cli.info(`done`)
-    }
-    run()
-}
 
 // DeviceTwin
 if (options.devicetwin) {
@@ -122,8 +77,9 @@ if (options.devicetwin) {
                 const fn = `${dir}/${dashify(srv.shortName)}.json`
                 cli.debug(`${srv.name} => ${fn}`)
                 cli.progress(i / (services.length - 1))
-                const dtdl = serviceSpecificationToServiceTwinSpecification(srv)
-                fs.writeJSONSync(fn, dtdl, { spaces: 2 })
+                const serviceTwin =
+                    serviceSpecificationToServiceTwinSpecification(srv)
+                fs.writeJSONSync(fn, serviceTwin, { spaces: 2 })
             })
         }
 
