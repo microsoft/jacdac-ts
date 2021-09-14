1d1a7fd83fd157f3e96bc52f83f1d3bf1d56e92f
pelikhan <jhalleux@microsoft.com>
2021-09-13 21:12:53 -0700

    added comments

--- tests/p5/sketch.js
+++ tests/p5/sketch.js
@@ -1,11 +1,15 @@
 function setup() {
     createCanvas(400, 400)
+    // connection to a USB/serial device must be initiated by the user (button click)
     connect = createButton("connect")
     connect.mousePressed(jacdac.connect)
 }
 
 function draw() {
+    // grabs an array with all potentiometer readings (number[]) connected to jacdac
     const { potentiometer } = jacdac.sensors
+    // destructure readings into r,g,b variables
     const [r = 0, g = 0, b = 0] = potentiometer
+    // rescale 0..1 to 0..255 to repaint background
     background(r * 255, g * 255, b * 255)
 }
