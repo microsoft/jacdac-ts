3c702a14ad20416b21d7cefba2c632012db2eb31
pelikhan <jhalleux@microsoft.com>
2021-11-05 09:03:28 -0700

    patch: more logic in catalog

--- src/jdom/catalog.ts
+++ src/jdom/catalog.ts
@@ -1,6 +1,20 @@
 import deviceRegistryData from "../../jacdac-spec/dist/devices.json"
 import { CHANGE } from "./constants"
 import JDEventSource from "./eventsource"
+import { cryptoRandomUint32 } from "./random"
+import { serviceSpecificationFromClassIdentifier } from "./spec"
+import { toFullHex } from "./utils"
+
+function looksRandom(n: number) {
+    const s = n.toString(16)
+    const h = "0123456789abcdef"
+    for (let i = 0; i < h.length; ++i) {
+        const hh = h[i]
+        if (s.indexOf(hh + hh + hh) >= 0) return false
+    }
+    if (/f00d|dead|deaf|beef/.test(s)) return false
+    return true
+}
 
 export interface DeviceSpecificationOptions {
     includeDeprecated?: boolean
@@ -102,6 +116,58 @@ export class DeviceCatalog extends JDEventSource {
 
         const ids = this.vendorIds(type)
         return ids.indexOf(id) > -1
+    }
+
+    /**
+     * Generates a unique firmware identifier
+     * @returns 
+     */
+    uniqueFirmwareId() {
+        const genFirmwareId = () => {
+            const n = cryptoRandomUint32(1)
+            if (n === undefined) return undefined
+            return (n[0] & 0xfff_ffff) | 0x3000_0000
+        }
+
+        let id = genFirmwareId()
+        while (
+            id !== undefined &&
+            (!looksRandom(id) ||
+                deviceCatalog.specificationFromProductIdentifier(id))
+        ) {
+            id = genFirmwareId()
+        }
+        return id !== undefined && toFullHex([id])
+    }
+
+    /**
+     * Generate a unique service identifier
+     * @returns 
+     */
+    uniqueServiceId() {
+        const genServId = () => {
+            const n = cryptoRandomUint32(1)
+            if (n === undefined) return undefined
+            return (n[0] & 0xfff_ffff) | 0x1000_0000
+        }
+
+        let id = genServId()
+        while (
+            id !== undefined &&
+            (!looksRandom(id) || serviceSpecificationFromClassIdentifier(id))
+        ) {
+            id = genServId()
+        }
+        return id !== undefined && toFullHex([id])
+    }
+
+    /**
+     * Generate a unique device identifier
+     * @returns 
+     */
+    uniqueDeviceId() {
+        const n = cryptoRandomUint32(2)
+        return n !== undefined && toFullHex([n[0], n[1]])
     }
 }
 
