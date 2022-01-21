320af7181b25d34afca1e56ffb688a910115f168
pelikhan <jhalleux@microsoft.com>
2022-01-21 13:10:11 -0800

    adding bridge to bundle

--- src/jdom/jacdac-jdom.ts
+++ src/jdom/jacdac-jdom.ts
@@ -63,6 +63,7 @@ export * from "./filters/devicefilter"
 export * from "./sensors"
 export * from "./bridges/iframebridge"
 export * from "./catalog"
+export * from "./bridge"
 
 export * from "./transport/nodesocket"
 export * from "./transport/nodespi"
