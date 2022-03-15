73c1de62b4d511ccd2721f0b8db795c658d28557
pelikhan <jhalleux@microsoft.com>
2022-03-15 10:18:27 -0700

    add status code to service client

--- src/jdom/serviceclient.ts
+++ src/jdom/serviceclient.ts
@@ -2,6 +2,7 @@ import { JDService } from "./service"
 import { JDDevice } from "./device"
 import { JDBus } from "./bus"
 import { JDClient } from "./client"
+import { CHANGE, EVENT, SystemEvent, SystemReg, SystemStatusCodes } from "./constants"
 
 /**
  * Base class for service clients
@@ -10,6 +11,11 @@ import { JDClient } from "./client"
 export class JDServiceClient extends JDClient {
     constructor(public readonly service: JDService) {
         super()
+
+        const statusCodeChanged = this.service.event(
+            SystemEvent.StatusCodeChanged
+        )
+        this.mount(statusCodeChanged?.subscribe(EVENT, () => this.emit(CHANGE)))
     }
 
     protected get device(): JDDevice {
@@ -18,6 +24,11 @@ export class JDServiceClient extends JDClient {
 
     protected get bus(): JDBus {
         return this.device.bus
+    }
+
+    get statusCode(): SystemStatusCodes {
+        const reg = this.service.register(SystemReg.StatusCode)
+        return reg.unpackedValue?.[0]
     }
 
     toString(): string {
