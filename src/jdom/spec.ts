38f8326e8b797f0cc41d3695ff022e46b6f21978
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-11-30 16:35:40 +0000

    more fixes

--- src/jdom/spec.ts
+++ src/jdom/spec.ts
@@ -19,8 +19,8 @@ import {
     SRV_UNIQUE_BRAIN,
     SRV_DASHBOARD,
     SRV_BRIDGE,
-    SRV_JACSCRIPT_CONDITION,
-    SRV_JACSCRIPT_MANAGER,
+    SRV_DEVICE_SCRIPT_CONDITION,
+    SRV_DEVICE_SCRIPT_MANAGER,
 } from "./constants"
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -139,8 +139,8 @@ export function isInfrastructure(spec: jdspec.ServiceSpec) {
             SRV_UNIQUE_BRAIN,
             SRV_DASHBOARD,
             SRV_BRIDGE,
-            SRV_JACSCRIPT_CONDITION,
-            SRV_JACSCRIPT_MANAGER,
+            SRV_DEVICE_SCRIPT_CONDITION,
+            SRV_DEVICE_SCRIPT_MANAGER,
         ].indexOf(spec.classIdentifier) > -1 ||
             spec.shortId[0] === "_")
     )
